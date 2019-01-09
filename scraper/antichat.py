dbb652593c1fc0c85c969ca105495f4c3d44b93f
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-01-09 21:11:13 +0545

    antichat scrapper updated.

--- scraper/antichat.py
+++ scraper/antichat.py
@@ -1,87 +1,136 @@
-from bs4 import BeautifulSoup
-import traceback
-import requests
-import time
+import re
 import os
+import time
+import traceback
+from requests import Session
+from lxml.html import fromstring
 
 
+# Topic Counter
+TOPIC_START_COUNT = 56700
+TOPIC_END_COUNT = 56800
+
+"""
+If OUTPUT_PATH not set, then, a new folder "antichat" will be created
+inside current script's path and files will be saved inside this new folder.
+"""
+OUTPUT_PATH = None
 
 
-class Download_Templates:
+class AntichatScrapper:
     def __init__(self):
-        self.topic_counter = 130
-        self.path = os.getcwd()
-        if not os.path.exists('%s/templates' %self.path):
-            os.makedirs('%s/templates' %self.path)
-        self.complete_path = '%s/templates' %self.path
+        self.topic_start_count = TOPIC_START_COUNT
+        self.topic_end_count = TOPIC_END_COUNT
+        self.site_link = "https://forum.antichat.ru/"
+        self.topic_url = self.site_link + "threads/{}/"
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/71.0.3578.98 Safari/537.36'
+        }
+        self.session = Session()
+        self.set_output_path()
 
+    def set_output_path(self):
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        self.output_path = OUTPUT_PATH\
+            if OUTPUT_PATH else '{}/antichat'.format(current_path)
 
-    def Request(self, url):
-        time.sleep(1)
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+
+    def get_html_response(self, content):
+        html_response = fromstring(content)
+        return html_response
+
+    def get_page_content(self, url):
+        time.sleep(0.5)
         try:
-            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-            page = requests.get(url, headers=headers)
-            return page.content
+            response = self.session.get(url, headers=self.headers)
+            content = response.content
+            html_response = self.get_html_response(content)
+            if html_response.xpath(
+               '//label[contains(text(),'
+               '"The requested thread could not be found")]'):
+                return
+            return content
         except:
-            return None
+            return
 
+    def process_first_page(self, topic):
+        url = self.topic_url.format(topic)
+        content = self.get_page_content(url)
+        if not content:
+            print('No data for url: {}'.format(url))
+            return
 
-    def write_data(self, page, topic):
-        f = open('%s/%d.html' %(self.complete_path, topic), 'wb')
-        f.write(page)
-        f.close()
+        initial_file = '{}/{}.html'.format(self.output_path, topic)
+        with open(initial_file, 'wb') as f:
+            f.write(content)
+        print('{} done..!'.format(topic))
+        html_response = self.get_html_response(content)
+        return html_response
 
-    def write_data_pagination(self, page, topic, pagination):
-        f = open('%s/%d-%d.html' %(self.complete_path, topic, pagination), 'wb')
-        f.write(page)
-        f.close()
+    def write_paginated_data(self, html_response):
+        next_page_block = html_response.xpath(
+            '//a[contains(@class,"currentPage")]'
+            '/following-sibling::a[1]/@href'
+        )
+        if not next_page_block:
+            return
+        next_page_url = self.site_link + next_page_block[0]\
+            if self.site_link not in next_page_block[0]\
+            else next_page_block[0]
+        pattern = re.compile(r"threads/(\d+)/page-(\d+)")
+        match = pattern.findall(next_page_url)
+        if not match:
+            return
+        topic, pagination_value = match[0]
 
-    def main(self):
-        print('**************  Script Is Running  **************\n')
+        content = self.get_page_content(next_page_url)
+        if not content:
+            return
 
-        #go to thread ------------------
-        for topic in range(100, 425295):
+        paginated_file = '{}/{}-{}.html'.format(
+            self.output_path, topic, pagination_value
+        )
+        with open(paginated_file, 'wb') as f:
+            f.write(content)
+
+        print('{}-{} done..!'.format(topic, pagination_value))
+        return content
+
+    def process_pagination(self, response):
+        while True:
+            paginated_content = self.write_paginated_data(response)
+            if not paginated_content:
+                return
+            response = self.get_html_response(paginated_content)
+
+    def clear_cookies(self,):
+        self.session.cookies['topicsread'] = ''
+
+    def do_scrape(self):
+        print('**************  Antichat Scrapper Started  **************\n')
+        # ----------------go to topic ------------------
+        for topic in range(self.topic_start_count, self.topic_end_count):
             try:
-                url = 'https://forum.antichat.ru/threads/%d' %topic
-                page = self.Request(url)
-
-                #getting pagination number----
-                try:
-                    soup = BeautifulSoup(page, 'html.parser')
-                    pagi = int(soup.select_one('.pageNavHeader').text.split()[-1]) 
-                except:
-                    pagi = 0
-
-                self.write_data(page, topic)
-                print('%d done..!' %topic)
-
-                #if there is no pagination then continue----
-                if pagi == 0:
+                response = self.process_first_page(topic)
+                if response is None:
                     continue
+
+                # ------------clear cookies without logout--------------
+                self.clear_cookies()
             except:
                 traceback.print_exc()
                 continue
+            self.process_pagination(response)
 
 
-            #for pagination ------------------
-            pagination_counter = 20
-            for pagination in range(2, int(pagi)):
-                try:
-                    p_url = 'https://forum.antichat.ru/threads/%d/page-%d' %(topic, pagination_counter)
-                    page = self.Request(p_url)
-
-                    if page == None:
-                        break
-
-                    self.write_data_pagination(page, topic, pagination)
-                    print('%d-%d done..!' %(topic, pagination))
-                    pagination_counter += 20
-                except:
-                    traceback.print_exc()
-                    continue
+def main():
+    template = AntichatScrapper()
+    template.do_scrape()
 
 
-obj = Download_Templates()
-obj.main()
-        
-        
+if __name__ == '__main__':
+    main()
