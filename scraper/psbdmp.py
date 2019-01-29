cd75bbcaf73a2472349c44fb15a8fd45c53ee83b
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-01-29 06:41:55 +0545

    start_date made as 2015/01/01 and recursively scraped to current date in 1 week interval. Proxy feature added.

--- scraper/psbdmp.py
+++ scraper/psbdmp.py
@@ -1,7 +1,7 @@
 import re
 import os
 import datetime
-import requests
+from requests import Session
 import time
 import traceback
 from lxml.html import fromstring
@@ -11,17 +11,30 @@ class PasteBinScrapper:
     def __init__(self, kwargs):
         self.api_url = 'https://psbdmp.ws/api/dump/get/timeval/{}/{}'
         self.dump_url = 'https://pastebin.com/{}'
-        self.start_date = kwargs.get('topic_start')
-        self.end_date = kwargs.get('topic_end')
+        self.start_date = "2015/01/01"
         self.output_path = kwargs.get('output')
+        self.session = Session()
+        if kwargs.get('proxy'):
+            self.session.proxies = {
+                'http': kwargs.get('proxy'),
+                'https': kwargs.get('proxy'),
+            }
 
-    def get_ts(self, date_string):
+    def get_ts(self,):
         try:
             pattern = '%Y/%m/%d'
-            ts = int(datetime.datetime.strptime(
-                date_string, pattern).timestamp())
-            return ts
+            start_date = datetime.datetime.strptime(self.start_date, pattern)
+            start_ts = int(start_date.timestamp())
+            end_date = start_date + datetime.timedelta(days=7)
+            end_ts = int(end_date.timestamp())
+            if end_ts > int(datetime.datetime.now().timestamp()):
+                return
+            self.start_date = end_date.strftime(pattern)
+            print('\n\nStart date: {}, End date: {}'
+                  .format(start_date, end_date))
+            return start_ts, end_ts
         except:
+            traceback.print_exc()
             return
 
     def save_file(self, _id):
@@ -31,7 +44,7 @@ class PasteBinScrapper:
         if os.path.exists(dump_file):
             return
         dump_url = self.dump_url.format(_id)
-        content = requests.get(dump_url).content
+        content = self.session.get(dump_url).content
         if not content:
             return
         html_response = fromstring(content)
@@ -39,6 +52,7 @@ class PasteBinScrapper:
             '//textarea[@class="paste_code"]/text()'
         )
         if not raw_text:
+            print('IP banned')
             return
         with open(dump_file, 'w') as f:
             f.write(raw_text[0])
@@ -46,25 +60,22 @@ class PasteBinScrapper:
 
     def do_scrape(self):
         print('************  Pastebin Scrapper Started  ************\n')
-        start_ts = self.get_ts(self.start_date)
-        end_ts = self.get_ts(self.end_date)
-        if not start_ts:
-            print('Invalid format for start date')
-            return
-        if not end_ts:
-            print('Invalid format for end date')
-            return
-        try:
-            url = self.api_url.format(start_ts, end_ts)
-            response = requests.get(url).json()
-            if response.get('error_info'):
-                print(response['error_info'])
+        while True:
+            ts = self.get_ts()
+            if not ts:
                 return
-            for data in response['data']:
-                self.save_file(data['id'])
-        except:
-            traceback.print_exc()
-            pass
+            start_ts, end_ts = ts
+            try:
+                url = self.api_url.format(start_ts, end_ts)
+                response = self.session.get(url).json()
+                if response.get('error_info'):
+                    print(response['error_info'])
+                    continue
+                for data in response['data']:
+                    self.save_file(data['id'])
+            except:
+                traceback.print_exc()
+                pass
 
 
 def main():
