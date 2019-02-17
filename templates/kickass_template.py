8b689c886633594185201c81285d4c4b2b14ac3d
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-02-17 08:53:14 +0545

    user profile parsing for kickass

--- templates/kickass_template.py
+++ templates/kickass_template.py
@@ -19,6 +19,9 @@ class KickAssParser:
         self.thread_name_pattern = re.compile(
             r'(\d+).*html'
         )
+        self.uid_pattern = re.compile(
+            r'UID-(\d+)\.html'
+        )
         self.files = self.get_filtered_files(files)
         self.folder_path = folder_path
         self.data_dic = OrderedDict()
@@ -42,6 +45,53 @@ class KickAssParser:
 
         return sorted_files
 
+    def extract_user_profile(self, html_response):
+        data = dict()
+        username = html_response.xpath(
+            '//fieldset//span[@class="largetext"]'
+            '//strong/text()'
+        )
+        if username:
+            data.update({
+                'username': username[0].strip()
+            })
+        jabber_id = html_response.xpath(
+            '//td[strong[contains(text(), "Jabber ID:")]]'
+            '/following-sibling::td[1]/text()'
+        )
+        if jabber_id:
+            data.update({
+                'jabberID': jabber_id[0].strip()
+            })
+        pgp_key_block = html_response.xpath(
+            '//td[strong[contains(text(), "PGP Public Key:")]]'
+            '/following-sibling::td[1]'
+        )
+        pgp_key = "".join([
+            pgp_key.xpath('string()') for pgp_key in pgp_key_block
+        ])
+        data.update({
+            'PGPKey': pgp_key.strip()
+        })
+        return data
+
+    def process_user_profile(self, uid, html_response):
+        data = {
+            'userID': uid
+        }
+        additional_data = self.extract_user_profile(html_response)
+        if not additional_data:
+            return
+        data.update(additional_data)
+        output_file = '{}/UID-{}.json'.format(
+            str(self.output_folder),
+            uid
+        )
+        with open(output_file, 'w', encoding='utf-8') as file_pointer:
+            utils.write_json(file_pointer, data)
+            print('\nJson written in {}'.format(output_file))
+            print('----------------------------------------\n')
+
     def main(self):
         comments = []
         output_file = None
@@ -50,6 +100,10 @@ class KickAssParser:
             try:
                 html_response = utils.get_html_response(template)
                 file_name_only = template.split('/')[-1]
+                uid_match = self.uid_pattern.findall(file_name_only)
+                if uid_match:
+                    self.process_user_profile(uid_match[0], html_response)
+                    continue
                 match = self.thread_name_pattern.findall(file_name_only)
                 if not match:
                     continue
