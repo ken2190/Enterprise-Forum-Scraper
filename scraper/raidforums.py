82ec527b934304f890fe2bb00756616f5fbed2b3
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-07-24 10:57:48 +0545

    For raidforums, switch added to scrape only users and user history pages are also downloaded.

--- scraper/raidforums.py
+++ scraper/raidforums.py
@@ -10,12 +10,13 @@ from scrapy.crawler import CrawlerProcess
 class RaidForumsSpider(scrapy.Spider):
     name = 'raidforums_spider'
 
-    def __init__(self, output_path):
+    def __init__(self, output_path, useronly):
         self.base_url = "https://raidforums.com/"
         self.pagination_pattern = re.compile(r'.*page=(\d+)')
         self.username_pattern = re.compile(r'User-(.*)')
         self.start_url = 'https://raidforums.com/'
         self.output_path = output_path
+        self.useronly = useronly
         self.headers = {
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) "
                           "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -31,7 +32,7 @@ class RaidForumsSpider(scrapy.Spider):
     def start_requests(self):
         yield Request(
             url=self.start_url,
-            headers=self.headers,
+            # headers=self.headers,
             callback=self.parse
         )
 
@@ -44,7 +45,7 @@ class RaidForumsSpider(scrapy.Spider):
                 url = self.base_url + url
             yield Request(
                 url=url,
-                headers=self.headers,
+                # headers=self.headers,
                 callback=self.parse_forum
             )
 
@@ -52,24 +53,25 @@ class RaidForumsSpider(scrapy.Spider):
         print('next_page_url: {}'.format(response.url))
         threads = response.xpath(
             '//a[@class="forum-display__thread-name"]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = str(
-                int.from_bytes(
-                    thread_url.encode('utf-8'), byteorder='big'
-                ) % (10 ** 7)
-            )
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
+        if not self.useronly:
+            for thread in threads:
+                thread_url = thread.xpath('@href').extract_first()
+                if self.base_url not in thread_url:
+                    thread_url = self.base_url + thread_url
+                topic_id = str(
+                    int.from_bytes(
+                        thread_url.encode('utf-8'), byteorder='big'
+                    ) % (10 ** 7)
+                )
+                file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
+                if os.path.exists(file_name):
+                    continue
+                yield Request(
+                    url=thread_url,
+                    # headers=self.headers,
+                    callback=self.parse_thread,
+                    meta={'topic_id': topic_id}
+                )
 
         users = response.xpath('//span[@class="author smalltext"]/a')
         for user in users:
@@ -84,7 +86,7 @@ class RaidForumsSpider(scrapy.Spider):
                 continue
             yield Request(
                 url=user_url,
-                headers=self.headers,
+                # headers=self.headers,
                 callback=self.parse_user,
                 meta={
                     'file_name': file_name,
@@ -99,7 +101,7 @@ class RaidForumsSpider(scrapy.Spider):
                 next_page_url = self.base_url + next_page_url
             yield Request(
                 url=next_page_url,
-                headers=self.headers,
+                # headers=self.headers,
                 callback=self.parse_forum
             )
 
@@ -108,6 +110,28 @@ class RaidForumsSpider(scrapy.Spider):
         with open(file_name, 'wb') as f:
             f.write(response.text.encode('utf-8'))
             print(f"User {response.meta['user_id']} done..!")
+        user_history = response.xpath(
+            '//span[text()="Username Changes:"]'
+            '/following-sibling::a[1]'
+        )
+        if user_history:
+            history_url = user_history.xpath('@href').extract_first()
+            if self.base_url not in history_url:
+                history_url = self.base_url + history_url
+            yield Request(
+                url=history_url,
+                # headers=self.headers,
+                callback=self.parse_user_history,
+                meta={'user_id': response.meta['user_id']}
+            )
+
+    def parse_user_history(self, response):
+        user_id = response.meta['user_id']
+        file_name = '{}/{}-history.html'.format(self.user_path, user_id)
+        with open(file_name, 'wb') as f:
+            f.write(response.text.encode('utf-8'))
+            print(f"History for user {user_id} done..!")
+
 
     def parse_thread(self, response):
         topic_id = response.meta['topic_id']
@@ -127,7 +151,7 @@ class RaidForumsSpider(scrapy.Spider):
                 next_page_url = self.base_url + next_page_url
             yield Request(
                 url=next_page_url,
-                headers=self.headers,
+                # headers=self.headers,
                 callback=self.parse_thread,
                 meta={'topic_id': topic_id}
             )
@@ -136,6 +160,7 @@ class RaidForumsSpider(scrapy.Spider):
 class RaidForumsScrapper():
     def __init__(self, kwargs):
         self.output_path = kwargs.get('output')
+        self.useronly = kwargs.get('useronly')
         self.proxy = kwargs.get('proxy') or None
         self.request_delay = 0.1
         self.no_of_threads = 20
@@ -163,7 +188,7 @@ class RaidForumsScrapper():
 
             })
         process = CrawlerProcess(settings)
-        process.crawl(RaidForumsSpider, self.output_path)
+        process.crawl(RaidForumsSpider, self.output_path, self.useronly)
         process.start()
 
 if __name__ == '__main__':
