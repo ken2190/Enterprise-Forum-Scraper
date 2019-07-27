950728a17effaa2639706fd4d418d5cb02d9934e
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-07-27 15:35:17 +0545

    psbdmp scraper updates

--- scraper/psbdmp.py
+++ scraper/psbdmp.py
@@ -10,9 +10,11 @@ from lxml.html import fromstring
 class PasteBinScrapper:
     def __init__(self, kwargs):
         self.base_url = 'https://psbdmp.ws'
-        self.daily_url = self.base_url + '/daily'
-        self.dump_url = 'https://psbdmp.ws/api/dump/get/{}'
+        self.start_url = 'https://psbdmp.ws/api/dump/getbydate'
+        self.dump_url = 'https://psbdmp.ws/archive/{}'
         self.output_path = kwargs.get('output')
+        self.date_format = '%d.%m.%Y'
+        self.start_date = datetime.datetime.strptime(kwargs.get('start_date'), self.date_format)
 
     def save_file(self, dump_id, output_path):
         dump_file = '{}/{}.txt'.format(
@@ -21,41 +23,35 @@ class PasteBinScrapper:
         if os.path.exists(dump_file):
             return
         dump_url = self.dump_url.format(dump_id)
-        response = requests.get(dump_url).json()
-        if not response:
-            return
-        if response.get('error_info'):
-            print(response['error_info'])
-            return
+        response = requests.get(dump_url).text
+        html_response = fromstring(response)
+        content = html_response.xpath(
+            '//div[@class="content article-body"]/descendant::text()')
+        content = ''.join(content)
         with open(dump_file, 'w') as f:
-            f.write(response['data'])
+            f.write(content)
         print('{} done..!'.format(dump_file))
 
     def do_scrape(self):
         print('************  Pastebin Scrapper Started  ************\n')
-        content = requests.get(self.daily_url).content
-        html_response = fromstring(content)
-        daily_dumps = html_response.xpath(
-            '//div[@class="container"]/table//td')
-        for daily_dump in reversed(daily_dumps):
+        while self.start_date < datetime.datetime.now():
             try:
-                url = daily_dump.xpath('a/@href')
-                date = daily_dump.xpath('a/text()')
-                if not url:
-                    continue
-                output_path = '{}/{}'.format(self.output_path, date[0])
+                _from = self.start_date.strftime(self.date_format)
+                output_path = '{}/{}'.format(self.output_path, _from)
                 if os.path.exists(output_path):
                     continue
                 os.makedirs(output_path)
-                print('\nGetting dump for {}'.format(date[0]))
-                print('--------------------------------')
-                url = self.base_url + url[0]
-                content = requests.get(url).content
-                html_response = fromstring(content)
-                dump_ids = html_response.xpath(
-                    '//div[@class="container"]/table//td/a')
-                for dump_id in dump_ids:
-                    dump_id = dump_id.xpath('text()')[0]
+                self.start_date = self.start_date + datetime.timedelta(days=1)
+                _to = self.start_date.strftime(self.date_format)
+                data = {
+                    'from': _from,
+                    'to': _to
+                }
+                print('\nGetting dump for {}'.format(_from))
+                response = requests.post(self.start_url, data=data)
+                json_data = response.json()
+                for data in json_data['data']:
+                    dump_id = data['id']
                     self.save_file(dump_id, output_path)
             except:
                 traceback.print_exc()
