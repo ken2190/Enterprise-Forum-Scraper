3079ccb02c359df3d956aee7e537d0b29b85d0d3
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-08-06 11:13:33 +0545

    rstforums scraper updated to have avatars.

--- scraper/rstforums.py
+++ scraper/rstforums.py
@@ -10,13 +10,14 @@ from scrapy.crawler import CrawlerProcess
 class RSTForumsSpider(scrapy.Spider):
     name = 'rstforums_spider'
 
-    def __init__(self, output_path):
+    def __init__(self, output_path, avatar_path):
         self.base_url = "https://rstforums.com/"
         self.topic_pattern = re.compile(r'topic/(\d+)-')
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.pagination_pattern = re.compile(r'.*/page/(\d+)/')
         self.start_url = 'https://rstforums.com/forum/'
         self.output_path = output_path
+        self.avatar_path = avatar_path
         self.headers = {
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) "
                           "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -85,6 +86,29 @@ class RSTForumsSpider(scrapy.Spider):
             f.write(response.text.encode('utf-8'))
             print(f'{topic_id}-{paginated_value} done..!')
 
+        avatars = response.xpath('//li[@class="cAuthorPane_photo"]/a/img')
+        for avatar in avatars:
+            avatar_url = avatar.xpath('@src').extract_first()
+            if 'image/svg' in avatar_url:
+                continue
+            user_id = avatar.xpath('@alt').extract_first()
+            name_match = self.avatar_name_pattern.findall(avatar_url)
+            if not name_match:
+                continue
+            name = name_match[0]
+            file_name = '{}/{}'.format(self.avatar_path, name)
+            if os.path.exists(file_name):
+                continue
+            yield Request(
+                url=avatar_url,
+                headers=self.headers,
+                callback=self.parse_avatar,
+                meta={
+                    'file_name': file_name,
+                    'user_id': user_id
+                }
+            )
+
         next_page = response.xpath('//li[@class="ipsPagination_next"]/a')
         if next_page:
             next_page_url = next_page.xpath('@href').extract_first()
@@ -97,6 +121,12 @@ class RSTForumsSpider(scrapy.Spider):
                 meta={'topic_id': topic_id}
             )
 
+    def parse_avatar(self, response):
+        file_name = response.meta['file_name']
+        with open(file_name, 'wb') as f:
+            f.write(response.body)
+            print(f"Avatar for user {response.meta['user_id']} done..!")
+
 
 class RSTForumsScrapper():
     def __init__(self, kwargs):
@@ -104,6 +134,12 @@ class RSTForumsScrapper():
         self.proxy = kwargs.get('proxy') or None
         self.request_delay = 0.1
         self.no_of_threads = 16
+        self.ensure_avatar_path()
+
+    def ensure_avatar_path(self, ):
+        self.avatar_path = f'{self.output_path}/avatars'
+        if not os.path.exists(self.avatar_path):
+            os.makedirs(self.avatar_path)
 
     def do_scrape(self):
         settings = {
@@ -128,7 +164,7 @@ class RSTForumsScrapper():
 
             })
         process = CrawlerProcess(settings)
-        process.crawl(RSTForumsSpider, self.output_path)
+        process.crawl(RSTForumsSpider, self.output_path, self.avatar_path)
         process.start()
 
 if __name__ == '__main__':
