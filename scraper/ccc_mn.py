cb2ea0b1fe086b0e725d11a73a8a735d5ee5a7e0
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-09-03 16:52:44 +0545

    cccmn scraper updated

--- scraper/ccc_mn.py
+++ scraper/ccc_mn.py
@@ -1,164 +1,172 @@
-import re
 import os
-import time
-import traceback
-import requests
-from requests import Session
-from scraper.base_scrapper import BaseScrapper
+import re
+import scrapy
+from math import ceil
+import configparser
+from scrapy.http import Request, FormRequest
+from scrapy.crawler import CrawlerProcess
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 
 
-class CCCMNScrapper(BaseScrapper):
-    def __init__(self, kwargs):
-        super(CCCMNScrapper, self).__init__(kwargs)
-        self.base_url = 'http://ccc.mn/index.php'
-        self.topic_pattern = re.compile(r'/topic/(\d+)-')
+class CCCMNSpider(scrapy.Spider):
+    name = 'ccc_spider'
+
+    def __init__(self, output_path, avatar_path):
+        self.base_url = "https://ccc.sb/"
+        self.topic_pattern = re.compile(r'topic/(\d+)-')
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.cloudfare_error = None
+        self.pagination_pattern = re.compile(r'.*/page/(\d+)/')
+        self.start_url = 'https://ccc.sb/'
+        self.output_path = output_path
+        self.avatar_path = avatar_path
+        self.headers = {
+            "user-agent": USER_AGENT,
+        }
 
-    def get_page_content(self, url):
-        time.sleep(self.wait_time)
-        try:
-            response = requests.get(url)
-            content = response.content
-            html_response = self.get_html_response(content)
-            if html_response.xpath('//div[@class="errorwrap"]'):
-                return
-            return content
-        except:
-            return
-
-    def save_avatar(self, name, url):
-        avatar_file = f'{self.avatar_path}/{name}'
-        if os.path.exists(avatar_file):
-            return
-        content = self.get_page_content(url)
-        if not content:
-            return
-        with open(avatar_file, 'wb') as f:
-            f.write(content)
-
-    def process_first_page(self, topic_url):
-        topic = self.topic_pattern.findall(topic_url)
-        if not topic:
-            return
-        topic = topic[0]
-        initial_file = f'{self.output_path}/{topic}-1.html'
-        if os.path.exists(initial_file):
-            return
-        content = self.get_page_content(topic_url)
-        if not content:
-            print(f'No data for url: {topic_url}')
-            return
-        with open(initial_file, 'wb') as f:
-            f.write(content)
-        print(f'{topic}-1 done..!')
-        html_response = self.get_html_response(content)
-        avatar_info = self.get_avatar_info(html_response)
-        for name, url in avatar_info.items():
-            self.save_avatar(name, url)
-        return html_response
-
-    def process_topic(self, topic_url):
-        html_response = self.process_first_page(topic_url)
-        if html_response is None:
-            return
-        while True:
-            response = self.write_paginated_data(html_response)
-            if response is None:
-                return
-            topic_url, html_response = response
-
-    def write_paginated_data(self, html_response):
-        next_page_block = html_response.xpath(
-            '//ul[@class="ipsList_inline forward left"]'
-            '/li[@class="next"]/a/@href'
+    def start_requests(self):
+        yield Request(
+            url=self.start_url,
+            headers=self.headers,
+            callback=self.parse,
+            dont_filter=True,
         )
-        if not next_page_block:
-            return
-        next_page_url = next_page_block[0]
-        pattern = re.compile(r'/topic/(\d+)-.*/page__st__(\d+)')
-        match = pattern.findall(next_page_url)
-        if not match:
-            return
-        topic, pagination_value = match[0]
-        content = self.get_page_content(next_page_url)
-        if not content:
-            return
-        pagination_value = int(int(pagination_value)/20 + 1)
-        paginated_file = f'{self.output_path}/{topic}-{pagination_value}.html'
-        with open(paginated_file, 'wb') as f:
-            f.write(content)
 
-        print(f'{topic}-{pagination_value} done..!')
-        html_response = self.get_html_response(content)
-        avatar_info = self.get_avatar_info(html_response)
-        for name, url in avatar_info.items():
-            self.save_avatar(name, url)
-        return next_page_url, html_response
-
-    def process_forum(self, url):
-        while True:
-            print(f"Forum URL: {url}")
-            forum_content = self.get_page_content(url)
-            if not forum_content:
-                print(f'No data for url: {forum_content}')
-                return
-            html_response = self.get_html_response(forum_content)
-            topic_urls = html_response.xpath(
-                '//td[contains(@class, "col_f_content")]/h4/a/@href')
-            for topic_url in topic_urls:
-                self.process_topic(topic_url)
-            forum_pagination_url = html_response.xpath(
-                '//ul[@class="ipsList_inline forward left"]'
-                '/li[@class="next"]/a/@href'
+    def parse(self, response):
+        forums = response.xpath(
+            '//div[@class="ipsDataItem_main"]//h4'
+            '/a[contains(@href, "forum/")]')
+        for forum in forums:
+            url = forum.xpath('@href').extract_first()
+            if self.base_url not in url:
+                url = self.base_url + url
+            yield Request(
+                url=url,
+                headers=self.headers,
+                callback=self.parse_forum
             )
-            if not forum_pagination_url:
-                return
-            url = forum_pagination_url[0]
 
-    def get_forum_urls(self, html_response):
-        extracted_urls = html_response.xpath(
-            '//td[@class="col_c_forum"]/h4/a/@href')
-        return extracted_urls
+    def parse_forum(self, response):
+        print('next_page_url: {}'.format(response.url))
+        threads = response.xpath(
+            '//div[@class="ipsDataItem_main"]/h4//a')
+        for thread in threads:
+            thread_url = thread.xpath('@href').extract_first()
+            if self.base_url not in thread_url:
+                thread_url = self.base_url + thread_url
+            topic_id = self.topic_pattern.findall(thread_url)
+            if not topic_id:
+                continue
+            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
+            if os.path.exists(file_name):
+                continue
+            yield Request(
+                url=thread_url,
+                headers=self.headers,
+                callback=self.parse_thread,
+                meta={'topic_id': topic_id[0]}
+            )
 
-    def clear_cookies(self,):
-        self.session.cookies.clear()
+        next_page = response.xpath('//li[@class="ipsPagination_next"]/a')
+        if next_page:
+            next_page_url = next_page.xpath('@href').extract_first()
+            if self.base_url not in next_page_url:
+                next_page_url = self.base_url + next_page_url
+            yield Request(
+                url=next_page_url,
+                headers=self.headers,
+                callback=self.parse_forum
+            )
 
-    def get_avatar_info(self, html_response):
-        avatar_info = dict()
-        urls = html_response.xpath(
-            '//li[@class="avatar"]/a/img/@src'
-        )
-        for url in urls:
-            name_match = self.avatar_name_pattern.findall(url)
+    def parse_thread(self, response):
+        topic_id = response.meta['topic_id']
+        pagination = self.pagination_pattern.findall(response.url)
+        paginated_value = pagination[0] if pagination else 1
+        file_name = '{}/{}-{}.html'.format(
+            self.output_path, topic_id, paginated_value)
+        with open(file_name, 'wb') as f:
+            f.write(response.text.encode('utf-8'))
+            print(f'{topic_id}-{paginated_value} done..!')
+
+        avatars = response.xpath('//li[@class="cAuthorPane_photo"]/a/img')
+        for avatar in avatars:
+            avatar_url = avatar.xpath('@src').extract_first()
+            if not avatar_url.startswith('http'):
+                avatar_url = self.base_url + avatar_url
+            name_match = self.avatar_name_pattern.findall(avatar_url)
             if not name_match:
                 continue
             name = name_match[0]
-            if name not in avatar_info:
-                avatar_info.update({
-                    name: url
-                })
-        return avatar_info
+            file_name = '{}/{}'.format(self.avatar_path, name)
+            if os.path.exists(file_name):
+                continue
+            yield Request(
+                url=avatar_url,
+                headers=self.headers,
+                callback=self.parse_avatar,
+                meta={
+                    'file_name': file_name,
+                }
+            )
+
+        next_page = response.xpath('//li[@class="ipsPagination_next"]/a')
+        if next_page:
+            next_page_url = next_page.xpath('@href').extract_first()
+            if self.base_url not in next_page_url:
+                next_page_url = self.base_url + next_page_url
+            yield Request(
+                url=next_page_url,
+                headers=self.headers,
+                callback=self.parse_thread,
+                meta={'topic_id': topic_id}
+            )
+
+    def parse_avatar(self, response):
+        file_name = response.meta['file_name']
+        file_name_only = file_name.rsplit('/', 1)[-1]
+        with open(file_name, 'wb') as f:
+            f.write(response.body)
+            print(f"Avatar {file_name_only} done..!")
+
+
+class CCCMNScrapper():
+    def __init__(self, kwargs):
+        self.output_path = kwargs.get('output')
+        self.proxy = kwargs.get('proxy') or None
+        self.request_delay = 0.1
+        self.no_of_threads = 16
+        self.ensure_avatar_path()
+
+    def ensure_avatar_path(self, ):
+        self.avatar_path = f'{self.output_path}/avatars'
+        if not os.path.exists(self.avatar_path):
+            os.makedirs(self.avatar_path)
 
     def do_scrape(self):
-        print('**************  CCCMN Scrapper Started  **************\n')
-        main_content = self.get_page_content(self.base_url)
-        if not main_content:
-            print(f'No data for url: {self.base_url}')
-            return
-        html_response = self.get_html_response(main_content)
-        forum_urls = self.get_forum_urls(html_response)
-        # print(forum_urls)
-        # return
-        # forum_urls = ["http://www.badkarma.ru/forums/veschevoj-karding.68/"]
-        for forum_url in forum_urls:
-            self.process_forum(forum_url)
+        settings = {
+            'DOWNLOAD_DELAY': self.request_delay,
+            'CONCURRENT_REQUESTS': self.no_of_threads,
+            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
+            'RETRY_HTTP_CODES': [403, 429, 500, 503],
+            'RETRY_TIMES': 10,
+            'LOG_ENABLED': True,
 
+        }
+        if self.proxy:
+            settings.update({
+                "DOWNLOADER_MIDDLEWARES": {
+                    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+                    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+                    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+                    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+                    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+                },
+                'ROTATING_PROXY_LIST': self.proxy,
 
-def main():
-    template = CCCMNScrapper()
-    template.do_scrape()
-
+            })
+        process = CrawlerProcess(settings)
+        process.crawl(CCCMNSpider, self.output_path, self.avatar_path)
+        process.start()
 
 if __name__ == '__main__':
-    main()
+    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
