407ff9f954039ec48895a89ea926fb9b4fc34872
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-09-04 15:09:37 +0545

    cccmn parser updated

--- templates/ccc_mn_template.py
+++ templates/ccc_mn_template.py
@@ -8,7 +8,7 @@ import json
 import utils
 import datetime
 from lxml.html import fromstring
-
+import dateutil.parser as dparser
 
 class BrokenPage(Exception):
     pass
@@ -25,12 +25,13 @@ class CCCMNParser:
         self.pagination_pattern = re.compile(
             r'\d+-(\d+)\.html$'
         )
-        self.avatar_name_pattern = re.compile(r'.*/(\w+\.\w+)')
+        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.files = self.get_filtered_files(files)
         self.folder_path = folder_path
         self.distinct_files = set()
         self.error_folder = "{}/Errors".format(output_folder)
         self.thread_id = None
+        self.index = 1
         # main function
         self.main()
 
@@ -47,13 +48,25 @@ class CCCMNParser:
 
         return sorted_files
 
+    def get_html_response(self, template):
+        """
+        returns the html response from the `template` contents
+        """
+        with open(template, 'r') as f:
+            content = f.read()
+            try:
+                html_response = fromstring(content)
+            except ParserError as ex:
+                return
+            return html_response
+
     def main(self):
         comments = []
         output_file = None
         for index, template in enumerate(self.files):
             print(template)
             try:
-                html_response = utils.get_html_response(template)
+                html_response = self.get_html_response(template)
                 file_name_only = template.split('/')[-1]
                 match = self.thread_name_pattern.findall(file_name_only)
                 if not match:
@@ -76,7 +89,7 @@ class CCCMNParser:
                         html_response, template)
                     if not data or not pagination == 1:
                         comments.extend(
-                            self.extract_comments(html_response))
+                            self.extract_comments(html_response, pagination))
                         continue
                     self.distinct_files.add(self.thread_id)
 
@@ -89,42 +102,41 @@ class CCCMNParser:
                     utils.write_json(file_pointer, data)
                 # extract comments
                 comments.extend(
-                    self.extract_comments(html_response))
+                    self.extract_comments(html_response, pagination))
 
                 if final:
                     utils.write_comments(file_pointer, comments, output_file)
                     comments = []
                     output_file = None
+                    self.index = 1
             except BrokenPage as ex:
                 utils.handle_error(
                     pid,
                     self.error_folder,
                     ex
                 )
-            except:
+            except Exception:
                 traceback.print_exc()
                 continue
 
-    def extract_comments(self, html_response):
+    def extract_comments(self, html_response, pagination):
         comments = list()
         comment_blocks = html_response.xpath(
-          '//div[@class="post_wrap"]'
+          '//article[contains(@id, "elComment_")]'
         )
-        # print(comment_blocks)
-        for index, comment_block in enumerate(comment_blocks, 1):
+        comment_blocks = comment_blocks[1:]\
+            if pagination == 1 else comment_blocks
+        for comment_block in comment_blocks:
             user = self.get_author(comment_block)
             comment_text = self.get_post_text(comment_block)
             comment_date = self.get_date(comment_block)
             pid = self.thread_id
             avatar = self.get_avatar(comment_block)
-            comment_id = self.get_comment_id(comment_block)
-            if not comment_id or comment_id == "1":
-                continue
             source = {
                 'forum': self.parser_name,
                 'pid': pid,
                 'message': comment_text.strip(),
-                'cid': comment_id,
+                'cid': str(self.index),
                 'author': user,
             }
             if comment_date:
@@ -136,9 +148,9 @@ class CCCMNParser:
                     'img': avatar
                 })
             comments.append({
-                
                 '_source': source,
             })
+            self.index += 1
         return comments
 
     def header_data_extract(self, html_response, template):
@@ -146,11 +158,9 @@ class CCCMNParser:
 
             # ---------------extract header data ------------
             header = html_response.xpath(
-                '//div[@class="post_wrap"]'
+                '//article[contains(@id, "elComment_")]'
             )
             if not header:
-                return
-            if not self.get_comment_id(header[0]) == "1":
                 return
             title = self.get_title(html_response)
             date = self.get_date(header[0])
@@ -174,53 +184,46 @@ class CCCMNParser:
                     'img': avatar
                 })
             return {
-                
                 '_source': source
             }
-        except:
+        except Exception:
             ex = traceback.format_exc()
             raise BrokenPage(ex)
 
     def get_date(self, tag):
         date_block = tag.xpath(
-            'div//abbr[@itemprop="commentTime"]/text()'
-        )
+                'div//div[@class="ipsType_reset"]//time/@title'
+            )
         date = date_block[0].strip() if date_block else ""
         try:
-            pattern = "%d %B %Y - %H:%M"
-            date = datetime.datetime.strptime(date, pattern).timestamp()
+            date = dparser.parse(date).timestamp()
             return str(date)
-        except:
-            date_block = tag.xpath(
-                'div//abbr[@itemprop="commentTime"]/@title'
-            )
-            date = date_block[0].split('+')[0].strip() if date_block else ""
-            pattern = "%Y-%m-%dT%H:%M:%S"
-            date = datetime.datetime.strptime(date, pattern).timestamp()
-            return str(date)
-        return ""
+        except Exception:
+            return ""
 
     def get_author(self, tag):
         author = tag.xpath(
-            'h3/span[@itemprop="creator name"]/text()'
+            'aside//a[@class="ipsType_break"]/text()'
         )
+        if not author:
+            author = tag.xpath(
+                'aside//a[@class="ipsType_break"]/span/text()'
+            )
         author = author[0].strip() if author else None
         return author
 
     def get_title(self, tag):
         title = tag.xpath(
-            '//div[@class="ipsBox_withphoto"]/class/text()'
+            '//span[@class="ipsType_break ipsContained"]/span/text()'
         )
         title = title[0].strip() if title else None
         return title
 
     def get_post_text(self, tag):
         post_text_block = tag.xpath(
-            'div/div[@itemprop="commentText"]'
-            '/descendant::text()['
-            'not(ancestor::p[@class="citation"]) and'
-            'not(ancestor::div[@class="blockquote"])]'
-        )
+                'div//div[@data-role="commentContent"]'
+                '/descendant::text()['
+                'not(ancestor::blockquote)]')
         post_text = " ".join([
             post_text.strip() for post_text in post_text_block
         ])
@@ -228,22 +231,13 @@ class CCCMNParser:
 
     def get_avatar(self, tag):
         avatar_block = tag.xpath(
-            'div//li[@class="avatar"]/a/img/@src'
+            'aside//li[@class="cAuthorPane_photo"]/a/img/@src'
         )
         if not avatar_block:
             return ""
         name_match = self.avatar_name_pattern.findall(avatar_block[0])
         if not name_match:
             return ""
+        if name_match[0].startswith('svg'):
+            return ''
         return name_match[0]
-
-    def get_comment_id(self, tag):
-        comment_id = ""
-        comment_block = tag.xpath(
-            'h3/span/a/text()'
-        )
-        # print(comment_block)
-        if comment_block:
-            comment_id = comment_block[0].strip().split('#')[-1]
-
-        return comment_id.replace(',', '')
