fbed4a9b0272b7bae0447409522e5784192f3cb5
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-09-17 13:04:55 +0545

    kickass parser updated

--- templates/kickass_template.py
+++ templates/kickass_template.py
@@ -22,6 +22,7 @@ class KickAssParser:
         self.uid_pattern = re.compile(
             r'UID-(\d+)\.html'
         )
+        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.files = self.get_filtered_files(files)
         self.folder_path = folder_path
         self.data_dic = OrderedDict()
@@ -141,7 +142,7 @@ class KickAssParser:
                     self.error_folder,
                     ex
                 )
-            except:
+            except Exception:
                 traceback.print_exc()
                 continue
 
@@ -158,16 +159,25 @@ class KickAssParser:
                 continue
             comment_text = self.get_post_text(comment_block)
             comment_date = self.get_date(comment_block)
+            avatar = self.get_avatar(comment_block)
             pid = self.thread_id
+            source = {
+                'forum': self.parser_name,
+                'pid': pid,
+                'message': comment_text.strip(),
+                'cid': commentID,
+                'author': user,
+            }
+            if comment_date:
+                source.update({
+                    'date': comment_date
+                })
+            if avatar:
+                source.update({
+                    'img': avatar
+                })
             comments.append({
-                
-                '_source': {
-                    'pid': pid,
-                    'date': comment_date,
-                    'message': comment_text.strip(),
-                    'cid': commentID,
-                    'author': user,
-                },
+                '_source': source,
             })
         return comments
 
@@ -175,6 +185,8 @@ class KickAssParser:
         try:
 
             # ---------------extract header data ------------
+            if not html_response:
+                return
             header = html_response.xpath(
                 '//div[contains(@class,"post classic")]'
             )
@@ -188,17 +200,26 @@ class KickAssParser:
             author_link = self.get_author_link(header[0])
             post_text = self.get_post_text(header[0])
             pid = self.thread_id
-            return {
-                
-                '_source': {
-                    'pid': pid,
-                    'subject': title,
-                    'date': date,
-                    'author': author,
-                    'message': post_text.strip(),
-                }
+            avatar = self.get_avatar(header[0])
+            source = {
+                'forum': self.parser_name,
+                'pid': pid,
+                'subject': title,
+                'author': author,
+                'message': post_text.strip(),
             }
-        except:
+            if date:
+                source.update({
+                   'date': date
+                })
+            if avatar:
+                source.update({
+                    'img': avatar
+                })
+            return {
+                '_source': source
+            }
+        except Exception:
             ex = traceback.format_exc()
             raise BrokenPage(ex)
 
@@ -213,7 +234,7 @@ class KickAssParser:
             pattern = '%Y-%m-%d %H:%M:%S'
             date = datetime.datetime.strptime(date, pattern).timestamp()
             return str(date)
-        except:
+        except Exception:
             return ""
 
     def get_author(self, tag):
@@ -241,13 +262,28 @@ class KickAssParser:
         return author_link
 
     def get_post_text(self, tag):
-        post_text = tag.xpath(
+        post_text_block = tag.xpath(
             'div[@class="post_content"]'
-            '/div[@class="post_body scaleimages"]'
+            '/div[@class="post_body scaleimages"]/descendant::text()'
+            '[not(ancestor::blockquote)]'
+
         )
-        post_text = post_text[0].xpath('string()')\
-            if post_text else ""
-        return post_text
+        post_text = " ".join([
+            post_text.strip() for post_text in post_text_block
+        ])
+        return post_text.strip()
+
+    def get_avatar(self, tag):
+        avatar_block = tag.xpath(
+            'div//div[@class="author_avatar"]'
+            '/a/img/@src'
+        )
+        if not avatar_block:
+            return ""
+        name_match = self.avatar_name_pattern.findall(avatar_block[0])
+        if not name_match:
+            return ""
+        return name_match[0] if 'svg' not in name_match[0] else ''
 
     def get_comment_id(self, tag):
         commentID = ""
