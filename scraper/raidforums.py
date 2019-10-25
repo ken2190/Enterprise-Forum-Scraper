16357cfb4a50e3503134c753668450c2a54e3419
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-10-25 14:40:13 +0545

    avatars added in raidforums.

--- scraper/raidforums.py
+++ scraper/raidforums.py
@@ -19,15 +19,17 @@ NO_OF_THREADS = 10
 class RaidForumsSpider(scrapy.Spider):
     name = 'raidforums_spider'
 
-    def __init__(self, output_path, useronly, update, db_path):
+    def __init__(self, output_path, useronly, update, db_path, avatar_path):
         self.base_url = "https://raidforums.com/"
         self.pagination_pattern = re.compile(r'.*page=(\d+)')
         self.username_pattern = re.compile(r'User-(.*)')
+        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.start_url = 'https://raidforums.com/'
         self.output_path = output_path
         self.useronly = useronly
         self.update = update
         self.db_path = db_path
+        self.avatar_path = avatar_path
         self.headers = {
             "user-agent": USER_AGENT
         }
@@ -70,6 +72,8 @@ class RaidForumsSpider(scrapy.Spider):
                 url = forum.xpath('@href').extract_first()
                 if self.base_url not in url:
                     url = self.base_url + url
+                # if 'Forum-General--187' not in url:
+                #     continue
                 yield Request(
                     url=url,
                     headers=self.headers,
@@ -217,6 +221,27 @@ class RaidForumsSpider(scrapy.Spider):
             f.write(response.text.encode('utf-8'))
             print(f'{topic_id}-{paginated_value} done..!')
 
+        avatars = response.xpath('//a[@class="post__user-avatar"]/img')
+        for avatar in avatars:
+            avatar_url = avatar.xpath('@src').extract_first()
+            if not avatar_url.startswith('http'):
+                avatar_url = self.base_url + avatar_url
+            name_match = self.avatar_name_pattern.findall(avatar_url)
+            if not name_match:
+                continue
+            name = name_match[0]
+            file_name = '{}/{}'.format(self.avatar_path, name)
+            if os.path.exists(file_name):
+                continue
+            yield Request(
+                url=avatar_url,
+                headers=self.headers,
+                callback=self.parse_avatar,
+                meta={
+                    'file_name': file_name,
+                }
+            )
+
         next_page = response.xpath(
             '//section[@id="thread-navigation"]//a[@class="pagination_next"]')
         if next_page:
@@ -230,6 +255,13 @@ class RaidForumsSpider(scrapy.Spider):
                 meta={'topic_id': topic_id}
             )
 
+    def parse_avatar(self, response):
+        file_name = response.meta['file_name']
+        file_name_only = file_name.rsplit('/', 1)[-1]
+        with open(file_name, 'wb') as f:
+            f.write(response.body)
+            print(f"Avatar {file_name_only} done..!")
+
 
 class RaidForumsScrapper():
     def __init__(self, kwargs):
@@ -238,6 +270,12 @@ class RaidForumsScrapper():
         self.update = kwargs.get('update')
         self.db_path = kwargs.get('db_path')
         self.proxy = kwargs.get('proxy') or None
+        self.ensure_avatar_path()
+
+    def ensure_avatar_path(self, ):
+        self.avatar_path = f'{self.output_path}/avatars'
+        if not os.path.exists(self.avatar_path):
+            os.makedirs(self.avatar_path)
 
     def do_scrape(self):
         settings = {
@@ -271,7 +309,8 @@ class RaidForumsScrapper():
             self.output_path,
             self.useronly,
             self.update,
-            self.db_path
+            self.db_path,
+            self.avatar_path
         )
         process.start()
         if self.update:
