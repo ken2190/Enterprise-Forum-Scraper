d74e9ce9cd8ec11d08815e0b236e1ec4d5eb2938
Umesh Pathak <umeshpd.pathak@gmail.com>
2019-11-12 13:51:33 +0545

    script to filter csv file based on some json field match

---
+++ tools/filter_csv_from_json_file_field.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+
+JSON_FILE = '/Users/umeshpathak/Desktop/sample.json'
+CSV_FILE = '/Users/umeshpathak/Desktop/sample.csv'
+OUTPUT_FILE = '/Users/umeshpathak/Desktop/output.csv'
+
+JSON_FIELD = 'email'
+CSV_COL = 'e'
+
+print('1. Reading Emails from JSON')
+emails = set()
+with open(JSON_FILE, 'r') as jsonfile:
+    for single_json in jsonfile:
+        json_response = json.loads(single_json)
+        if not json_response.get(JSON_FIELD):
+            continue
+        emails.add(json_response[JSON_FIELD])
+print('\tEmail read done\n')
+print('2. Reading csv with Pandas')
+df = pd.read_csv(CSV_FILE)
+print('\tCSV Read done\n')
+print('3. Filtering CSV w.r.t email')
+final_df = df[~df[CSV_COL].isin(list(emails))]
+print('\tFiltering Done\n')
+print('4. Saving Filtered CSV')
+final_df.to_csv(OUTPUT_FILE, index=False)
+print('\tOutput Saved')
+
+
+
+
