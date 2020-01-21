e088717bca962a9e52d8085d4d192ddf0b421044
largomath2010 <muoinguyen.idgv@gmail.com>
2020-01-22 01:44:03 +0700

    add back code login for exploit

--- scraper/bhf_io.py
+++ scraper/bhf_io.py
@@ -1,12 +1,5 @@
-import time
-import requests
 import os
 import re
-import scrapy
-import configparser
-
-from math import ceil
-from scrapy.crawler import CrawlerProcess
 from datetime import datetime
 
 from scrapy import (
@@ -18,15 +11,17 @@ from scraper.base_scrapper import (
     SiteMapScrapper
 )
 
-
-COOKIES = "__cfduid=d6ef4439eab55f1bd4b26ed82ddb494701579398531; _ym_d=1579406014; _ym_uid=1579406014689608580; xf_csrf=SnAVgm9lZet0wXWE; _ym_isad=2; _ym_visorc_57012907=w; xf_user=300107%2Cv_paBr7XBsjxh8X61CX5rERie483RAXlzQqq3cru; xf_session=bRCBmLoWGgNB2uV2uoWGfK-tlFRZr2xU; evercookie_etag=undefined; evercookie_cache=undefined"
+USERNAME = "thecreator101@protonmail.com"
+PASSWORD = "Night#Bhf01"
 
 
 class BHFIOSpider(SitemapSpider):
+
     name = 'bhfio_spider'
 
     # Url stuffs
     base_url = 'https://bhf.io'
+    login_url = 'https://bhf.io/login/login'
     start_urls = ["https://bhf.io/"]
     sitemap_url = "https://bhf.io/sitemap.php"
 
@@ -34,6 +29,11 @@ class BHFIOSpider(SitemapSpider):
     topic_pattern = re.compile(r'threads/(\d+)')
     avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
     pagination_pattern = re.compile(r'.*page-(\d+)')
+
+    # Css stuffs
+    login_form_css = "form[action]"
+    backup_code_css = "a[href*=\"provider=backup\"]::attr(href)"
+    account_css = r'a[href="/account/"]'
 
     # Xpath stuffs
     forum_xpath = "//sitemap/loc/text()"
@@ -60,11 +60,138 @@ class BHFIOSpider(SitemapSpider):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Update headers
         self.headers.update(
             {
                 'referer': 'https://bhf.io/',
             }
         )
+
+        # Load backup codes
+        self.backup_code_file = os.path.join(
+            os.getcwd(),
+            "code/%s" % self.name
+        )
+        with open(
+            file=self.backup_code_file,
+            mode="r",
+            encoding="utf-8"
+        ) as file:
+            self.backup_codes = [
+                code.strip() for code in file.read().split("\n")
+            ]
+
+    def write_backup_codes(self):
+        with open(
+            file=self.backup_code_file,
+            mode="w+",
+            encoding="utf-8"
+        ) as file:
+            file.write(
+                "\n".join(self.backup_codes)
+            )
+
+    def start_requests(self):
+        yield Request(
+            url=self.login_url,
+            dont_filter=True,
+            headers=self.headers,
+            callback=self.parse_login
+        )
+
+    def parse_login(self, response):
+        yield FormRequest.from_response(
+            response=response,
+            formcss=self.login_form_css,
+            formdata={
+                "login": USERNAME,
+                "password": PASSWORD
+            },
+            dont_filter=True,
+            callback=self.parse_post_login
+        )
+
+    def parse_post_login(self, response):
+        backup_code_url = "%s%s" % (
+            self.base_url,
+            response.css(self.backup_code_css).extract_first()
+        )
+        yield Request(
+            url=backup_code_url,
+            headers=self.headers,
+            dont_filter=True,
+            callback=self.parse_backup_code
+        )
+
+    def parse_backup_code(self, response):
+
+        # Load backup code
+        code = self.backup_codes[0]
+        self.backup_codes = self.backup_codes[1:]
+
+        yield FormRequest.from_response(
+            response=response,
+            formcss=self.login_form_css,
+            formdata={
+                "code": code.replace(" ", ""),
+                "trust": "0",
+                "remember": "0"
+            },
+            meta={
+                "backup_code_url": response.request.url,
+                "code": code
+            },
+            dont_filter=True,
+            callback=self.parse_post_backup_code
+        )
+
+    def parse_post_backup_code(self, response):
+
+        # Load backup code
+        backup_code_url = response.meta.get("backup_code_url")
+
+        # Load code
+        code = response.meta.get("code")
+
+        # Load account
+        account = response.css(self.account_css).extract_first()
+
+        # If not account and no more backup codes, return
+        if not account and not self.backup_codes:
+            self.logger.info(
+                "None of backup code work."
+            )
+            self.write_backup_codes()
+            return
+
+        # If not account, try other code
+        if not account:
+            self.logger.info(
+                "Code %s failed." % code
+            )
+
+            with open(
+                file="%s.html" % code,
+                mode="w+",
+                encoding="utf-8"
+            ) as file:
+                file.write(response.text)
+
+            yield Request(
+                url=backup_code_url,
+                headers=self.headers,
+                dont_filter=True,
+                callback=self.parse_backup_code
+            )
+            return
+
+        # If account, success
+        self.logger.info(
+            "Code %s success." % code
+        )
+        yield from super().start_requests()
+        self.write_backup_codes()
 
     def get_topic_id(self, url=None):
         topic_id = self.topic_pattern.findall(url)
@@ -208,11 +335,6 @@ class BHFIOScrapper(SiteMapScrapper):
                 'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads
             }
         )
-
-    def load_spider_kwargs(self):
-        spider_kwargs = super().load_spider_kwargs()
-        spider_kwargs["cookies"] = COOKIES
-        return spider_kwargs
 
 
 if __name__ == '__main__':
