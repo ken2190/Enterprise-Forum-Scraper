049f6d3eb9a41da0927c400f6da611415f9bfb9e
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-01-30 15:29:06 +0545

    prologic sitemap added.

--- scraper/prologic.py
+++ scraper/prologic.py
@@ -7,7 +7,9 @@ import configparser
 from lxml.html import fromstring
 from urllib.parse import urlencode
 from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
+from scrapy import Selector
+from scrapy.utils.gz import gunzip
+from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 USER = 'vrx9'
 PASS = 'Night#Pro000'
@@ -16,17 +18,23 @@ REQUEST_DELAY = 1
 NO_OF_THREADS = 10
 
 
-class ProLogicSpider(scrapy.Spider):
+class ProLogicSpider(SitemapSpider):
     name = 'prologic_spider'
+    sitemap_url = 'https://prologic.su/sitemap.xml'
+    # Xpath stuffs
+    forum_xpath = '//sitemap/loc[contains(text(), "sitemap_topics.xml")]/text()'
+    thread_xpath = '//url[loc[contains(text(),"/topic/")] and lastmod]'
+    thread_url_xpath = "//loc/text()"
+    thread_date_xpath = "//lastmod/text()"
+    sitemap_datetime_format = "%Y-%m-%d"
 
-    def __init__(self, output_path, avatar_path):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.base_url = "https://prologic.su/"
         self.topic_pattern = re.compile(r'/topic/(\d+)-')
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.pagination_pattern = re.compile(r'.*page__st__(\d+)$')
         self.start_url = 'https://prologic.su/'
-        self.output_path = output_path
-        self.avatar_path = avatar_path
         self.headers = {
             "user-agent": USER_AGENT,
             'sec-fetch-mode': 'navigate',
@@ -52,9 +60,10 @@ class ProLogicSpider(scrapy.Spider):
             'ips_password': PASS,
             "rememberMe": '1',
         }
-        url = 'https://prologic.su/index.php?app=core&module=global&section=login&do=process'
+        login_url = 'https://prologic.su/index.php?app=core&module=global'\
+                    '&section=login&do=process'
         yield FormRequest(
-            url=url,
+            url=login_url,
             callback=self.parse_main_page,
             formdata=params,
             headers=self.headers,
@@ -62,25 +71,43 @@ class ProLogicSpider(scrapy.Spider):
             )
 
     def parse_main_page(self, response):
-        forums = response.xpath(
-            '//h4[@class="forum_name"]/strong/a')
-        subforums = response.xpath(
-            '//ol[contains(@id, "subforums_")]/li/a')
-        forums.extend(subforums)
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            # if not url.endswith('2-microsoft-windows/'):
-            #     continue
-            yield Request(
-                url=url,
+        if self.start_date:
+            yield scrapy.Request(
+                url=self.sitemap_url,
                 headers=self.headers,
-                callback=self.parse_forum
+                callback=self.parse_sitemap
             )
+        else:
+            forums = response.xpath(
+                '//h4[@class="forum_name"]/strong/a')
+            subforums = response.xpath(
+                '//ol[contains(@id, "subforums_")]/li/a')
+            forums.extend(subforums)
+            for forum in forums:
+                url = forum.xpath('@href').extract_first()
+                if self.base_url not in url:
+                    url = self.base_url + url
+                # if not url.endswith('2-microsoft-windows/'):
+                #     continue
+                yield Request(
+                    url=url,
+                    headers=self.headers,
+                    callback=self.parse_forum
+                )
+
+    def parse_sitemap_forum(self, response):
+
+        # Load selector
+        text = gunzip(response.body)
+        selector = Selector(text=gunzip(response.body))
+
+        # Load thread
+        all_threads = selector.xpath(self.thread_xpath).extract()
+        for thread in all_threads:
+            yield from self.parse_sitemap_thread(thread)
 
     def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
+        self.logger.info('next_page_url: {}'.format(response.url))
         threads = response.xpath(
             '//a[contains(@id, "tid-link-")]')
         for thread in threads:
@@ -120,7 +147,7 @@ class ProLogicSpider(scrapy.Spider):
             self.output_path, topic_id, paginated_value)
         with open(file_name, 'wb') as f:
             f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
+            self.logger.info(f'{topic_id}-{paginated_value} done..!')
 
         avatars = response.xpath('//li[@class="avatar"]/a/img')
         for avatar in avatars:
@@ -160,35 +187,20 @@ class ProLogicSpider(scrapy.Spider):
         file_name_only = file_name.rsplit('/', 1)[-1]
         with open(file_name, 'wb') as f:
             f.write(response.body)
-            print(f"Avatar {file_name_only} done..!")
+            self.logger.info(f"Avatar {file_name_only} done..!")
 
 
-class ProLogicScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.ensure_avatar_path()
+class ProLogicScrapper(SiteMapScrapper):
 
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
+    spider_class = ProLogicSpider
 
-    def do_scrape(self):
-        settings = {
-            "DOWNLOADER_MIDDLEWARES": {
-                # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-                'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                # 'scrapy.downloadermiddlewares.defaultheaders.DefaultHeadersMiddleware': None
-            },
-            'DOWNLOAD_DELAY': REQUEST_DELAY,
-            'CONCURRENT_REQUESTS': NO_OF_THREADS,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        process = CrawlerProcess(settings)
-        process.crawl(ProLogicSpider, self.output_path, self.avatar_path)
-        process.start()
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update(
+            {
+                'DOWNLOAD_DELAY': REQUEST_DELAY,
+                'CONCURRENT_REQUESTS': NO_OF_THREADS,
+                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+            }
+        )
+        return settings
