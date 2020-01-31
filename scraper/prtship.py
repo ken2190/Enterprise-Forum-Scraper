0bc3e1b7e4b45798c705ab8f3cc6fe95c4bdc577
Vinny Troia <vinny@nightlionsecurity.com>
2020-01-30 18:14:28 -0600

    trying to do the prtship.com update myself
    trying to do the prtship.com update myself

--- scraper/prtship.py
+++ scraper/prtship.py
@@ -9,10 +9,19 @@ from scrapy.http import Request, FormRequest
 from scrapy.crawler import CrawlerProcess
 
 
-class PrtShipSpider(scrapy.Spider):
+class PrtShipSpider(SitemapSpider):
     name = 'prtship_spider'
+    sitemap_url = 'https://prtship.com/sitemap.xml'
+    # Xpath stuffs
+    forum_xpath = '//sitemap/loc[contains(text(), "sitemap_topics.xml")]/text()'
+    thread_xpath = '//url[loc[contains(text(),"/topic/")] and lastmod]'
+    thread_url_xpath = "//loc/text()"
+    thread_date_xpath = "//lastmod/text()"
+    sitemap_datetime_format = "%Y-%m-%d"
 
-    def __init__(self, output_path):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.base_url = "https://prtship.com"
         self.start_url = '{}/forums/'.format(self.base_url)
         self.topic_pattern = re.compile(r'.*\.(\d+)/$')
@@ -49,24 +58,27 @@ class PrtShipSpider(scrapy.Spider):
             headers=self.headers
             )
 
+    def parse_sitemap_forum(self, response):
+
+        # Load selector
+        text = gunzip(response.body)
+        selector = Selector(text=gunzip(response.body))
+
+        # Load thread
+        all_threads = selector.xpath(self.thread_xpath).extract()
+        for thread in all_threads:
+            yield from self.parse_sitemap_thread(thread)
+
     def parse_main_page(self, response):
-        json_response = json.loads(response.text)
-        html_response = json_response['html']['content']
-        response = fromstring(html_response)
-        forums = response.xpath(
-            '//h3[@class="node-title"]/a')
-        for forum in forums:
-            url = forum.xpath('@href')[0]
-            if self.base_url not in url:
-                url = self.base_url + url
-            yield Request(
-                url=url,
+        if self.start_date:
+            yield scrapy.Request(
+                url=self.sitemap_url,
                 headers=self.headers,
-                callback=self.parse_forum
+                callback=self.parse_sitemap
             )
 
     def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
+        self.logger.info('next_page_url: {}'.format(response.url))
         threads = response.xpath(
             '//a[@data-preview-url]')
         for thread in threads:
@@ -106,7 +118,7 @@ class PrtShipSpider(scrapy.Spider):
             self.output_path, topic_id, paginated_value)
         with open(file_name, 'wb') as f:
             f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
+            self.logger.info(f'{topic_id}-{paginated_value} done..!')
 
         next_page = response.xpath(
             '//a[@class="pageNav-jump pageNav-jump--next"]')
@@ -121,39 +133,25 @@ class PrtShipSpider(scrapy.Spider):
                 meta={'topic_id': topic_id}
             )
 
+    def parse_avatar(self, response):
+        file_name = response.meta['file_name']
+        file_name_only = file_name.rsplit('/', 1)[-1]
+        with open(file_name, 'wb') as f:
+            f.write(response.body)
+            self.logger.info(f"Avatar {file_name_only} done..!")
 
-class PrtShipScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.request_delay = 0.1
-        self.no_of_threads = 16
 
-    def do_scrape(self):
-        settings = {
-            'DOWNLOAD_DELAY': self.request_delay,
-            'CONCURRENT_REQUESTS': self.no_of_threads,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
+class PrtShipScrapper(SiteMapScrapper):
 
-        }
-        if self.proxy:
-            settings.update({
-                "DOWNLOADER_MIDDLEWARES": {
-                    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-                    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
-                    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-                    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-                },
-                'ROTATING_PROXY_LIST': self.proxy,
+    spider_class = ProLogicSpider
 
-            })
-        process = CrawlerProcess(settings)
-        process.crawl(PrtShipSpider, self.output_path)
-        process.start()
-
-if __name__ == '__main__':
-    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update(
+            {
+                'DOWNLOAD_DELAY': REQUEST_DELAY,
+                'CONCURRENT_REQUESTS': NO_OF_THREADS,
+                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+            }
+        )
+        return settings
