9015560dc394f142f95b99aee04cee2e91c25563
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-02-04 16:23:36 +0545

    antichat update module different that sitemap

--- scraper/antichat.py
+++ scraper/antichat.py
@@ -3,13 +3,14 @@ import os
 import re
 import json
 import scrapy
+from glob import glob
 from math import ceil
 import configparser
 from lxml.html import fromstring
 from scrapy.http import Request, FormRequest
 from scrapy.crawler import CrawlerProcess
 from datetime import datetime
-from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
+from scraper.base_scrapper import BypassCloudfareSpider, SiteMapScrapper
 
 
 USER = 'blacklotus2000@protonmail.com'
@@ -19,15 +20,10 @@ NO_OF_THREADS = 5
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 
 
-class AntichatSpider(SitemapSpider):
+class AntichatSpider(BypassCloudfareSpider):
     name = 'antichat_spider'
-    sitemap_url = 'https://forum.antichat.ru/sitemap.php'
-    # Xpath stuffs
-    forum_xpath = "//loc/text()"
-    thread_url_xpath = "//loc/text()"
-    thread_xpath = "//url[lastmod]"
-    thread_date_xpath = "//lastmod/text()"
-    sitemap_datetime_format = '%Y-%m-%dT%H:%M:%S'
+    new_posts_url = 'https://forum.antichat.ru/find-new/posts'
+    thread_date_format = '%d %b %Y at %H:%M %p'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -45,11 +41,91 @@ class AntichatSpider(SitemapSpider):
             'user-agent': USER_AGENT,
         }
 
-    def parse_thread_date(self, thread_date):
-        return datetime.strptime(
-            thread_date.split('+')[0],
-            self.sitemap_datetime_format
-        )
+    def start_requests(self):
+        if self.start_date:
+            yield scrapy.Request(
+                url=self.new_posts_url,
+                headers=self.headers,
+                callback=self.parse_new_posts,
+            )
+        else:
+            yield Request(
+                url=self.base_url,
+                headers=self.headers,
+                dont_filter=True,
+                callback=self.parse,
+            )
+
+    def parse_new_posts(self, response):
+        topic_blocks = response.xpath(
+            '//ol[@class="discussionListItems"]/li[@id]')
+        if not topic_blocks:
+            topic_blocks = response.xpath(
+                '//div[@class="uix_stickyThreads"]/li')
+        for topic in topic_blocks:
+            # Get thread url
+            thread_url = topic.xpath(
+                './/a[@data-previewurl]/@href').extract_first()
+            if self.base_url not in thread_url:
+                thread_url = f'{self.base_url}{thread_url}'
+
+            # Get topic id
+            match = self.topic_pattern.findall(thread_url)
+            if not match:
+                continue
+            topic_id = match[0]
+
+            # Check thread update time
+            thread_date = self.get_thread_date(topic)
+            if thread_date < self.start_date:
+                self.logger.info(
+                    f"TopicId {topic_id} ignored "
+                    f"because of older udpate: {thread_date}")
+                continue
+
+            # Check existing saved paginated files and get  max paginated value
+            max_pagination = self.get_latest_pagination(topic_id)
+            self.logger.info(
+                f"Started downloading of TopicId {topic_id} from "
+                f"pagination {max_pagination}")
+            thread_url = f'{thread_url}page-{max_pagination}'
+            yield Request(
+                url=thread_url,
+                headers=self.headers,
+                callback=self.parse_thread,
+                meta={'topic_id': topic_id}
+            )
+        next_page = response.xpath(
+            '//a[@class="text" and text()="Next >"]'
+            )
+        if next_page:
+            next_page_url = next_page.xpath('@href').extract_first()
+            if self.base_url not in next_page_url:
+                next_page_url = self.base_url + next_page_url
+            yield Request(
+                url=next_page_url,
+                headers=self.headers,
+                callback=self.parse_new_posts,
+            )
+
+    def get_thread_date(self, tag):
+        updated_ts = tag.xpath(
+            './/dl[@class="lastPostInfo"]'
+            '//abbr[@class="DateTime"]/@data-time').extract_first()
+        if updated_ts:
+            return datetime.fromtimestamp(int(updated_ts))
+        updated_dt_value = tag.xpath(
+            './/dl[@class="lastPostInfo"]'
+            '//span[@class="DateTime"]/@title').extract_first()
+        return datetime.strptime(updated_dt_value, self.thread_date_format)
+
+    def get_latest_pagination(self, topic_id):
+        pagination_pattern = re.compile(r'\d+-(\d+)')
+        existing_paginations = [
+            int(pagination_pattern.search(x).group(1))
+            for x in glob(f'{self.output_path}/{topic_id}-*.html')
+        ]
+        return max(existing_paginations) if existing_paginations else 1
 
     def proceed_for_login(self):
         login_url = 'https://forum.antichat.ru/login/login'
