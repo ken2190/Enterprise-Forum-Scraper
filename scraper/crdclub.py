5bf088247718b3599be36c2948db1b56146b6032
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-02-09 16:14:59 +0545

    update module for crdclub

--- scraper/crdclub.py
+++ scraper/crdclub.py
@@ -1,35 +1,78 @@
-import time
-import requests
 import os
 import re
-import scrapy
-from math import ceil
-import configparser
-from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
+
+from datetime import (
+    datetime,
+    timedelta
+)
+from scrapy import (
+    Request,
+    FormRequest
+)
+from scraper.base_scrapper import (
+    SitemapSpider,
+    SiteMapScrapper
+)
+
+REQUEST_DELAY = 0.5
+NO_OF_THREADS = 5
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
+class CrdClubSpider(SitemapSpider):
+    name = "crdclub_spider"
 
+    # Url stuffs
+    base_url = "http://crdclub.su/"
 
-class CrdClubSpider(scrapy.Spider):
-    name = 'crdclub_spider'
+    # Regex stuffs
+    topic_pattern = re.compile(
+        r't=(\d+)',
+        re.IGNORECASE
+    )
+    avatar_name_pattern = re.compile(
+        r'.*u=(\w+)\&',
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r'.*page=(\d+)',
+        re.IGNORECASE
+    )
 
-    def __init__(self, output_path, avatar_path):
-        self.base_url = "http://crdclub.cc/"
-        self.topic_pattern = re.compile(r't=(\d+)')
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page=(\d+)')
-        self.start_url = 'http://crdclub.cc/index.php'
-        self.output_path = output_path
-        self.avatar_path = avatar_path
-        self.headers = {
-            'referer': 'http://crdclub.cc/',
+    # Xpath stuffs
+    forum_xpath = '//a[contains(@href, "forumdisplay.php?")]/@href'
+    thread_xpath = '//tr[td[contains(@id, "td_threadtitle_")]]'
+    thread_first_page_xpath = '//td[contains(@id, "td_threadtitle_")]/div'\
+                              '/a[contains(@href, "showthread.php?")]/@href'
+    thread_last_page_xpath = '//td[contains(@id, "td_threadtitle_")]/div/span'\
+                             '/a[contains(@href, "showthread.php?")]'\
+                             '[last()]/@href'
+    thread_date_xpath = '//span[@class="time"]/preceding-sibling::text()'
+
+    pagination_xpath = '//a[@rel="next"]/@href'
+    thread_pagination_xpath = '//a[@rel="prev"]/@href'
+    thread_page_xpath = '//div[@class="pagenav"]//span/strong/text()'
+    post_date_xpath = '//table[contains(@id, "post")]//td[@class="thead"][1]/text()'
+    avatar_xpath = '//a[contains(@href, "member.php?")]/img/@src'
+
+    # Other settings
+    use_proxy = False
+    sitemap_datetime_format = '%d-%m-%Y'
+    post_datetime_format = '%d-%m-%Y, %H:%M'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.headers.update({
+            'referer': 'http://crdclub.su/',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
             'user-agent': USER_AGENT,
-        }
+        })
 
     def start_requests(self):
-        login_url = 'http://crdclub.cc/login.php?do=login'
+
+        login_url = 'https://crdclub.su/login.php?do=login'
         formdata = {
             'vb_login_username': 'darkcylon',
             'cookieuser': '1',
@@ -44,146 +87,113 @@ class CrdClubSpider(scrapy.Spider):
             url=login_url,
             formdata=formdata,
             headers=self.headers,
-            callback=self.after_login
+            callback=self.after_login,
         )
 
     def after_login(self, response):
+        # Synchronize user agent in cloudfare middleware
+        self.synchronize_headers(response)
         yield Request(
-            url=self.start_url,
+            url=self.base_url,
             headers=self.headers,
-            callback=self.parse
+            callback=self.parse,
+            meta=self.synchronize_meta(response),
         )
 
+    def parse_thread_date(self, thread_date):
+        """
+        :param thread_date: str => thread date as string
+        :return: datetime => thread date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        thread_date = thread_date.strip()
+        if "today" in thread_date.lower():
+            return datetime.today()
+        elif "yesterday" in thread_date.lower():
+            return datetime.today() - timedelta(days=1)
+        else:
+            return datetime.strptime(
+                thread_date,
+                self.sitemap_datetime_format
+            )
+
+    def parse_post_date(self, post_date):
+        """
+        :param post_date: str => post date as string
+        :return: datetime => post date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        post_date = post_date.strip()
+        if not post_date:
+            return
+
+        if "today" in post_date.lower():
+            return datetime.today()
+        elif "yesterday" in post_date.lower():
+            return datetime.today() - timedelta(days=1)
+        else:
+            return datetime.strptime(
+                post_date,
+                self.post_datetime_format
+            )
+
     def parse(self, response):
-        forums = response.xpath(
-            '//a[contains(@href, "forumdisplay.php?f=")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            # if not url.endswith('f=75'):
-            #     continue
-            if self.base_url not in url:
-                url = self.base_url + url
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
 
-    def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//td[contains(@id, "td_threadtitle_")]'
-            '/div/a[contains(@href, "showthread.php?")]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id[0]}
-            )
+        # Synchronize cloudfare user agent
+        self.synchronize_headers(response)
 
-        next_page = response.xpath('//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
+
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
+
             yield Request(
-                url=next_page_url,
+                url=forum_url,
                 headers=self.headers,
-                callback=self.parse_forum
+                callback=self.parse_forum,
+                meta=self.synchronize_meta(response)
             )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath('//a[contains(@href, "member.php?")]/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if self.base_url not in avatar_url:
-                avatar_url = self.base_url + avatar_url
-            user_id = avatar.xpath('@alt').re(r'(\w+)\'s')
-            if not user_id:
-                continue
-            file_name = '{}/{}.jpg'.format(self.avatar_path, user_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                    'user_id': user_id[0]
-                }
+        # Parse generic thread
+        yield from super().parse_thread(response)
+
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
+
+    def get_avatar_file(self, url=None):
+        """
+        :param url: str => avatar url
+        :return: str => extracted avatar file from avatar url
+        """
+
+        try:
+            file_name = os.path.join(
+                self.avatar_path,
+                self.avatar_name_pattern.findall(url)[0]
             )
-
-        next_page = response.xpath('//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar for user {response.meta['user_id']} done..!")
+            return f'{file_name}.jpg'
+        except Exception as err:
+            return
 
 
-class CrdClubScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.request_delay = 0.1
-        self.no_of_threads = 16
-        self.ensure_avatar_path()
+class CrdClubScrapper(SiteMapScrapper):
 
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
+    spider_class = CrdClubSpider
+    site_name = 'crdclub.su'
 
-    def do_scrape(self):
-        settings = {
-            "DOWNLOADER_MIDDLEWARES": {
-                'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-                'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                'scrapy.downloadermiddlewares.defaultheaders.DefaultHeadersMiddleware': None
-            },
-            'DOWNLOAD_DELAY': self.request_delay,
-            'CONCURRENT_REQUESTS': self.no_of_threads,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        process = CrawlerProcess(settings)
-        process.crawl(CrdClubSpider, self.output_path, self.avatar_path)
-        process.start()
-
-
-if __name__ == '__main__':
-    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update(
+            {
+                'DOWNLOAD_DELAY': REQUEST_DELAY,
+                'CONCURRENT_REQUESTS': NO_OF_THREADS,
+                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+            }
+        )
+        return settings
