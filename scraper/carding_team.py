f3f95208d195fd4b3a2fee77de54b5e35af01b6d
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-02-11 18:26:24 +0545

    carding_team scraper

--- scraper/carding_team.py
+++ scraper/carding_team.py
@@ -1,3 +1,4 @@
+import time
 import os
 import re
 import scrapy
@@ -5,184 +6,139 @@ from math import ceil
 import configparser
 from scrapy.http import Request, FormRequest
 from scrapy.crawler import CrawlerProcess
-from scraper.base_scrapper import BypassCloudfareSpider
+from selenium import webdriver
+from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 REQUEST_DELAY = 0.5
 NO_OF_THREADS = 5
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 
 
-class CardingTeamSpider(BypassCloudfareSpider):
+class CardingTeamSpider(SitemapSpider):
     name = 'cardingteam_spider'
+    base_url = "https://cardingteam.cc/"
 
-    def __init__(self, output_path, avatar_path):
-        self.base_url = "https://cardingteam.cc/"
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page=(\d+)')
-        self.start_url = self.base_url
-        self.output_path = output_path
-        self.avatar_path = avatar_path
-        self.headers = {
+    # Sitemap Stuffs
+    sitemap_url = 'https://cardingteam.cc/sitemap-index.xml'
+    forum_sitemap_xpath = '//url[loc[contains(text(),"sitemap-threads.xml")] and lastmod]'
+    thread_sitemap_xpath = '//url[loc[contains(text(),"Thread-")] and lastmod]'
+    thread_url_xpath = '//loc/text()'
+    thread_lastmod_xpath = "//lastmod/text()"
+
+    # Xpaths
+    forum_xpath = '//a[contains(@href, "Forum-")]/@href'
+    pagination_xpath = '//div[@class="pagination"]'\
+                       '/a[@class="pagination_next"]/@href'
+    thread_xpath = '//tr[@class="inline_row"]'
+    thread_first_page_xpath = '//span[contains(@id,"tid_")]/a/@href'
+    thread_last_page_xpath = '//td[contains(@class,"forumdisplay_")]/div'\
+                             '/span/span[contains(@class,"smalltext")]'\
+                             '/a[last()]/@href'
+    thread_date_xpath = '//td[contains(@class,"forumdisplay")]'\
+                        '/span[@class="lastpost smalltext"]/text()[1]|'\
+                        '//td[contains(@class,"forumdisplay")]'\
+                        '/span[@class="lastpost smalltext"]/span/@title'
+    thread_pagination_xpath = '//div[@class="pagination"]'\
+                              '//a[@class="pagination_previous"]/@href'
+    thread_page_xpath = '//span[@class="pagination_current"]/text()'
+    post_date_xpath = '//span[@class="post_date2"]/text()[1]|'\
+                      '//span[@class="post_date2"]/span/@title'\
+
+    avatar_xpath = '//div[@class="author_avatar"]/a/img/@src'
+
+    # Regex stuffs
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r".*page=(\d+)",
+        re.IGNORECASE
+    )
+
+    # custom settings
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None
+        }
+    }
+
+    def get_cookies(self,):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument(f'user-agent={USER_AGENT}')
+        browser = webdriver.Chrome(
+            '/usr/local/bin/chromedriver',
+            chrome_options=chrome_options)
+        browser.get(self.base_url)
+        time.sleep(1)
+        cookies = browser.get_cookies()
+        return '; '.join([
+            f"{c['name']}={c['value']}" for c in cookies
+        ])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cookies = self.get_cookies()
+        self.headers.update({
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'same-origin',
             'sec-fetch-user': '?1',
-            'user-agent': self.custom_settings.get("DEFAULT_REQUEST_HEADERS"),
-        }
+            'user-agent': USER_AGENT,
+            'cookie': cookies
 
-    def start_requests(self):
-        yield Request(
-            url=self.start_url,
-            headers=self.headers,
-            callback=self.parse,
-            dont_filter=True,
-        )
+        })
+
+    def parse_thread_date(self, thread_date):
+        return datetime.datetime.today()
+
+    def parse_post_date(self, thread_date):
+        return datetime.datetime.today()
 
     def parse(self, response):
-        forums = response.xpath(
-            '//strong/a[contains(@href, "Forum-")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            # if 'Forum-Private-Tutorials-and-Utilities' not in url:
-            #     continue
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
+        # Synchronize cloudfare user agent
+        self.synchronize_headers(response)
+        print(self.headers)
+        print(response.text)
 
-    def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//span[contains(@class, "subject_") and contains(@id, "tid_")]/a')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = str(
-                int.from_bytes(
-                    thread_url.replace('Thread-', '').encode('utf-8'), byteorder='big'
-                ) % (10 ** 7)
-            )
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
 
-        next_page = response.xpath(
-            '//a[@class="pagination_next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
+            print(forum_url)
+            continue
             yield Request(
-                url=next_page_url,
+                url=forum_url,
                 headers=self.headers,
-                callback=self.parse_forum
+                callback=self.parse_forum,
+                meta=self.synchronize_meta(response),
             )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath(
-            '//div[@class="author_avatar"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if 'svg+xml' in avatar_url:
-                continue
-            if not avatar_url.startswith('http'):
-                avatar_url = self.base_url + avatar_url
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
+        # Parse generic thread
+        yield from super().parse_thread(response)
 
-        next_page = response.xpath(
-            '//div[@class="pagination"]'
-            '/a[@class="pagination_next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar {file_name_only} done..!")
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
 
 
-class CardingTeamScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.ensure_avatar_path()
+class CardingTeamScrapper(SiteMapScrapper):
 
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
+    spider_class = CardingTeamSpider
+    site_name = 'cardingteam.cc'
 
-    def do_scrape(self):
-        settings = {
-            "DOWNLOADER_MIDDLEWARES": {
-                'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-            },
-            'DOWNLOAD_DELAY': REQUEST_DELAY,
-            'CONCURRENT_REQUESTS': NO_OF_THREADS,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503, 520],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        if self.proxy:
-            settings.update({
-                "DOWNLOADER_MIDDLEWARES": {
-                    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
-                    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-                    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-                },
-                'ROTATING_PROXY_LIST': self.proxy,
-
-            })
-        process = CrawlerProcess(settings)
-        process.crawl(CardingTeamSpider, self.output_path, self.avatar_path)
-        process.start()
-
-
-if __name__ == '__main__':
-    kwargs = {
-        'output': '/root/cardingteam'
-    }
-    CardingTeamScrapper(kwargs).do_scrape()
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update(
+            {
+                'DOWNLOAD_DELAY': REQUEST_DELAY,
+                'CONCURRENT_REQUESTS': NO_OF_THREADS,
+                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+                "RETRY_HTTP_CODES": [406, 429, 500, 503],
+            }
+        )
+        return settings
