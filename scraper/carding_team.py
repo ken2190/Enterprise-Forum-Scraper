3c4ae1aa1bd0ddb1a39144fd7f0d06d9d976551e
largomath2010 <muoinguyen.idgv@gmail.com>
2020-02-13 14:59:47 +0700

    fix carding team cookie stuffs

--- scraper/carding_team.py
+++ scraper/carding_team.py
@@ -20,6 +20,7 @@ class CardingTeamSpider(SitemapSpider):
 
     # Sitemap Stuffs
     sitemap_url = 'https://cardingteam.cc/sitemap-index.xml'
+
     forum_sitemap_xpath = '//url[loc[contains(text(),"sitemap-threads.xml")] and lastmod]'
     thread_sitemap_xpath = '//url[loc[contains(text(),"Thread-")] and lastmod]'
     thread_url_xpath = '//loc/text()'
@@ -64,30 +65,43 @@ class CardingTeamSpider(SitemapSpider):
     }
 
     def get_cookies(self,):
+        # Init chrome options
         chrome_options = webdriver.ChromeOptions()
+
+        # Init chrome arguments
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument(f'user-agent={USER_AGENT}')
+
+        # Load chrome driver
         browser = webdriver.Chrome(
             '/usr/local/bin/chromedriver',
-            chrome_options=chrome_options)
-        browser.get(self.base_url)
+            chrome_options=chrome_options
+        )
+
+        # Load target site
+        if self.start_date:
+            browser.get(self.sitemap_url)
+        else:
+            browser.get(self.base_url)
+
         time.sleep(1)
         cookies = browser.get_cookies()
         return '; '.join([
             f"{c['name']}={c['value']}" for c in cookies
+            if c.get("name") == "vDDoS"
         ])
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        cookies = self.get_cookies()
-        self.headers.update({
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'user-agent': USER_AGENT,
-            'cookie': cookies
 
+        # Init cookies
+        cookies = self.get_cookies()
+
+        # Update cookie headers
+        self.headers.update({
+            'User-Agent': USER_AGENT,
+            'Cookie': cookies
         })
 
     def parse_thread_date(self, thread_date):
@@ -99,8 +113,6 @@ class CardingTeamSpider(SitemapSpider):
     def parse(self, response):
         # Synchronize cloudfare user agent
         self.synchronize_headers(response)
-        print(self.headers)
-        print(response.text)
 
         all_forums = response.xpath(self.forum_xpath).extract()
         for forum_url in all_forums:
@@ -108,8 +120,7 @@ class CardingTeamSpider(SitemapSpider):
             # Standardize url
             if self.base_url not in forum_url:
                 forum_url = self.base_url + forum_url
-            print(forum_url)
-            continue
+
             yield Request(
                 url=forum_url,
                 headers=self.headers,
