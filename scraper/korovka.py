44459ed9b066a5ecb5542a2cbc4e720c871f25bc
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-02-21 11:48:23 +0545

    korovka changes

--- scraper/korovka.py
+++ scraper/korovka.py
@@ -1,75 +1,130 @@
-import time
-import requests
 import os
 import re
+import sys
 import scrapy
+import hashlib
 from math import ceil
 import configparser
-import hashlib
-from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
+from scrapy import (
+    Request,
+    FormRequest
+)
 
+from datetime import datetime, timedelta
+
+from scraper.base_scrapper import (
+    SitemapSpider,
+    SiteMapScrapper
+)
+
+
+REQUEST_DELAY = 1.5
+NO_OF_THREADS = 1
 
 CODE = 'shithead'
-USER = "Sandman011"
-PASS = '12312312232312111'
+USER = "cyrax11"
+PASS = 'Night#Pass6676'
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
 
 PROXY = 'http://127.0.0.1:8118'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
 
 
-class KorovkaSpider(scrapy.Spider):
+class KorovkaSpider(SitemapSpider):
+
     name = 'korovka_spider'
 
-    def __init__(self, output_path, avatar_path, proxy):
-        self.base_url = "http://korovka32xc3t5cg.onion/"
-        self.topic_pattern = re.compile(r't=(\d+)')
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page=(\d+)')
-        self.output_path = output_path
-        self.avatar_path = avatar_path
-        self.proxy = proxy
-        self.headers = {
-            'Accept': 'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded',
+    # Url stuffs
+    base_url = "http://korovka32xc3t5cg.onion/"
+    login_url = "http://korovka32xc3t5cg.onion/login.php?do=login"
+
+    # Xpath stuffs
+    forum_xpath = '//a[contains(@href, "forumdisplay.php?f=")]/@href'
+    thread_xpath = '//tr[td[contains(@id, "td_threadtitle_")]]'
+    thread_first_page_xpath = '//td[contains(@id, "td_threadtitle_")]/div'\
+                              '/a[contains(@href, "showthread.php?")]/@href'
+    thread_last_page_xpath = '//td[contains(@id, "td_threadtitle_")]/div/span'\
+                             '/a[contains(@href, "showthread.php?")]'\
+                             '[last()]/@href'
+    thread_date_xpath = '//span[@class="time"]/preceding-sibling::text()'
+
+    pagination_xpath = '//a[@rel="next"]/@href'
+    thread_pagination_xpath = '//a[@rel="prev"]/@href'
+    thread_page_xpath = '//div[@class="pagenav"]//span/strong/text()'
+    post_date_xpath = '//table[contains(@id, "post")]//td[@class="thead"]'\
+                      '/a[contains(@name,"post")]/following-sibling::text()'
+    avatar_xpath = '//a[contains(@href, "member.php?") and img/@src]'
+
+    # Other settings
+    use_proxy = False
+    download_delay = REQUEST_DELAY
+    download_thread = NO_OF_THREADS
+    sitemap_datetime_format = '%d.%m.%Y'
+    post_datetime_format = '%d.%m.%Y, %H:%M'
+
+    # Regex stuffs
+    topic_pattern = re.compile(
+        r".*t=(\d+)",
+        re.IGNORECASE
+    )
+    avatar_name_pattern = re.compile(
+        r"u=(\d+)",
+        re.IGNORECASE
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.headers.update({
             'Host': 'korovka32xc3t5cg.onion',
-            'Referer': 'http://korovka32xc3t5cg.onion/',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': USER_AGENT,
+            'Referer': self.base_url,
+            'User-Agent': USER_AGENT
+        })
+
+    def synchronize_meta(self, response, default_meta={}):
+        meta = {
+            key: response.meta.get(key) for key in ["cookiejar", "ip"]
+            if response.meta.get(key)
         }
+
+        meta.update(default_meta)
+        meta.update({'proxy': PROXY})
+
+        return meta
 
     def start_requests(self):
         md5_pass = hashlib.md5(PASS.encode('utf-8')).hexdigest()
-        login_url = 'http://korovka32xc3t5cg.onion/login.php?do=login'
         formdata = {
-            'cookieuser': '1',
-            'do': 'login',
-            'url': '/',
-            'vb_login_md5password': md5_pass,
-            'vb_login_md5password_utf': md5_pass,
-            'vb_login_password': '',
-            'vb_login_username': USER
+            "vb_login_username": USER,
+            "vb_login_password": "",
+            "vb_login_md5password": md5_pass,
+            "vb_login_md5password_utf": md5_pass,
+            "cookieuser": '1',
+            "url": "/",
+            "do": "login"
         }
+
         yield FormRequest(
-            url=login_url,
+            self.login_url,
             formdata=formdata,
             headers=self.headers,
-            callback=self.after_login,
-            meta={'proxy': self.proxy}
+            callback=self.parse_redirect,
+            meta={
+                'proxy': PROXY
+            }
         )
 
-    def after_login(self, response):
-        yield Request(
-            url=self.base_url,
+    def parse_redirect(self, response):
+        yield FormRequest.from_response(
+            response,
+            meta=self.synchronize_meta(response),
+            dont_filter=True,
             headers=self.headers,
-            callback=self.parse_code,
-            meta={'proxy': self.proxy}
+            callback=self.parse_code
         )
 
     def parse_code(self, response):
+        # Synchronize headers user agent with cloudfare middleware
+        self.synchronize_headers(response)
         code_block = response.xpath(
             '//td[contains(text(), "буквы Вашего кодового слова форму")]'
             '/text()').re(r'Введите (\d+)-.*? и (\d+)')
@@ -85,149 +140,123 @@ class KorovkaSpider(scrapy.Spider):
             's': s,
             'securitytoken': security_token
         }
-        code_submit_url = 'http://korovka32xc3t5cg.onion/misc.php?do=apa&check=1'
+        print(formdata)
+        code_submit_url = f'{self.base_url}misc.php?do=apa&check=1'
         yield FormRequest(
             url=code_submit_url,
-            callback=self.parse_main_page,
+            callback=self.parse_start,
             formdata=formdata,
             headers=self.headers,
-            meta={'proxy': self.proxy}
+            meta=self.synchronize_meta(response)
         )
 
-    def parse_main_page(self, response):
-        forums = response.xpath(
-            '//a[contains(@href, "forumdisplay.php?f=")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            url = url.strip('/')
-            if self.base_url not in url:
-                url = self.base_url + url
+    def parse_thread_date(self, thread_date):
+        """
+        :param thread_date: str => thread date as string
+        :return: datetime => thread date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        thread_date = thread_date.strip()
 
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum,
-                meta={'proxy': self.proxy}
+        if 'сегодня' in thread_date.lower():
+            return datetime.today()
+        elif "вчера" in thread_date.lower():
+            return datetime.today() - timedelta(days=1)
+        else:
+            return datetime.strptime(
+                thread_date,
+                self.sitemap_datetime_format
             )
 
-    def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//td[contains(@id, "td_threadtitle_")]'
-            '/div/a[contains(@href, "showthread.php?t=")]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={
-                    'topic_id': topic_id[0],
-                    'proxy': self.proxy
-                },
+    def parse_post_date(self, post_date):
+        """
+        :param post_date: str => post date as string
+        :return: datetime => post date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        post_date = post_date.strip()
+
+        if "сегодня" in post_date.lower():
+            return datetime.today()
+        elif "вчера" in post_date.lower():
+            return datetime.today() - timedelta(days=1)
+        else:
+            return datetime.strptime(
+                post_date,
+                self.post_datetime_format
             )
 
-        next_page = response.xpath('//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
+    def parse_start(self, response):
+        # Synchronize cloudfare user agent
+        self.synchronize_headers(response)
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
+
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
+
+            # if 'f=133' not in forum_url:
+            #     continue
             yield Request(
-                url=next_page_url,
+                url=forum_url,
                 headers=self.headers,
                 callback=self.parse_forum,
-                meta={'proxy': self.proxy}
+                meta=self.synchronize_meta(response)
             )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
+        # Parse generic thread
+        yield from super().parse_thread(response)
 
-        avatars = response.xpath('//a[contains(@href, "member.php?u=")]/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if self.base_url not in avatar_url:
+        # Parse generic avatar
+        yield from self.parse_avatars(response)
+
+    def parse_avatars(self, response):
+
+        # Synchronize headers user agent with cloudfare middleware
+        self.synchronize_headers(response)
+
+        # Save avatar content
+        for avatar in response.xpath(self.avatar_xpath):
+            avatar_url = avatar.xpath('img/@src').extract_first()
+
+            # Standardize avatar url
+            if not avatar_url.lower().startswith("http"):
                 avatar_url = self.base_url + avatar_url
-            user_id = avatar.xpath('@alt').extract_first()
-            if not user_id:
+
+            if 'image/svg' in avatar_url:
                 continue
-            file_name = '{}/{}.jpg'.format(self.avatar_path, user_id)
+
+            user_url = avatar.xpath('@href').extract_first()
+            match = self.avatar_name_pattern.findall(user_url)
+            if not match:
+                continue
+
+            file_name = os.path.join(
+                self.avatar_path,
+                f'{match[0]}.jpg'
+            )
+
             if os.path.exists(file_name):
                 continue
+
             yield Request(
                 url=avatar_url,
                 headers=self.headers,
                 callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                    'user_id': user_id,
-                    'proxy': self.proxy
-                }
+                meta=self.synchronize_meta(
+                    response,
+                    default_meta={
+                        "file_name": file_name
+                    }
+                ),
             )
 
-        next_page = response.xpath('//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={
-                    'topic_id': topic_id,
-                    'proxy': self.proxy
-                }
-            )
 
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar for user {response.meta['user_id']} done..!")
+class KorovkaScrapper(SiteMapScrapper):
 
-
-class KorovkaScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or PROXY
-        self.request_delay = 0.1
-        self.no_of_threads = 16
-        self.ensure_avatar_path()
-
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
-
-    def do_scrape(self):
-        settings = {
-            "DOWNLOADER_MIDDLEWARES": {
-                'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                'scrapy.downloadermiddlewares.defaultheaders.DefaultHeadersMiddleware': None
-            },
-            'DOWNLOAD_DELAY': self.request_delay,
-            'CONCURRENT_REQUESTS': self.no_of_threads,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        process = CrawlerProcess(settings)
-        process.crawl(KorovkaSpider, self.output_path, self.avatar_path, self.proxy)
-        process.start()
+    spider_class = KorovkaSpider
+    site_name = 'korovka (korovka32xc3t5cg.onion)'
