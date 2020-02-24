21137127c665f53ef38de72b4d72042f446bd6a6
osboxes.org <osboxes@osboxes.org>
2020-02-24 11:53:39 -0500

    add hashkiller

--- scraper/hashkiller.py
+++ scraper/hashkiller.py
@@ -1,176 +1,98 @@
-import os
 import re
-import scrapy
-from math import ceil
-import configparser
-from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+from datetime import datetime
+from scrapy import (
+    Request,
+    FormRequest
+)
+from scraper.base_scrapper import (
+    SitemapSpider,
+    SiteMapScrapper
+)
 
 
-class HashKillerSpider(scrapy.Spider):
-    name = 'hashkiller_spider'
+class HashKillerSpider(SitemapSpider):
+    name = "hashkiller_spider"
+    
+    # Url stuffs
+    base_url = "https://forum.hashkiller.io"
 
-    def __init__(self, output_path, avatar_path):
-        self.base_url = "https://forum.hashkiller.io/"
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.topic_pattern = re.compile(r'.*t=(\d+)')
-        self.pagination_pattern = re.compile(r'.*p=(\d+)')
-        self.start_url = 'https://forum.hashkiller.io/default.aspx'
-        self.output_path = output_path
-        self.avatar_path = avatar_path
-        self.headers = {
-            "user-agent": USER_AGENT,
-        }
+    # Xpath stuffs
+    forum_xpath = "//h3[@class=\"node-title\"]/a/@href"
+    pagination_xpath = "//a[contains(@class,\"pageNav-jump--next\")]/@href"
+
+    thread_xpath = "//div[contains(@class,\"js-threadListItem\")]"
+    thread_first_page_xpath = "//div[@class=\"structItem-title\"]/a/@href"
+    thread_last_page_xpath = "//div[@class=\"structItem-minor\"]/span/a[last()]/@href"
+    thread_date_xpath = "//div[contains(@class,\"structItem-cell--latest\")]/a/time/@datetime"
+
+    thread_pagination_xpath = "//a[contains(@class,\"pageNav-jump--prev\")]/@href"
+    thread_page_xpath = "//li[contains(@class,\"pageNav-page--current\")]/a/text()"
+    post_date_xpath = "//div[@class=\"message-attribution-main\"]/a/time/@datetime"
+    avatar_xpath = "//div[@class=\"message-avatar-wrapper\"]/a/img/@src"
+    
+    # Regex stuffs
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
+    topic_pattern = re.compile(
+        r".(\d+)/$",
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r".*p=(\d+)",
+        re.IGNORECASE
+    )
+
+    # Other settings
+    sitemap_datetime_format = "%Y-%m-%dT%H:%M:%S"
+    post_datetime_format = "%Y-%m-%dT%H:%M:%S"
+
+    def parse_post_date(self, post_date):
+        return super().parse_post_date(post_date[:-5])
+
+    def parse_thread_date(self, thread_date):
+        return super().parse_thread_date(thread_date[:-5])
 
     def start_requests(self):
         yield Request(
-            url=self.start_url,
-            headers=self.headers,
-            callback=self.parse
+            url="https://forum.hashkiller.io/index.php",
+            headers=self.headers
         )
 
     def parse(self, response):
-        forums = response.xpath(
-            '//td/h3/a[@id and contains(@href, "forum-")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
 
-    def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//a[contains(text(), "View first page")]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id[0]}
-            )
+        # Synchronize user agent for cloudfare middleware
+        self.synchronize_headers(response)
 
-        next_page = response.xpath(
-            '//div[@class="paging"]/a[@class="selected"]'
-            '/following-sibling::a[1][contains(@href, "/forum-view")]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
+        # Load all forums
+        all_forums = response.xpath(self.forum_xpath).extract()
+
+        for forum_url in all_forums:
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
+
             yield Request(
-                url=next_page_url,
+                url=forum_url,
                 headers=self.headers,
+                meta=self.synchronize_meta(response),
                 callback=self.parse_forum
             )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = int(pagination[0])+1 if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath('//td/img[@id and @class="avatar"]')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if not avatar_url.startswith('http'):
-                avatar_url = self.base_url + avatar_url
-            if 'image/svg' in avatar_url:
-                continue
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0].split('f=')[-1]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
+        # Parse generic thread
+        yield from super().parse_thread(response)
 
-        next_page = response.xpath(
-            '//div[@class="paging"]/a[@class="selected"]'
-            '/following-sibling::a[1][contains(@href, "/topic-view")]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar {file_name_only} done..!")
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
 
 
-class HashKillerScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.request_delay = 0.1
-        self.no_of_threads = 16
-        self.ensure_avatar_path()
+class HashKillerScrapper(SiteMapScrapper):
+    spider_class = HashKillerSpider
 
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
 
-    def do_scrape(self):
-        settings = {
-            'DOWNLOAD_DELAY': self.request_delay,
-            'CONCURRENT_REQUESTS': self.no_of_threads,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        if self.proxy:
-            settings.update({
-                "DOWNLOADER_MIDDLEWARES": {
-                    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-                    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
-                    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-                    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-                },
-                'ROTATING_PROXY_LIST': self.proxy,
-
-            })
-        process = CrawlerProcess(settings)
-        process.crawl(HashKillerSpider, self.output_path, self.avatar_path)
-        process.start()
-
-if __name__ == '__main__':
-    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
+if __name__ == "__main__":
+    pass
