bfdfc85fb3baf7df49aee459df0ea07551b88e95
osboxes.org <osboxes@osboxes.org>
2020-02-26 11:48:25 -0500

    add hoxforum

--- scraper/hoxforum.py
+++ scraper/hoxforum.py
@@ -1,188 +1,110 @@
-import os
 import re
-import scrapy
-from math import ceil
-import configparser
-from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
+
+from datetime import (
+    datetime, 
+    timedelta
+)
+from scrapy import (
+    Request,
+    FormRequest
+)
+from scraper.base_scrapper import (
+    SitemapSpider,
+    SiteMapScrapper
+)
 
 
-class HoxForumSpider(scrapy.Spider):
-    name = 'hoxforum_spider'
+class HoxForumSpider(SitemapSpider):
+    name = "hoxforum_spider"
 
-    def __init__(self, output_path, avatar_path):
-        self.base_url = "http://hoxforum.com/"
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page=(\d+)')
-        self.start_url = 'http://hoxforum.com/'
-        self.output_path = output_path
-        self.avatar_path = avatar_path
-        self.headers = {
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/75.0.3770.142 Safari/537.36",
-        }
+    # Url stuffs
+    base_url = "http://hoxforum.com/"
 
-    def start_requests(self):
-        yield Request(
-            url=self.start_url,
-            headers=self.headers,
-            callback=self.parse
-        )
+    # Xpath stuffs
+    forum_xpath = "//td[contains(@class,\"trow\")]/strong/a/@href|" \
+                  "//td[contains(@class,\"trow\")]/div[@class=\"smalltext\"]/a/@href"
+    pagination_xpath = "//a[@class=\"pagination_next\"]/@href"
+
+    thread_xpath = "//tr[@class=\"inline_row\"]"
+    thread_first_page_xpath = "//span[contains(@id,\"tid\")]/a/@href"
+    thread_last_page_xpath = "//span/span[@class=\"smalltext\"]/a[last()]/@href"
+    thread_date_xpath = "//span[@class=\"lastpost smalltext\"]/text()[1]"
+
+    thread_pagination_xpath = "//div[@class=\"pagination\"]/a[@class=\"pagination_previous\"]/@href"
+    thread_page_xpath = "//span[@class=\"pagination_current\"]/text()"
+    post_date_xpath = "//span[@class=\"post_date\"]/text()"
+
+    avatar_xpath = "//div[@class=\"author_avatar\"]/a/img/@src"
+    
+    # Regex stuffs
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r".*page=(\d+)",
+        re.IGNORECASE
+    )
+
+    # Other settings
+    sitemap_datetime_format = "%m-%d-%Y, %I:%M %p"
+    post_datetime_format = "%m-%d-%Y, %I:%M %p"
+
+    def parse_thread_date(self, thread_date):
+        if "yesterday" in thread_date.lower():
+            return datetime.today() - timedelta(days=1)
+        elif "hour" in thread_date.lower():
+            return datetime.today()
+        else:
+            return super().parse_thread_date(thread_date)
+    
+    def parse_post_date(self, post_date):
+        if "yesterday" in post_date.lower():
+            return datetime.today() - timedelta(days=1)
+        elif "hour" in post_date.lower():
+            return datetime.today()
+        else:
+            return super().parse_post_date(post_date)
 
     def parse(self, response):
-        forums = response.xpath(
-            '//td[@valign]/strong/a[contains(@href, "Forum-")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
 
-        sub_forums = response.xpath(
-            '//strong[text()="Sub Forums:"]/'
-            'following-sibling::a[contains(@href, "Forum-")]'
-            )
-        for sub_forum in sub_forums:
-            url = sub_forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
+        # Synchronize user agent for cloudfare middleware
+        self.synchronize_headers(response)
+
+        # Load all forums
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
+            # Standardize forum url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
+
             yield Request(
-                url=url,
+                url=forum_url,
                 headers=self.headers,
+                meta=self.synchronize_meta(response),
                 callback=self.parse_forum
             )
 
     def parse_forum(self, response):
-        print('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//span[contains(@id, "tid_")]'
-            '/a[contains(@href, "Thread-")]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = str(
-                    int.from_bytes(
-                        thread_url.encode('utf-8'), byteorder='big'
-                    ) % (10 ** 7)
-                )
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
 
-        next_page = response.xpath('//a[@class="pagination_next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
+        # Parse sub forums
+        yield from self.parse(response)
+
+        # Parse generic forum
+        yield from super().parse_forum(response)
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath('//div[@class="author_avatar"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if 'image/svg' in avatar_url:
-                continue
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
+        # Save generic thread
+        yield from super().parse_thread(response)
 
-        next_page = response.xpath(
-            '//a[@class="pagination_next" '
-            'and contains(@href, "Thread-")]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar {file_name_only} done..!")
+        # Save avatars
+        yield from super().parse_avatars(response)
 
 
-class HoxForumScrapper():
-    def __init__(self, kwargs):
-        self.output_path = kwargs.get('output')
-        self.proxy = kwargs.get('proxy') or None
-        self.request_delay = 0.1
-        self.no_of_threads = 16
-        self.ensure_avatar_path()
+class HoxForumScrapper(SiteMapScrapper):
+    spider_class = HoxForumSpider
 
-    def ensure_avatar_path(self, ):
-        self.avatar_path = f'{self.output_path}/avatars'
-        if not os.path.exists(self.avatar_path):
-            os.makedirs(self.avatar_path)
 
-    def do_scrape(self):
-        settings = {
-            'DOWNLOAD_DELAY': self.request_delay,
-            'CONCURRENT_REQUESTS': self.no_of_threads,
-            'CONCURRENT_REQUESTS_PER_DOMAIN': self.no_of_threads,
-            'RETRY_HTTP_CODES': [403, 429, 500, 503],
-            'RETRY_TIMES': 10,
-            'LOG_ENABLED': True,
-
-        }
-        if self.proxy:
-            settings.update({
-                "DOWNLOADER_MIDDLEWARES": {
-                    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-                    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
-                    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
-                    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-                    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-                },
-                'ROTATING_PROXY_LIST': self.proxy,
-
-            })
-        process = CrawlerProcess(settings)
-        process.crawl(HoxForumSpider, self.output_path, self.avatar_path)
-        process.start()
-
-if __name__ == '__main__':
-    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
+if __name__ == "__main__":
+    pass
