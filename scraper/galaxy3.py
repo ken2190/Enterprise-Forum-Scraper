fc2e8a578433ac4c21dceaf3d48d89789cd8ad53
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-03-01 15:44:06 +0545

    update module for galaxy3 and prologic

--- scraper/galaxy3.py
+++ scraper/galaxy3.py
@@ -1,221 +1,127 @@
-import re
 import os
-import hashlib
-import time
-import random
-import traceback
-from requests import Session
-from lxml.html import fromstring
-from scraper.base_scrapper import BaseScrapper
+import re
+import scrapy
+from math import ceil
+import configparser
+from scrapy.http import Request, FormRequest
+from datetime import datetime, timedelta
+import locale
+from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 
-# Credentials
-USERNAME = "vrx9"
-PASSWORD = "Night#Wall09"
+REQUEST_DELAY = 0.2
+NO_OF_THREADS = 10
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
+
+PROXY = 'http://127.0.0.1:8118'
 
 
-PROXY = "socks5h://localhost:9050"
+class Galaxy3Spider(SitemapSpider):
+    name = 'galaxy3_spider'
+    start_url = 'http://galaxy3m2mn5iqtn.onion/thewire/all'
+    base_url = 'http://galaxy3m2mn5iqtn.onion'
 
+    # Xpaths
+    pagination_xpath = '//li/a[contains(text(), "Next")]/@href'
+    thread_xpath = '//ul[contains(@class, "elgg-list")]/li'
+    thread_first_page_xpath = './/a[contains(text(), "Thread") and '\
+                              'contains(@href, "thewire/thread/")]/@href'
+    thread_date_xpath = './/div/time[@datetime]/@datetime'
+    thread_pagination_xpath = '//li/a[contains(text(), "Next")]/@href'
+    thread_page_xpath = '//li[@class="elgg-state-selected"]/span/text()'
+    post_date_xpath = '//div/time[@datetime]/@datetime'
 
-class Galaxy3Scrapper(BaseScrapper):
-    def __init__(self, kwargs):
-        super(Galaxy3Scrapper, self).__init__(kwargs)
-        self.start_url = "http://galaxy3m2mn5iqtn.onion/thewire/all"
-        self.topic_url = "http://galaxy3m2mn5iqtn.onion/thewire/thread/{}"
-        self.username = kwargs.get('user')
-        self.password = kwargs.get('password')
-        self.ignore_xpath = '//div[@class="elgg-main elgg-body" and '\
-                            'not(ul[@class="elgg-list elgg-list-entity"])]'
-        self.proxy = kwargs.get('proxy') or PROXY
-        self.avatar_name_pattern = re.compile(r'.*/(\w+\.\w+)')
+    avatar_xpath = '//div[contains(@class,"elgg-avatar")]/a/img/@src'
 
-    def write_paginated_data(self, html_response):
-        next_page_block = html_response.xpath(
-            '//p[@class="paging"]/strong'
-            '//following-sibling::a[1]/@href'
-        )
-        if not next_page_block:
-            return
-        next_page_url = next_page_block[0]
-        pattern = re.compile(r'id=(\d+)&p=(\d+)')
-        match = pattern.findall(next_page_url)
-        if not match:
-            return
-        topic, pagination_value = match[0]
-        content = self.get_page_content(
-            next_page_url, self.ignore_xpath
-        )
-        if not content:
-            return
+    # Regex stuffs
+    topic_pattern = re.compile(
+        r"thread/(\d+)",
+        re.IGNORECASE
+    )
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
 
-        paginated_file = '{}/{}-{}.html'.format(
-            self.output_path, topic, pagination_value
-        )
-        with open(paginated_file, 'wb') as f:
-            f.write(content)
+    # Other settings
+    use_proxy = False
+    sitemap_datetime_format = "%Y-%m-%dT%H:%M:%S"
+    post_datetime_format = "%Y-%m-%dT%H:%M:%S"
 
-        print('{}-{} done..!'.format(topic, pagination_value))
-        return content
-
-    def clear_cookies(self,):
-        self.session.cookies['topicsread'] = ''
-
-    def get_avatar_info(self, html_response):
-        avatar_info = dict()
-        urls = html_response.xpath(
-            '//div[@class="elgg-image-block clearfix thewire-post"]//img/@src'
-        )
-        for url in urls:
-            name_match = self.avatar_name_pattern.findall(url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            if name not in avatar_info:
-                avatar_info.update({
-                    name: url
-                })
-        return avatar_info
-
-    def login(self):
-        password = self.password or PASSWORD
-        username = self.username or USERNAME
-        for i in range(5):
-            try:
-                response = self.session.get(self.login_url).content
-                break
-            except:
-                time.sleep(5)
-                response = None
-        if not response:
-            return False
-        html_response = self.get_html_response(response)
-        token = html_response.xpath('//input[@name="csrf_token"]/@value')
-        if not token:
-            return False
-        data = {
-            'csrf_token': token[0],
-            "form_sent": "1",
-            "login": "Login",
-            'req_password': password,
-            'req_username': username,
-            'save_pass': "1",
-        }
-        login_response = self.session.post(self.login_url, data=data)
-        html_response = self.get_html_response(login_response.content)
-        if html_response.xpath(
-           '//span[text()="Incorrect username and/or password."]'):
-            return False
-        return True
-
-    def process_topic(self, topic):
-        try:
-            response = self.process_first_page(
-                topic, self.ignore_xpath
-            )
-            if response is None:
-                return
-
-            avatar_info = self.get_avatar_info(response)
-            for name, url in avatar_info.items():
-                self.save_avatar(name, url)
-
-            # ------------clear cookies without logout--------------
-            self.clear_cookies()
-        except:
-            traceback.print_exc()
-            return
-        self.process_pagination(response)
-
-    def do_rescan(self,):
-        print('**************  Rescanning  **************')
-        print('Broken Topics found')
-        broken_topics = self.get_broken_file_topics()
-        print(broken_topics)
-        if not broken_topics:
-            return
-        self.session.proxies.update({
-            'http': self.proxy,
-            'https': self.proxy,
-        })
-        for topic in broken_topics:
-            file_path = "{}/{}.html".format(self.output_path, topic)
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            self.process_topic(topic)
-
-    def do_new_posts_scrape(self,):
-        print('**************  New posts scan  **************')
-        self.session.proxies.update({
-            'http': self.proxy,
-            'https': self.proxy,
-        })
-        new_post_url = 'http://x7bwsmcore5fmx56.onion/search.php?'\
-                       'action=show_new'
-        while True:
-            print('Url: {}'.format(new_post_url))
-            content = self.get_page_content(new_post_url)
-            if not content:
-                print('New posts not found')
-                return
-            new_topics = list()
-            html_response = self.get_html_response(content)
-            urls = html_response.xpath('//h3[@class="hn"]/a/@href')
-            pattern = re.compile(r"id=(\d+)")
-            for url in urls:
-                match = pattern.findall(url)
-                if not match:
-                    continue
-                new_topics.append(match[0])
-            for topic in new_topics:
-                # file_path = "{}/{}.html".format(self.output_path, topic)
-                # if os.path.exists(file_path):
-                #     os.remove(file_path)
-                self.process_topic(topic)
-            next_url = html_response.xpath(
-                '//p[@class="paging"]/strong'
-                '//following-sibling::a[1]/@href')
-            if not next_url:
-                return
-            new_post_url = self.site_link + next_url[0]
-
-    def do_scrape(self):
-        self.session.proxies.update({
-            'http': self.proxy,
-            'https': self.proxy,
-        })
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
         self.headers.update({
-            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0',
+            "user-agent": USER_AGENT
         })
-        url = self.start_url
-        topic_pattern = re.compile(r'/thread/(\d+)')
-        while True:
-            topic_list = set()
-            content = self.get_page_content(url)
-            if not content:
-                print('No data found for:{}'.format(url))
-                return
-            html_response = self.get_html_response(content)
-            topic_urls = html_response.xpath(
-                '//a[@class="elgg-menu-content"]/@href')
-            for topic_url in topic_urls:
-                match = topic_pattern.findall(topic_url)
-                if not match:
-                    continue
-                topic_list.add(match[0])
 
-            for topic in topic_list:
-                self.process_topic(topic)
-            next_page_url = html_response.xpath(
-                '//li/a[contains(text(), "Next")]/@href')
-            if not next_page_url:
-                break
-            url = next_page_url[0]
+    def start_requests(self):
+        yield Request(
+            url=self.start_url,
+            callback=self.parse_forum,
+            headers=self.headers,
+            meta={
+                'proxy': PROXY
+            }
+        )
+
+    def synchronize_meta(self, response, default_meta={}):
+        meta = {
+            key: response.meta.get(key) for key in ["cookiejar", "ip"]
+            if response.meta.get(key)
+        }
+
+        meta.update(default_meta)
+        meta.update({'proxy': PROXY})
+
+        return meta
+
+    def parse_thread_date(self, thread_date):
+        """
+        :param thread_date: str => thread date as string
+        :return: datetime => thread date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+
+        return datetime.strptime(
+            thread_date.strip()[:-6],
+            self.sitemap_datetime_format
+        )
+
+    def parse_post_date(self, post_date):
+        """
+        :param post_date: str => post date as string
+        :return: datetime => post date as datetime converted from string,
+                            using class post_datetime_format
+        """
+        return datetime.strptime(
+            post_date.strip()[:-6],
+            self.post_datetime_format
+        )
+
+    def parse_thread(self, response):
+
+        # Parse generic thread
+        yield from super().parse_thread(response)
+
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
 
 
-def main():
-    print('**************  Started galaxy Scrapper **************\n')
-    template = Galaxy3Scrapper()
-    template.do_scrape()
+class Galaxy3Scrapper(SiteMapScrapper):
 
-if __name__ == '__main__':
-    main()
+    spider_class = Galaxy3Spider
+    site_name = 'galaxy3_lgalaxy3m2mn5iqtn'
+
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update(
+            {
+                'DOWNLOAD_DELAY': REQUEST_DELAY,
+                'CONCURRENT_REQUESTS': NO_OF_THREADS,
+                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+                "RETRY_HTTP_CODES": [406, 429, 500, 503],
+            }
+        )
+        return settings
--- scraper/prologic.py
+++ scraper/prologic.py
@@ -1,56 +1,99 @@
 import os
+import uuid
 import re
 import json
 import scrapy
-from math import ceil
-import configparser
-from lxml.html import fromstring
-from urllib.parse import urlencode
 from scrapy.http import Request, FormRequest
-from scrapy import Selector
-from scrapy.utils.gz import gunzip
+from datetime import datetime, timedelta
 from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 USER = 'vrx9'
 PASS = 'Night#Pro000'
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-REQUEST_DELAY = 0.2
-NO_OF_THREADS = 10
+
+REQUEST_DELAY = 0.5
+NO_OF_THREADS = 5
 
 
 class ProLogicSpider(SitemapSpider):
     name = 'prologic_spider'
-    sitemap_url = 'https://prologic.su/sitemap.xml'
-    # Xpath stuffs
-    forum_sitemap_xpath = '//sitemap/loc[contains(text(), "sitemap_topics.xml")]/text()'
-    thread_sitemap_xpath = '//url[loc[contains(text(),"/topic/")] and lastmod]'
-    thread_url_xpath = "//loc/text()"
-    thread_lastmod_xpath = "//lastmod/text()"
-    sitemap_datetime_format = "%Y-%m-%d"
+    base_url = "https://prologic.su/"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.base_url = "https://prologic.su/"
-        self.topic_pattern = re.compile(r'/topic/(\d+)-')
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page__st__(\d+)$')
-        self.start_url = 'https://prologic.su/'
-        self.headers = {
-            "user-agent": USER_AGENT,
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-        }
+    # Xpaths
+    forum_xpath = '//h4[@class="forum_name"]/strong/a/@href|'\
+                  '//ol[contains(@id, "subforums_")]/li/a/@href'
+    pagination_xpath = '//a[@rel="next"]/@href'
+    thread_xpath = '//tr[contains(@id,"trow_")]'
+    thread_first_page_xpath = '//h4/a[contains(@href,"/topic/")]/@href'
+    thread_last_page_xpath = '//ul[@class="mini_pagination"]'\
+                             '/li[last()]/a/@href'
+    thread_date_xpath = '//li[@class="desc lighter blend_links"]/a/text()'
+    thread_pagination_xpath = '//div[@class="topic_controls"]'\
+                              '//a[@rel="prev"]/@href'
+    thread_page_xpath = '//li[@class="page active"]/text()'
+    post_date_xpath = '//div[@class="post_date"]/abbr/@title'
+
+    avatar_xpath = '//li[@class="avatar"]/a/img/@src'
+
+    # Regex stuffs
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
+    topic_pattern = re.compile(
+        r'/topic/(\d+)-',
+        re.IGNORECASE
+    )
+
+    # Other settings
+    download_delay = REQUEST_DELAY
+    download_thread = NO_OF_THREADS
+    sitemap_datetime_format = '%d %b %Y'
+    post_datetime_format = '%Y-%m-%dT%H:%M:%S'
+
+    def parse_thread_date(self, thread_date):
+        """
+        :param thread_date: str => thread date as string
+        :return: datetime => thread date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        thread_date = thread_date.strip()
+
+        if any(d in thread_date.lower() for d in ['сегодня', 'минут']):
+            return datetime.today()
+        elif "вчера" in thread_date.lower():
+            return datetime.today() - timedelta(days=1)
+        else:
+            return datetime.strptime(
+                thread_date,
+                self.sitemap_datetime_format
+            )
+
+    def parse_post_date(self, post_date):
+        """
+        :param post_date: str => post date as string
+        :return: datetime => post date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+        # Standardize thread_date
+        post_date = post_date.strip()
+
+        return datetime.strptime(
+            post_date[:-6],
+            self.post_datetime_format
+        )
 
     def start_requests(self):
         yield Request(
-            url=self.start_url,
+            url=self.base_url,
             headers=self.headers,
-            callback=self.proceed_for_login
+            meta={
+                "cookiejar": uuid.uuid1().hex
+            },
+            dont_filter=True
         )
 
-    def proceed_for_login(self, response):
+    def parse(self, response):
         auth_key = response.xpath(
             '//input[@name="auth_key"]/@value').extract_first()
         params = {
@@ -64,130 +107,34 @@ class ProLogicSpider(SitemapSpider):
                     '&section=login&do=process'
         yield FormRequest(
             url=login_url,
-            callback=self.parse_main_page,
+            callback=self.parse_start,
             formdata=params,
             headers=self.headers,
             dont_filter=True,
             )
 
-    def parse_main_page(self, response):
-        if self.start_date:
-            yield scrapy.Request(
-                url=self.sitemap_url,
-                headers=self.headers,
-                callback=self.parse_sitemap
-            )
-        else:
-            forums = response.xpath(
-                '//h4[@class="forum_name"]/strong/a')
-            subforums = response.xpath(
-                '//ol[contains(@id, "subforums_")]/li/a')
-            forums.extend(subforums)
-            for forum in forums:
-                url = forum.xpath('@href').extract_first()
-                if self.base_url not in url:
-                    url = self.base_url + url
-                # if not url.endswith('2-microsoft-windows/'):
-                #     continue
-                yield Request(
-                    url=url,
-                    headers=self.headers,
-                    callback=self.parse_forum
-                )
+    def parse_start(self, response):
+        # Synchronize cloudfare user agent
+        self.synchronize_headers(response)
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
 
-    def parse_sitemap_forum(self, response):
-
-        # Load selector
-        text = gunzip(response.body)
-        selector = Selector(text=gunzip(response.body))
-
-        # Load thread
-        all_threads = selector.xpath(self.thread_sitemap_xpath).extract()
-        for thread in all_threads:
-            yield from self.parse_sitemap_thread(thread, response)
-
-    def parse_forum(self, response):
-        self.logger.info('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//a[contains(@id, "tid-link-")]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
             yield Request(
-                url=thread_url,
+                url=forum_url,
                 headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id[0]}
-            )
-
-        next_page = response.xpath(
-            '//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_forum
+                callback=self.parse_forum,
+                meta=self.synchronize_meta(response),
             )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = int(pagination[0])//25 + 1 if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            self.logger.info(f'{topic_id}-{paginated_value} done..!')
+        # Parse generic thread
+        yield from super().parse_thread(response)
 
-        avatars = response.xpath('//li[@class="avatar"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if self.base_url not in avatar_url:
-                avatar_url = self.base_url + avatar_url
-            match = self.avatar_name_pattern.findall(avatar_url)
-            if not match:
-                continue
-            file_name = '{}/{}.jpg'.format(self.avatar_path, match[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
-
-        next_page = response.xpath(
-            '//a[@rel="next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            self.logger.info(f"Avatar {file_name_only} done..!")
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
 
 
 class ProLogicScrapper(SiteMapScrapper):
@@ -199,9 +146,7 @@ class ProLogicScrapper(SiteMapScrapper):
         settings = super().load_settings()
         settings.update(
             {
-                'DOWNLOAD_DELAY': REQUEST_DELAY,
-                'CONCURRENT_REQUESTS': NO_OF_THREADS,
-                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS,
+                "RETRY_HTTP_CODES": [403, 406, 429, 500, 503],
             }
         )
         return settings
