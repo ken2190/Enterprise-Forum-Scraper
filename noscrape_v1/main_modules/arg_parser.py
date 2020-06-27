a02b25eb22697704c99b2f35e0e4f22aeab5d1ce
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-06-27 16:11:45 +0545

    -o --out_folder switch added for output folder!

--- noscrape_v1/main_modules/arg_parser.py
+++ noscrape_v1/main_modules/arg_parser.py
@@ -14,10 +14,10 @@ class ArgParser():
         parser._optionals.title = "Standard arguments"
 
         parser.add_argument('--type', '-type', nargs='?', help='State the type of database', choices=databases, required=True)
-        parser.add_argument('--out_file', '-o', nargs='?', default=None, metavar="OutputFile", help=argparse.SUPPRESS)
+        # parser.add_argument('--out_file', '-o', nargs='?', default=None, metavar="OutputFile", help=argparse.SUPPRESS, required=True)
         parser.add_argument('-v', action="store_true", help=argparse.SUPPRESS)
         parser.add_argument('--examples', action="store_true", help=argparse.SUPPRESS)
-        parser.add_argument('--out_folder', '-of', action="store_true",help=argparse.SUPPRESS)
+        parser.add_argument('--out_folder', '-o', nargs='?', default=None, metavar="OutputFolder", help=argparse.SUPPRESS, required=True)
 
         # Mongo and Elastic stuff
         nosql_and_elastic_options = parser.add_argument_group('MongoDB and ElasticSearch Options')
