91105ca22d153c0b037e0d7240411e33da19e145
Dylan Dannenhauer <dylan.dannenhauer@ignitioncommerce.com>
2020-06-29 18:13:21 -0400

    Add .idea to .gitignore. Write recursive algorithm to hash all values of json document

---
+++ tools/fbhash.py
@@ -0,0 +1,45 @@
+
+import hmac, base64, json
+from collections import OrderedDict
+TEST_KEY = 'facebook,instagram,oculus,whatsapp'
+
+test_data = {
+	"a": 1,
+	"b": 2,
+	"c": {
+		"c1": '1c',
+		"c2": '2c',
+		"c3": {
+			"d": "doit",
+			"d1": '1d',
+			"d2": '2d'
+		},
+		"c4": "4c",
+		"the_list": [1,2,3,4,5]
+	},
+	"d": 3,
+	"e": 4
+}
+
+def keyed_hashing_algorithm(value):
+	value_string = str(value).lower().strip()
+	value_bytes = value_string.encode('utf-8')
+	hash_bytes = hmac.digest(TEST_KEY.encode('utf-8'), value_bytes, 'sha256')
+	hash_string = base64.standard_b64encode(hash_bytes).decode('utf-8')
+	return hash_string
+
+
+def update(d, u):
+	for k, v in u.items():
+		if isinstance(v, dict):
+			d[k] = update(d.get(k, {}), v)
+		elif isinstance(v, list):
+			d[k] = [keyed_hashing_algorithm(val) for val in v]
+		else:
+			d[k] = keyed_hashing_algorithm(v)
+	return d
+
+
+doit = update(test_data, test_data)
+print(json.dumps(doit, indent=4))
+
