d1d0f5ef5a8f691e72d22d787f3519da25f61680
Dylan Dannenhauer <dylan.dannenhauer@ignitioncommerce.com>
2020-06-30 18:51:34 -0400

    Change tabs to spaces

--- tools/fbhash.py
+++ tools/fbhash.py
@@ -2,29 +2,28 @@
 import hmac, base64, json
 from collections import OrderedDict
 
-# from settings import DATASET_HASH_KEY
+from settings import DATASET_HASH_KEY
 
-DATASET_HASH_KEY = 'facebook,instagram,oculus,whatsapp'
 
 def keyed_hashing_algorithm(value):
-	if value is None:
-		return value
+    if value is None:
+        return value
 
-	value_string = str(value).lower().strip()
-	value_bytes = value_string.encode('utf-8', errors='surrogatepass')
-	hash_bytes = hmac.digest(DATASET_HASH_KEY.encode('utf-8'), value_bytes, 'sha256')
-	hash_string = base64.standard_b64encode(hash_bytes).decode('utf-8')
-	return hash_string
+    value_string = str(value).lower().strip()
+    value_bytes = value_string.encode('utf-8', errors='surrogatepass')
+    hash_bytes = hmac.digest(DATASET_HASH_KEY.encode('utf-8'), value_bytes, 'sha256')
+    hash_string = base64.standard_b64encode(hash_bytes).decode('utf-8')
+    return hash_string
 
 
 def hash_dataset(payload_1):
-	for k, v in payload_1.items():
-		if isinstance(v, dict):
-			payload_1[k] = hash_dataset(payload_1.get(k, {}))
-		elif isinstance(v, list):
-			payload_1[k] = [keyed_hashing_algorithm(val) for val in v]
-		else:
-			payload_1[k] = keyed_hashing_algorithm(v)
-	return payload_1
+    for k, v in payload_1.items():
+        if isinstance(v, dict):
+            payload_1[k] = hash_dataset(payload_1.get(k, {}))
+        elif isinstance(v, list):
+            payload_1[k] = [keyed_hashing_algorithm(val) for val in v]
+        else:
+            payload_1[k] = keyed_hashing_algorithm(v)
+    return payload_1
 
 
