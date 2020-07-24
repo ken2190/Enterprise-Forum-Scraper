19de6c8969559b87547f31c93c2bad1d7f5324d0
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-07-24 10:26:45 +0545

    Refactor script

--- tools/refactor.py
+++ tools/refactor.py
@@ -1,3 +1,4 @@
+import re
 import json
 import traceback
 import argparse
@@ -5,6 +6,8 @@ from copy import deepcopy
 
 EMAILS = ['e', 'e1', 'e2', 'e3', 'e4', 'e5']
 IPS = ['ip', 'ip1', 'ip2', 'ip3', 'ip4', 'ip5']
+TELEPHONES = ['t', 't1', 't2', 't3', 't4', 't5']
+ADDRESS = ['address', 'a1', 'a2', 'a3', 'a4']
 
 KEYS_TO_CHECK = ['breach']
 
@@ -50,20 +53,37 @@ def process_line(out_file, single_json, mapper):
     data = deepcopy(json_response['_source'])
     email = list()
     ip = list()
+    telephone = list()
+    address = dict()
     for key, value in data.items():
         json_response['_source'].pop(key)
         if key in EMAILS:
             email.append(value)
         elif key in IPS:
             ip.append(value)
+        elif key in TELEPHONES:
+            telephone.append(value)
+        elif key in ADDRESS:
+            address.update({key: value})
         else:
             new_key = mapper.get(key, key)
             json_response['_source'][new_key] = value
     if email:
         json_response['_source']['email'] = email[0]\
             if len(email) == 1 else email
+    if telephone:
+        json_response['_source']['telephone'] = telephone[0]\
+            if len(telephone) == 1 else telephone
     if ip:
         json_response['_source']['ip'] = ip[0] if len(ip) == 1 else ip
+    if address:
+        merged_addr = f"{address.get('address', '')} "\
+                      f"{address.get('a1', '')} "\
+                      f"{address.get('a2', '')} "\
+                      f"{address.get('a3', '')} "\
+                      f"{address.get('a4', '')}"
+        merged_addr = re.sub(r'\s{2,}', ' ', merged_addr.strip())
+        json_response['_source']['address'] = merged_addr
     out_file.write(json.dumps(json_response, ensure_ascii=False) + '\n')
 
 
