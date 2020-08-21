0dbf0215620c08a8710cfd9ed743f5139f92c69d
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-08-21 20:19:35 +0545

    new parsers for darkmarket and dedicatet

--- templates/__init__.py
+++ templates/__init__.py
@@ -183,6 +183,9 @@ from templates.itshqip_template import ItshqipParser
 from templates.totalblackhat_template import TotalBlackHatParser
 from templates.digitalgangster_template import DigitalGangsterParser
 from templates.stormfront_template import StormFrontParser
+from templates.dedicatet_template import DedicatetParser
+from templates.darkmarket_cm_template import DarkMarketCmParser
+
 
 PARSER_MAP = {
     '0day': OdayParser,
@@ -368,5 +371,7 @@ PARSER_MAP = {
     'digitalgangster': DigitalGangsterParser,
     'stormfront': StormFrontParser,
     'totalblackhat': TotalBlackHatParser,
-    'itshqip': ItshqipParser
+    'itshqip': ItshqipParser,
+    'dedicatet': DedicatetParser,
+    'darkmarket_cm': DarkMarketCmParser
 }
