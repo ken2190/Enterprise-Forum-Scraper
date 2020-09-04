eb05a64745b652136e48e696512f175533923576
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-09-05 00:44:38 +0545

    boveda fix

--- scraper/boveda.py
+++ scraper/boveda.py
@@ -55,6 +55,12 @@ class BovedaSpider(SitemapSpider):
         r'threads/.*\.(\d+)/',
         re.IGNORECASE
     )
+
+    pagination_pattern = re.compile(
+        r".*page-(\d+).*",
+        re.IGNORECASE
+    )
+
     avatar_name_pattern = re.compile(
         r".*/(\S+\.\w+)",
         re.IGNORECASE
@@ -63,7 +69,7 @@ class BovedaSpider(SitemapSpider):
     def parse(self, response):
         # Synchronize cloudfare user agent
         self.synchronize_headers(response)
-        all_forums = response.xpath(self.forum_xpath).extract()        
+        all_forums = response.xpath(self.forum_xpath).extract()
         for forum_url in all_forums:
 
             # Standardize url
@@ -76,29 +82,6 @@ class BovedaSpider(SitemapSpider):
                 callback=self.parse_forum,
                 meta=self.synchronize_meta(response),
             )
-
-    def parse_thread_date(self, thread_date):
-        """
-        :param thread_date: str => thread date as string
-        :return: datetime => thread date as datetime converted from string,
-                            using class sitemap_datetime_format
-        """
-
-        return datetime.strptime(
-            thread_date.strip()[:-5],
-            self.sitemap_datetime_format
-        )
-
-    def parse_post_date(self, post_date):
-        """
-        :param post_date: str => post date as string
-        :return: datetime => post date as datetime converted from string,
-                            using class post_datetime_format
-        """
-        return datetime.strptime(
-            post_date.strip()[:-5],
-            self.post_datetime_format
-        )
 
     def parse_thread(self, response):
         # Parse generic thread
