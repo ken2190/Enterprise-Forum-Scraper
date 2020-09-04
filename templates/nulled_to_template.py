e24083c328fa0f1ae8677990be826be77c759c3f
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-09-04 15:29:22 +0545

    ogusers scraper and parser fix

--- templates/nulled_to_template.py
+++ templates/nulled_to_template.py
@@ -11,21 +11,13 @@ class NulledToParser(BaseTemplate):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parser_name = "nulled.to"
-        self.thread_name_pattern = re.compile(
-            r'(\d+).*html$'
-        )
-        self.pagination_pattern = re.compile(
-            r'\d+-(\d+)\.html'
-        )
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.files = self.get_filtered_files(kwargs.get('files'))
         self.comments_xpath = '//div[contains(@id, "post_id_")]/div[@class="post_wrap"]'
         self.header_xpath = '//div[contains(@id, "post_id_")]/div[@class="post_wrap"]'
         self.date_xpath = 'div/div[@class="post_date"]/abbr/@title'
-        self.date_pattern = "%Y-%m-%dT%H:%M:%S"
         self.author_xpath = 'div[@class="author_info clearfix"]//span[@class="author vcard"]/a/span/span/text()'
         self.title_xpath = '//div[@class="maintitle clear clearfix"]/span/text()'
-        self.post_text_xpath = 'div//section[@id="nulledPost"]'
+        self.post_text_xpath = 'div//section[@id="nulledPost"]/descendant::text()'
         self.avatar_xpath = 'div//li[@class="avatar"]/img/@src'
         self.comment_block_xpath = 'div//a[@itemprop="replyToUrl"]/text()'
         self.avatar_name_pattern = re.compile(
@@ -35,48 +27,3 @@ class NulledToParser(BaseTemplate):
 
         # main function
         self.main()
-
-    def get_date(self, tag):
-        date = tag.xpath(self.date_xpath)
-        date = date[0].split('+')[0].strip() if date else ""
-
-        try:
-            date = datetime.datetime.strptime(date, self.date_pattern).timestamp()
-            return str(date)
-        except:
-            try:
-                date = dparser.parse(date).timestamp()
-                return str(date)
-            except :
-                pass
-
-        return ""
-
-    def get_author(self, tag):
-        author = tag.xpath(self.author_xpath)
-        if not author:
-            author = tag.xpath(
-                'div[@class="author_info clearfix"]'
-                '//span[@class="author vcard"]/s/text()'
-            )
-        if not author:
-            author = tag.xpath(
-                'div[@class="author_info clearfix"]'
-                '/div[@class="post_username"]/text()'
-            )
-        if not author:
-            author = tag.xpath(
-                'div[@class="author_info clearfix"]'
-                '//span[@class="author vcard"]/descendant::text()'
-            )
-
-        author = ''.join(author).strip() if author else None
-        return author
-
-    def get_post_text(self, tag):
-        post_text_block = tag.xpath(self.post_text_xpath)
-        post_text = "\n".join([
-            post_text.xpath('string()') for post_text in post_text_block
-        ])
-
-        return post_text.strip()
--- templates/ogusers_template.py
+++ templates/ogusers_template.py
@@ -10,76 +10,18 @@ class OgUsersParser(BaseTemplate):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parser_name = "ogusers.com"
-        self.thread_name_pattern = re.compile(
-            r'(\d+).*html$'
-        )
-        self.pagination_pattern = re.compile(
-            r'\d+-(\d+)\.html$'
-        )
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.files = self.get_filtered_files(kwargs.get('files'))
+        self.avatar_name_pattern = re.compile(r".*avatar_(\d+\.\w+)\?")
         self.mode = 'r'
         self.comments_xpath = '//div[@class="postbox"]'
         self.header_xpath = '//div[@class="postbox"]'
-        self.date_xpath = 'div//div[@class="inline pb_date smalltext"]/span/@title'
+        self.date_xpath = 'div//div[contains(@class,"pb_date")]/span/@title'\
+                          '|div//div[contains(@class,"pb_date")]/text()'
         self.title_xpath = '//span[@class="showthreadtopbar_size"]/text()'
         self.post_text_xpath = 'div//div[@class="post_body scaleimages"]/descendant::text()[not(ancestor::blockquote)]'
-        self.avatar_xpath = 'aside//div[@class="postbit-avatar"]/a/img/@src'
+        self.avatar_xpath = 'aside//div[@class="postbit-avatar"]/a/img/@src|.//img[@class="profileshow"]/@src'
         self.comment_block_xpath = 'div//div[@class="float_right postbitnum"]/strong/a/text()'
+        self.author_xpath = 'aside//div[contains(@class,"postbitdetail")]/span//text()'
+        self.date_xpath = 'div//div[contains(@class,"pb_date")]/text()'
 
         # main function
         self.main()
-
-    def get_filtered_files(self, files):
-        filtered_files = list(
-            filter(
-                lambda x: self.thread_name_pattern.search(x) is not None,
-                files
-            )
-        )
-        sorted_files = sorted(
-            filtered_files,
-            key=lambda x: (
-                int(self.thread_name_pattern.search(x).group(1)),
-                int(self.pagination_pattern.search(x).group(1))
-            ))
-
-        return sorted_files
-
-    def get_date(self, tag):
-        date_block = tag.xpath(self.date_xpath)
-        if not date_block:
-            date_block = tag.xpath(
-                'div//div[@class="inline pb_date smalltext"]/text()')
-
-        date = date_block[0].strip() if date_block else ""
-        try:
-            date = dparser.parse(date).timestamp()
-            return str(date)
-        except Exception:
-            return ""
-
-    def get_author(self, tag):
-        author = tag.xpath(
-            'aside//div[@class="postbitdetail"]/span/a/span/text()'
-        )
-        if not author:
-            author = tag.xpath(
-                'aside//div[@class="postbitdetail"]/span/a/text()')
-        if not author:
-            author = tag.xpath(
-                'aside//div[@class="postbitdetail"]/span/a/font/text()')
-
-        author = author[0].strip() if author else None
-        return author
-
-    def get_avatar(self, tag):
-        avatar_block = tag.xpath(self.avatar_xpath)
-        if not avatar_block:
-            return ""
-
-        name_match = self.avatar_name_pattern.findall(avatar_block[0])
-        if not name_match:
-            return ""
-
-        return name_match[0] if 'svg' not in name_match[0] else ''
