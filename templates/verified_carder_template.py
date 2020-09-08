7792dcaeaa2f1fba5614a3a5508864bac5e4538c
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-09-08 13:04:59 +0545

    verified carder parser fix

--- templates/verified_carder_template.py
+++ templates/verified_carder_template.py
@@ -26,34 +26,6 @@ class VerifiedCarderParser(BaseTemplate):
         self.post_text_xpath = 'div//article/div[@class="bbWrapper"]/descendant::text()[not(ancestor::div[contains(@class, "bbCodeBlock bbCodeBlock--expandable bbCodeBlock--quote")])]'
         self.avatar_xpath = 'div//div[@class="message-avatar-wrapper"]/span/img/@src'
         self.comment_block_xpath = 'div//ul[@class="message-attribution-opposite message-attribution-opposite--list"]/li/a/text()'
+        self.author_xpath = 'div//div[@class="message-userDetails"]/h4//text()'
 
         # main function
-        self.main()
-
-    def get_date(self, tag):
-        date = tag.xpath(self.date_xpath)
-        return date[0] if date else ''
-
-    def get_author(self, tag):
-        author = tag.xpath(
-            'div//div[@class="message-userDetails"]/h4/a/text()'
-        )
-        if not author:
-            author = tag.xpath(
-                'div//div[@class="message-userDetails"]/h4/a/span/text()'
-            )
-        if not author:
-            author = tag.xpath(
-                'div//div[@class="message-userDetails"]/h4/span/text()'
-            )
-
-        author = author[0].strip() if author else None
-        return author
-
-    def get_comment_id(self, tag):
-        comment_id = ""
-        comment_block = tag.xpath(self.comment_block_xpath)
-        if comment_block:
-            comment_id = comment_block[-1].strip().split('#')[-1]
-
-        return comment_id.replace(',', '').replace('.', '')
