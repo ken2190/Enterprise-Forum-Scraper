1a4796d438f94b261aabc90f134e1632f2f9422f
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-09-11 00:28:17 +0545

    demonforums scrapers fix ! uses cookies !

--- scraper/demonforums.py
+++ scraper/demonforums.py
@@ -1,12 +1,21 @@
 import os
 import re
-import scrapy
-import uuid
 
 from scrapy.http import Request, FormRequest
 from scraper.base_scrapper import (
-    BypassCloudfareSpider,
-    SiteMapScrapper
+    SitemapSpider,
+    SiteMapScrapper,
+    PROXY_USERNAME,
+    PROXY_PASSWORD,
+    PROXY
+)
+
+import uuid
+
+
+from selenium.webdriver import (
+    Chrome,
+    ChromeOptions
 )
 
 
@@ -16,8 +25,12 @@ NO_OF_THREADS = 10
 USER = 'Cyrax_011'
 PASS = 'Night#India065'
 
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '\
+             'AppleWebKit/537.36 (KHTML, like Gecko) '\
+             'Chrome/81.0.4044.138 Safari/537.36'
 
-class DemonForumsSpider(BypassCloudfareSpider):
+
+class DemonForumsSpider(SitemapSpider):
     name = 'demonforums_spider'
 
     use_proxy = True
@@ -29,21 +42,78 @@ class DemonForumsSpider(BypassCloudfareSpider):
 
     rotation_tries = 0
 
+    base_url = 'https://demonforums.net/'
+    avatar_name_pattern = re.compile(r'avatar_(\d+\.\w+)')
+    pagination_pattern = re.compile(r'.*page=(\d+)')
+
+    # # Xpath stuffs
+    forum_xpath = '//a[contains(@href, "Forum-")]/@href'
+    thread_xpath = '//tr[contains(@class,"inline_row")]'
+
+    thread_first_page_xpath = '//span[contains(@class, "subject_")'\
+                              ' and contains(@id, "tid_")]/a'
+
+    thread_last_page_xpath = '//td[contains(@class,"lastpost")]//'\
+                             'a[text()="Last Post"]/@href'
+
+    # thread date later
+    thread_date_xpath = '//td[contains(@class,"last_post")]/span/span/@title|'\
+                        '//td[contains(@class,"last_post")]/span/a[last()]/'\
+                        'following-sibling::text()'
+
+    pagination_xpath = '//a[contains(@class,"pagination_next")]/@href'
+
+    thread_pagination_xpath = '//a[contains(@class,"pagination_previous")]'\
+                              '/@href'
+
+    thread_page_xpath = '//span[contains(@class,"pagination_current")]/text()'
+
+    post_date_xpath = '//span[@class="post_date"]/span/@title|//span'\
+                      '[@class="post_date"]/text()'
+
+    avatar_xpath = '//div[@class="author_avatar"]//img/@src'
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.base_url = 'https://demonforums.net/'
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page=(\d+)')
         self.headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            'accept': 'text/html,application/xhtml+xml,application/xml'
+                      ';q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                      'application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'referer': 'https://demonforums.net/',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6)'
+                          ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+                          '85.0.4183.83 Safari/537.36'
         }
 
     def start_requests(self):
+        cookies, ip = self.get_cookies(
+            base_url=self.base_url,
+            proxy=self.use_proxy,
+            fraud_check=True,
+            check_captcha=True
+        )
+
+        # Init request kwargs and meta
+        meta = {
+            "cookiejar": uuid.uuid1().hex,
+            "ip": ip
+        }
+
+        self.logger.info(f'COOKIES: {cookies}')
         yield Request(
             url=self.base_url,
             headers=self.headers,
             callback=self.process_login,
+            cookies=cookies,
+            meta=meta,
             dont_filter=True
         )
 
@@ -71,6 +141,8 @@ class DemonForumsSpider(BypassCloudfareSpider):
 
     def process_login(self, response):
 
+        self.synchronize_headers(response)
+
         if response.status == 403:
             yield from self.parse_captcha(response)
             return
@@ -79,11 +151,14 @@ class DemonForumsSpider(BypassCloudfareSpider):
             '//input[@name="my_post_key"]/@value').extract_first()
         if not my_post_key:
             return
+
+        self.logger.info("Found post_key")
+
         form_data = {
             'action': 'do_login',
             'url': '',
             'quick_login': '1',
-            'my_post_key':  my_post_key,
+            'my_post_key': my_post_key,
             'quick_username': USER,
             'quick_password': PASS,
             'quick_remember': 'yes',
@@ -95,11 +170,13 @@ class DemonForumsSpider(BypassCloudfareSpider):
             formdata=form_data,
             callback=self.parse,
             headers=response.request.headers,
-            meta=response.meta,
-            dont_filter=True
+            meta=self.synchronize_meta(response),
+            dont_filter=True,
         )
 
     def parse(self, response):
+
+        self.synchronize_headers(response)
 
         if response.status == 403:
             yield from self.parse_captcha(response)
@@ -109,167 +186,39 @@ class DemonForumsSpider(BypassCloudfareSpider):
             url=self.base_url,
             callback=self.parse_start,
             headers=response.request.headers,
-            meta=response.meta,
+            meta=self.synchronize_meta(response),
             dont_filter=True
         )
 
     def parse_start(self, response):
 
-        if response.status == 403:
-            meta = response.meta.copy()
-            tries = meta.get('tries', 1)
-            if tries < 20:
-                meta['tries'] = tries + 1
-                yield Request(
-                    url=self.base_url,
-                    callback=self.parse_start,
-                    headers=response.request.headers,
-                    meta=meta,
-                    dont_filter=True
-                )
-                return
+        # Synchronize user agent for cloudfare middlewares
+        self.synchronize_headers(response)
 
-        forums = response.xpath(
-            '//a[contains(@href, "Forum-")]')
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            # if 'Forum-Graphics--36' not in url:
-            #     continue
+        # If captcha detected
+        if response.status in [503, 403]:
+            yield from self.parse_captcha(response)
+            return
+
+        # Load all forums
+        all_forums = response.xpath(self.forum_xpath).extract()
+
+        for forum_url in all_forums:
+            # Standardize url
+            if 'http://' not in forum_url and 'https://' not in forum_url:
+                if self.base_url not in forum_url:
+                    forum_url = self.base_url + forum_url
+
             yield Request(
-                url=url,
+                url=forum_url,
+                headers=self.headers,
                 callback=self.parse_forum,
-                headers=response.request.headers,
-                meta=response.meta,
-            )
-
-    def parse_forum(self, response):
-
-        if response.status == 403:
-            meta = response.meta.copy()
-            tries = meta.get('tries', 1)
-            if tries < 20:
-                meta['tries'] = tries + 1
-                yield Request(
-                    url=response.url,
-                    callback=self.parse_forum,
-                    headers=response.request.headers,
-                    meta=meta,
-                    dont_filter=True
-                )
-                return
-
-        self.logger.info('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//span[contains(@class, "subject_") and contains(@id, "tid_")]/a')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = str(
-                int.from_bytes(
-                    thread_url.replace('Thread-', '').encode('utf-8'),
-                    byteorder='big'
-                ) % (10 ** 7)
-            )
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
-            if os.path.exists(file_name):
-                continue
-            meta = response.meta
-            meta.update({
-                'topic_id': topic_id
-            })
-            yield Request(
-                url=thread_url,
-                callback=self.parse_thread,
-                headers=response.request.headers,
-                meta=meta
-            )
-
-        next_page = response.xpath(
-            '//a[@class="pagination_next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                callback=self.parse_forum,
-                headers=response.request.headers,
-                meta=response.meta
+                meta=self.synchronize_meta(response)
             )
 
     def parse_thread(self, response):
 
-        if response.status == 403:
-            meta = response.meta.copy()
-            tries = meta.get('tries', 1)
-            if tries < 20:
-                meta['tries'] = tries + 1
-                yield Request(
-                    url=response.url,
-                    callback=self.parse_forum,
-                    headers=response.request.headers,
-                    meta=meta,
-                    dont_filter=True
-                )
-                return
-
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            self.logger.info(f'{topic_id}-{paginated_value} done..!')
-
-        avatars = response.xpath(
-            '//div[@class="author_avatar"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if 'svg+xml' in avatar_url:
-                continue
-            if not avatar_url.startswith('http'):
-                avatar_url = self.base_url + avatar_url
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            meta = response.meta
-            meta.update({
-                'file_name': file_name
-            })
-            yield Request(
-                url=avatar_url,
-                callback=self.parse_avatar,
-                headers=response.request.headers,
-                meta=meta
-            )
-
-        next_page = response.xpath(
-            '//div[@class="pagination"]'
-            '/a[@class="pagination_next"]')
-        meta = response.meta
-        meta.update({
-            'topic_id': topic_id
-        })
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                callback=self.parse_thread,
-                headers=response.request.headers,
-                meta=response.meta
-            )
-
-    def parse_avatar(self, response):
+        self.synchronize_headers(response)
 
         if response.status == 403:
             meta = response.meta.copy()
@@ -280,17 +229,13 @@ class DemonForumsSpider(BypassCloudfareSpider):
                     url=response.url,
                     callback=self.parse_forum,
                     headers=response.request.headers,
-                    meta=meta,
+                    meta=self.synchronize_meta(response),
                     dont_filter=True
                 )
                 return
 
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            self.logger.info(f"Avatar {file_name_only} done..!")
-
+        yield from super().parse_thread(response)
+        yield from super().parse_avatars(response)
 
 class DemonForumsScrapper(SiteMapScrapper):
 
