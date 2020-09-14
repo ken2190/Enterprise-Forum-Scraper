ac78d18d13ae136df793e57aa4d24097edca4d9c
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-09-14 23:03:40 +0545

    dark2web parser null author issue fix !

--- templates/dark2web_template.py
+++ templates/dark2web_template.py
@@ -11,23 +11,16 @@ class Dark2WebParser(BaseTemplate):
         locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
         super().__init__(*args, **kwargs)
         self.parser_name = "dark2web.ru"
-        self.thread_name_pattern = re.compile(
-            r'(\d+).*html$'
-        )
-        self.pagination_pattern = re.compile(
-            r'\d+-(\d+)\.html$'
-        )
         self.avatar_name_pattern = re.compile(r'.*/(\w+\.\w+)')
-        self.files = self.get_filtered_files(kwargs.get('files'))
         self.comments_xpath = '//div[@class="message-inner"]'
         self.header_xpath = '//div[@class="message-inner"]'
         self.date_xpath = 'div//div[@class="message-attribution-main"]/a/time/@title'
         self.date_pattern = "%d.%m.%Y в %H:%M"
-        self.title_xpath = '//h1[@class="p-title-value"]/text()'
-        self.post_text_xpath = 'div//div[@class="bbWrapper"]/descendant::text()[not(ancestor::div[contains(@class, "bbCodeBlock bbCodeBlock--expandable bbCodeBlock--quote")])]'
+        self.title_xpath = '//h1[@class="p-title-value"]//text()'
+        self.post_text_xpath = 'div//div[@class="bbWrapper"]/descendant::text()[not(ancestor::blockquote)]'
         self.avatar_xpath = 'div//div[@class="message-avatar-wrapper"]/a/img/@src'
         self.comment_block_xpath = 'div//header[@class="message-attribution message-attribution--split"]//a/text()'
-        self.author_xpath = '//div[contains(@class, "nadavoi")]//span[contains(@class, "username--style")]/text()'
+        self.author_xpath = './/div[contains(@class, "nadavoi")]//a[contains(@class, "username")]//text()'
 
         # main function
         self.main()
