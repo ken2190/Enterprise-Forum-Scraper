0901d61e9d8462fd47343cb59ab10b578ceaa6e4
Umesh Pathak <umeshpd.pathak@gmail.com>
2020-10-30 09:39:50 +0545

    skynetzone scraper changes to support recaptcha

--- scraper/skynetzone.py
+++ scraper/skynetzone.py
@@ -1,170 +1,113 @@
-import time
-import requests
 import os
-import json
 import re
-import scrapy
-from math import ceil
-import configparser
-from urllib.parse import urlencode
-from lxml.html import fromstring
-from scrapy.http import Request, FormRequest
-from scrapy.crawler import CrawlerProcess
-from scraper.base_scrapper import SiteMapScrapper
+import uuid
+import json
+import dateparser
 
+from scrapy import (
+    Request,
+    FormRequest
+)
+from scraper.base_scrapper import (
+    SitemapSpider,
+    SiteMapScrapper
+)
 
-REQUEST_DELAY = 0.3
-NO_OF_THREADS = 10
+USERNAME = "Cyrax_011"
+PASSWORD = "Night#India065"
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) '\
-             'AppleWebKit/537.36 (KHTML, like Gecko) '\
-             'Chrome/79.0.3945.117 Safari/537.36',
+class SkyNetZoneSpider(SitemapSpider):
 
-
-class SkyNetZoneSpider(scrapy.Spider):
     name = 'skynetzone_spider'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.base_url = "https://skynetzone.pw"
-        self.topic_pattern = re.compile(r'threads/.*\.(\d+)/')
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*/page-(\d+)')
-        self.start_url = "https://skynetzone.pw/"
-        self.output_path = kwargs.get("output_path")
-        self.avatar_path = kwargs.get("avatar_path")
-        self.headers = {
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'user-agent': USER_AGENT
-        }
+    # Url stuffs
+    base_url = 'https://skynetzone.pw'
+    login_url = f'{base_url}/login/login'
+
+    # Regex stuffs
+    topic_pattern = re.compile(r'threads/(\d+)')
+    avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
+    pagination_pattern = re.compile(r'.*page-(\d+)')
+
+    # Xpath stuffs
+    login_form_xpath = "//form[@method='post']"
+    forum_xpath = "//a[contains(@href,\"/forums\")]/@href"
+    pagination_xpath = "//a[contains(@class,\"pageNav-jump--next\")]/@href"
+    thread_xpath = "//div[contains(@class,\"structItem--thread\")]"
+    thread_first_page_xpath = "//div[@class=\"structItem-title\"]/a/@href"
+    thread_last_page_xpath = "//span[@class=\"structItem-pageJump\"]/"\
+                             "a[last()]/@href"
+    thread_date_xpath = "//time[contains(@class,\"structItem-latestDate\")]"\
+                        "/@title"
+    thread_pagination_xpath = "//a[contains(@class,\"pageNav-jump--prev\")]"\
+                              "/@href"
+    thread_page_xpath = "//li[contains(@class,\"pageNav-page--current\")]"\
+                        "/a/text()"
+    post_date_xpath = "//div[@class=\"message-attribution-main\"]"\
+                      "/a[@class=\"u-concealed\"]/time/@title"
+
+    avatar_xpath = "//img[contains(@class,\"avatar\")]/@src"
+
+    # Recaptcha stuffs
+    recaptcha_site_key_xpath = '//div[@data-sitekey]/@data-sitekey'
+
+    # Other settings
+    use_proxy = True
+    download_delay = 0.3
+    download_thread = 10
+
+    def parse_thread_date(self, thread_date):
+        thread_date = thread_date.strip()[:-5]
+        if not thread_date:
+            return
+        return dateparser.parse(thread_date)
+
+    def parse_post_date(self, post_date):
+        post_date = post_date.strip()[:-5]
+        return dateparser.parse(post_date)
 
     def start_requests(self):
         yield Request(
-            url=self.start_url,
+            url=self.login_url,
+            dont_filter=True,
             headers=self.headers,
-            callback=self.parse
+            callback=self.parse_login,
         )
 
-    def parse(self, response):
-        forums = response.xpath(
-            '//h3[@class="node-title"]/a')
-        sub_forums = response.xpath(
-            '//a[contains(@class, "subNodeLink subNodeLink--forum")]')
-        forums.extend(sub_forums)
-
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
-            # if 'ischu-rabotu-predlagaju-svoi-uslugi.95' not in url:
-            #     continue
-            yield Request(
-                url=url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
-
-    def parse_forum(self, response):
-        self.logger.info('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//a[@data-preview-url]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id[0]}
-            )
-
-        next_page = response.xpath(
-            '//a[@class="pageNav-jump pageNav-jump--next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
+    def parse_login(self, response):
+        self.synchronize_headers(response)
+        # Solve hcaptcha
+        captcha_response = self.solve_recaptcha(response)
+        self.logger.debug(f'captcha_response: {captcha_response}')
+        token = response.xpath(
+            '//input[@name="_xfToken"]/@value').extract_first()
+        formdata = {
+            'login': USERNAME,
+            'password': PASSWORD,
+            'g-recaptcha-response': captcha_response,
+            "remember": '1',
+            '_xfRedirect': self.base_url,
+            '_xfToken': token
+        }
+        yield FormRequest.from_response(
+            response,
+            formxpath=self.login_form_xpath,
+            formdata=formdata,
+            meta=self.synchronize_meta(response),
+            dont_filter=True,
+            headers=self.headers,
+        )
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            self.logger.info(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath(
-            '//div[@class="message-avatar-wrapper"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if self.base_url not in avatar_url:
-                avatar_url = self.base_url + avatar_url
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
+        # Parse generic thread
+        yield from super().parse_thread(response)
 
-        next_page = response.xpath(
-            '//a[@class="pageNav-jump pageNav-jump--next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        file_name_only = file_name_only.rsplit('.', 1)[0]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            self.logger.info(f"Avatar for {file_name_only} done..!")
+        # Parse generic avatar
+        yield from super().parse_avatars(response)
 
 
 class SkyNetZoneScrapper(SiteMapScrapper):
 
     spider_class = SkyNetZoneSpider
     site_name = 'skynetzone.pw'
-
-    def load_settings(self):
-        spider_settings = super().load_settings()
-        spider_settings.update(
-            {
-                'DOWNLOAD_DELAY': REQUEST_DELAY,
-                'CONCURRENT_REQUESTS': NO_OF_THREADS,
-                'CONCURRENT_REQUESTS_PER_DOMAIN': NO_OF_THREADS
-            }
-        )
-        return spider_settings
