8cc6e9f257dde334d562c30a919bb1fc0fab0005
Sergey Totmyanin <STotmyanin@gmail.com>
2020-10-31 14:51:35 +0300

    Skynetzone: rework the scraper, fix logging in issue

--- scraper/skynetzone.py
+++ scraper/skynetzone.py
@@ -1,8 +1,4 @@
-import os
 import re
-import uuid
-import json
-import dateparser
 
 from scrapy import (
     Request,
@@ -15,6 +11,8 @@ from scraper.base_scrapper import (
 
 USERNAME = "Cyrax_011"
 PASSWORD = "Night#India065"
+MAX_TRY_TO_LOG_IN_COUNT = 3
+
 
 class SkyNetZoneSpider(SitemapSpider):
 
@@ -24,49 +22,43 @@ class SkyNetZoneSpider(SitemapSpider):
     base_url = 'https://skynetzone.pw'
     login_url = f'{base_url}/login/login'
 
-    # Regex stuffs
-    topic_pattern = re.compile(r'threads/(\d+)')
-    avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-    pagination_pattern = re.compile(r'.*page-(\d+)')
-
     # Xpath stuffs
-    login_form_xpath = "//form[@method='post']"
-    forum_xpath = "//a[contains(@href,\"/forums\")]/@href"
-    pagination_xpath = "//a[contains(@class,\"pageNav-jump--next\")]/@href"
-    thread_xpath = "//div[contains(@class,\"structItem--thread\")]"
-    thread_first_page_xpath = "//div[@class=\"structItem-title\"]/a/@href"
-    thread_last_page_xpath = "//span[@class=\"structItem-pageJump\"]/"\
-                             "a[last()]/@href"
-    thread_date_xpath = "//time[contains(@class,\"structItem-latestDate\")]"\
-                        "/@title"
-    thread_pagination_xpath = "//a[contains(@class,\"pageNav-jump--prev\")]"\
-                              "/@href"
-    thread_page_xpath = "//li[contains(@class,\"pageNav-page--current\")]"\
-                        "/a/text()"
-    post_date_xpath = "//div[@class=\"message-attribution-main\"]"\
-                      "/a[@class=\"u-concealed\"]/time/@title"
+    login_form_xpath = "//form[@action='/login/login']"
 
-    avatar_xpath = "//img[contains(@class,\"avatar\")]/@src"
+    forum_xpath = '//h3[@class="node-title"]/a/@href|'\
+                  '//a[contains(@class,"subNodeLink--forum")]/@href'
+    thread_xpath = '//div[contains(@class, "structItem structItem--thread")]'
+    thread_first_page_xpath = './/div[@class="structItem-title"]'\
+                              '/a[contains(@href,"threads/")]/@href'
+    thread_last_page_xpath = './/span[@class="structItem-pageJump"]'\
+                             '/a[last()]/@href'
+    thread_date_xpath = './/time[contains(@class, "structItem-latestDate")]'\
+                        '/@datetime'
+    pagination_xpath = '//a[contains(@class,"pageNav-jump--next")]/@href'
+    thread_pagination_xpath = '//a[contains(@class, "pageNav-jump--prev")]'\
+                              '/@href'
+    thread_page_xpath = '//li[contains(@class, "pageNav-page--current")]'\
+                        '/a/text()'
+    post_date_xpath = '//div/a/time[@datetime]/@datetime'
+
+    avatar_xpath = '//div[@class="message-avatar-wrapper"]/a/img/@src'
 
     # Recaptcha stuffs
     recaptcha_site_key_xpath = '//div[@data-sitekey]/@data-sitekey'
+
+    # Regex stuffs
+    topic_pattern = re.compile(r'threads/.*\.(\d+)/', re.IGNORECASE)
+    avatar_name_pattern = re.compile(r".*/(\S+\.\w+)", re.IGNORECASE)
+    pagination_pattern = re.compile(r'.*page-(\d+)', re.IGNORECASE)
 
     # Other settings
     use_proxy = True
     download_delay = 0.3
     download_thread = 10
 
-    def parse_thread_date(self, thread_date):
-        thread_date = thread_date.strip()[:-5]
-        if not thread_date:
-            return
-        return dateparser.parse(thread_date)
-
-    def parse_post_date(self, post_date):
-        post_date = post_date.strip()[:-5]
-        return dateparser.parse(post_date)
-
     def start_requests(self):
+        self._try_to_log_in_count = 0
+
         yield Request(
             url=self.login_url,
             dont_filter=True,
@@ -75,28 +67,54 @@ class SkyNetZoneSpider(SitemapSpider):
         )
 
     def parse_login(self, response):
+        self._try_to_log_in_count += 1
+
         self.synchronize_headers(response)
-        # Solve hcaptcha
-        captcha_response = self.solve_recaptcha(response)
-        self.logger.debug(f'captcha_response: {captcha_response}')
-        token = response.xpath(
-            '//input[@name="_xfToken"]/@value').extract_first()
+
+        # Solve reCAPTCHA
+        solved_captcha = self.solve_recaptcha(response)  # , proxyless=True)
+        self.logger.debug(f'reCAPTCHA token: {solved_captcha.solution.token}')
+
         formdata = {
             'login': USERNAME,
             'password': PASSWORD,
-            'g-recaptcha-response': captcha_response,
-            "remember": '1',
-            '_xfRedirect': self.base_url,
-            '_xfToken': token
+            'g-recaptcha-response': solved_captcha.solution.token
         }
+
         yield FormRequest.from_response(
             response,
             formxpath=self.login_form_xpath,
             formdata=formdata,
+            dont_click=True,
             meta=self.synchronize_meta(response),
             dont_filter=True,
             headers=self.headers,
+            callback=self.check_if_logged_in,
+            cb_kwargs=dict(solved_captcha=solved_captcha)
         )
+
+    def check_if_logged_in(self, response, solved_captcha):
+        # check if logged in successfully
+        if response.xpath(self.forum_xpath):
+            # report a good CAPTCHA
+            solved_captcha.report_good()
+            # start forum scraping
+            yield from super().parse(response)
+            return
+
+        err_msg = response.css('div.blockMessage--error::text').get()
+        if err_msg:
+            if 'CAPTCHA' in err_msg:
+                # report a bad CAPTCHA
+                solved_captcha.report_bad()
+
+            if self._try_to_log_in_count >= MAX_TRY_TO_LOG_IN_COUNT:
+                self.logger.error('Unable to log in! Exceeded maximum try count!')
+                return
+
+            yield from self.parse_login(response)
+        else:
+            self.logger.error('Unable to log in to the forum!')
 
     def parse_thread(self, response):
 
@@ -111,3 +129,11 @@ class SkyNetZoneScrapper(SiteMapScrapper):
 
     spider_class = SkyNetZoneSpider
     site_name = 'skynetzone.pw'
+
+    def load_settings(self):
+        settings = super().load_settings()
+        settings.update({
+            'RETRY_HTTP_CODES': [403, 406, 408, 429, 500, 502, 503, 504, 522, 524],
+            # 'CLOSESPIDER_ERRORCOUNT': 1
+        })
+        return settings
