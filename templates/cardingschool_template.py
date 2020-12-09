b4577a01d3a3fb84380de8f60a54f2c3b16cf86d
jaazielvale <jaazielvale@gmail.com>
2020-12-09 04:36:50 -0500

    forumparser: cardingschool getfiles fix

--- templates/cardingschool_template.py
+++ templates/cardingschool_template.py
@@ -10,12 +10,6 @@ class CardingSchoolParser(BaseTemplate):
     def __init__(self, *args, **kwargs):
         super().__init__(kwargs)
         self.parser_name = "carding.school"
-        self.thread_name_pattern = re.compile(
-            r'(\d+).*html$'
-        )
-        self.pagination_pattern = re.compile(
-            r'\d+-(\d+)\.html'
-        )
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
         self.files = self.get_filtered_files(kwargs.get('files'))
         self.comments_xpath = '//article[@id]'
