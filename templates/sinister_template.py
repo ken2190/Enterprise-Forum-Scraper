77638ef47e54058539a1ce4ae480c920a9f5ae80
jaazielvale <jaazielvale@gmail.com>
2020-12-10 02:31:29 -0500

    parser - sinister update

--- templates/sinister_template.py
+++ templates/sinister_template.py
@@ -1,5 +1,6 @@
 # -- coding: utf-8 --
 import re
+import dateparser
 
 from .base_template import BaseTemplate
 
@@ -12,7 +13,8 @@ class SinisterParser(BaseTemplate):
         self.avatar_name_pattern = re.compile(r'(\d+\.\w+)')
         self.comments_xpath = '//div[@id="posts"]/div'
         self.header_xpath = '//div[@id="posts"]/div'
-        self.date_xpath = 'div//span[@class="post_date postbit_date"]/text()'
+        self.date_xpath_1 = 'div//span[@class="post_date postbit_date"]/text()'
+        self.date_xpath_2 = 'div//span[@class="post_date postbit_date"]/span/@title'
         self.date_pattern = "%m-%d-%Y, %I:%M %p"
         self.author_xpath = 'div//span[@class="largetext postbit_username"]/a/span/text()'
         self.title_xpath = '//table//strong/text()'
@@ -22,3 +24,28 @@ class SinisterParser(BaseTemplate):
 
         # main function
         self.main()
+
+    def get_date(self, tag):
+        date_block = tag.xpath(self.date_xpath_1)
+        date = date_block[0].strip() if date_block else None
+
+        if not date:
+            date_block = tag.xpath(self.date_xpath_2)
+            date = date_block[0].strip() if date_block else None
+
+        # check if date is already a timestamp
+        try:
+            date = dateparser.parse(date).timestamp()
+            return str(date)
+        except:
+            try:
+                date = float(date)
+                return date
+            except:
+                try:
+                    date = datetime.datetime.strptime(date, self.date_pattern).timestamp()
+                    return str(date)
+                except:
+                    pass
+
+        return ""
