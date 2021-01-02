c30449b43afe33c7ba7e94136c394bb01429f88f
nunofragoso <nunofragoso67@gmail.com>
2021-01-02 06:04:59 +0000

    spider: verifiedcarder update

--- scraper/verified_carder.py
+++ scraper/verified_carder.py
@@ -15,176 +15,102 @@ from datetime import datetime
 from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
 
 
 class VerifiedCarderSpider(SitemapSpider):
     name = 'verifiedcarder_spider'
-    # Sitemap Stuffs
-    sitemap_url = 'https://verifiedcarder.ws/sitemap.php'
-    thread_sitemap_xpath = '//url[loc[contains(text(),"/threads/")] and lastmod]'
-    thread_url_xpath = '//loc/text()'
-    thread_lastmod_xpath = "//lastmod/text()"
-    sitemap_datetime_format = '%Y-%m-%dT%H:%M:%S'
-    custom_settings = {
-        'DOWNLOADER_MIDDLEWARES': {
-            'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None
-        }
-    }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.base_url = "https://www.verifiedcarder.ws"
-        self.topic_pattern = re.compile(r'.*\.(\d+)/')
-        self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.pagination_pattern = re.compile(r'.*page-(\d+)')
-        cookies = self.get_cookies()
-        self.logger.info(f'Obtained Cookies: {cookies}')
-        self.start_url = 'https://www.verifiedcarder.ws'
-        self.headers = {
-            'referer': 'https://verifiedcarder.ws/',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'cookie': cookies,
-            "user-agent": USER_AGENT
-        }
+    # Url stuffs
+    base_url = "https://verifiedcarder.cc/"
+    
+    # Xpath stuffs
+    forum_xpath = '//div[@id="ipsLayout_mainArea"]//h4[contains(@class, "ipsDataItem_title ipsType_large")]/a/@href'
 
-    def get_cookies(self,):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument(f'user-agent={USER_AGENT}')
-        browser = webdriver.Chrome(
-            '/usr/local/bin/chromedriver',
-            chrome_options=chrome_options)
-        browser.get(self.base_url)
-        self.logger.info('Waiting to get cookies from selenium browser')
-        time.sleep(5)
-        cookies = browser.get_cookies()
-        cookies = '; '.join([
-            f"{c['name']}={c['value']}" for c in cookies
-        ])
-        return cookies
+    pagination_xpath = '//a[@class="pagination_next"]/@href'
 
-    def parse_sitemap(self, response):
-        # Load selector
-        selector = Selector(text=response.text)
+    thread_xpath = '//div[@id="ipsLayout_mainArea"]//div[@class="ipsBox"]/ol/li[contains(@class, "ipsDataItem ipsDataItem_responsivePhoto")]'
+    thread_first_page_xpath = './div[@class="ipsDataItem_main"]/h4//a/@href'
+    thread_last_page_xpath = './div[@class="ipsDataItem_main"]/h4//span[contains(@class, "ipsPagination_mini")]/span[last()]/a/@href'
+    thread_date_xpath = './ul[contains(@class, "ipsDataItem_lastPoster")]//time/@datetime'
 
-        # Load forum
-        all_threads = selector.xpath(self.thread_sitemap_xpath).extract()
+    thread_pagination_xpath = '//ul[@class="ipsPagination"]//li[@class="ipsPagination_prev"]/a/@href'
+    thread_page_xpath = '//ul[@class="ipsPagination"]/li[contains(@class, "ipsPagination_active")]/a/text()'
+    post_date_xpath = '//div[@class="ipsType_reset"]//time/@datetime'
 
-        for thread in all_threads:
-            yield from self.parse_sitemap_thread(thread, response)
+    avatar_xpath = '//div[@class="cAuthorPane_photo"]//img/@src'
+
+    # Regex stuffs
+    avatar_name_pattern = re.compile(
+        r".*/(\S+\.\w+)",
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r".*page/(\d+)",
+        re.IGNORECASE
+    )
+
+    # Other settings
+    sitemap_datetime_format = "%Y-%m-%dT%H:%M:%S"
+    post_datetime_format = "%Y-%m-%dT%H:%M:%S"
 
     def parse_thread_date(self, thread_date):
+        """
+        :param thread_date: str => thread date as string
+        :return: datetime => thread date as datetime converted from string,
+                            using class sitemap_datetime_format
+        """
+
         return datetime.strptime(
-            thread_date.split('+')[0],
+            thread_date.strip()[:-1],
             self.sitemap_datetime_format
         )
 
+    def parse_post_date(self, post_date):
+        """
+        :param post_date: str => post date as string
+        :return: datetime => post date as datetime converted from string,
+                            using class post_datetime_format
+        """
+        return datetime.strptime(
+            post_date.strip()[:-1],
+            self.post_datetime_format
+        )
+
     def parse(self, response):
-        forums = response.xpath(
-            '//h3[@class="node-title"]/a')
-        sub_forums = response.xpath(
-            '//ol[@class="node-subNodeFlatList"]/li/a')
-        forums.extend(sub_forums)
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
+
+        # Synchronize user agent for cloudfare middleware
+        self.synchronize_headers(response)
+
+        # Load all forums
+        all_forums = response.xpath(self.forum_xpath).extract()
+        for forum_url in all_forums:
+            # Standardize forum url
+            if self.base_url not in forum_url:
+                forum_url = response.urljoin(forum_url)
 
             yield Request(
-                url=url,
+                url=forum_url,
                 headers=self.headers,
+                meta=self.synchronize_meta(response),
                 callback=self.parse_forum
             )
 
     def parse_forum(self, response):
-        self.logger.info('next_page_url: {}'.format(response.url))
-        threads = response.xpath(
-            '//a[@data-preview-url]')
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-            topic_id = self.topic_pattern.findall(thread_url)
-            if not topic_id:
-                continue
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id[0]}
-            )
 
-        next_page = response.xpath(
-            '//a[@class="pageNav-jump pageNav-jump--next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_forum
-            )
+        # Parse sub forums
+        yield from self.parse(response)
+
+        # Parse generic forum
+        yield from super().parse_forum(response)
 
     def parse_thread(self, response):
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            self.logger.info(f'{topic_id}-{paginated_value} done..!')
 
-        avatars = response.xpath(
-            '//div[@class="message-avatar-wrapper"]//a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if not avatar_url:
-                continue
-            if not avatar_url.startswith('http'):
-                avatar_url = self.base_url + avatar_url
-            match = self.avatar_name_pattern.findall(avatar_url)
-            if not match:
-                continue
-            file_name = '{}/{}'.format(self.avatar_path, match[0])
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta={
-                    'file_name': file_name,
-                }
-            )
+        # Save generic thread
+        yield from super().parse_thread(response)
 
-        next_page = response.xpath(
-            '//a[@class="pageNav-jump pageNav-jump--next"]')
-        if next_page:
-            next_page_url = next_page.xpath('@href').extract_first()
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta={'topic_id': topic_id}
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            self.logger.info(f"Avatar for {file_name_only} done..!")
+        # Save avatars
+        yield from super().parse_avatars(response)
 
 
 class VerifiedCarderScrapper(SiteMapScrapper):
@@ -203,7 +129,3 @@ class VerifiedCarderScrapper(SiteMapScrapper):
             }
         )
         return spider_settings
-
-
-if __name__ == '__main__':
-    run_spider('/Users/PathakUmesh/Desktop/BlackHatWorld')
