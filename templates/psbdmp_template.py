c57ae587bfbc1d40ef6af5b97bdd6d84a9cad73d
nunofragoso <nunofragoso67@gmail.com>
2021-01-04 07:22:38 +0000

    parser: psbdmp update

--- templates/psbdmp_template.py
+++ templates/psbdmp_template.py
@@ -3,6 +3,7 @@ import os
 import traceback
 import utils
 import dateutil.parser as dparser
+import json
 
 from .base_template import BaseTemplate
 
@@ -48,17 +49,18 @@ class PsbdmpParser(BaseTemplate):
 
     def process_file(self, input_file_path, output_file_path, ts):
         paste_id = input_file_path.rsplit('/', 1)[-1].rsplit('.', 1)[0]
-        data = {
-            '_source': {
-                'source': 'pastebin',
-                'date': ts,
-                'paste_id': paste_id
-            }
-        }
 
         with open(input_file_path, 'r') as fp:
             content = fp.read()
-            data['_source'].update({'content': content})
+            json_data = json.loads(content)
+            data = {
+                '_source': {
+                    'source': 'pastebin',
+                    'paste_id': paste_id
+                }
+            }
+            data['_source'].update({'date': dparser.parse(json_data["date"]).timestamp()})
+            data['_source'].update({'content': json_data["content"]})
 
             with open(output_file_path, 'a', encoding='utf-8') as file_pointer:
                 utils.write_json(file_pointer, data)
