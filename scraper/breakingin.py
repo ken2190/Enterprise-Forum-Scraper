5a8e364f073dd16974b03578735de92a7e7c1981
jaazielvale <jaazielvale@gmail.com>
2021-02-04 10:57:00 +0000

    breakingin: update cloudflare

--- scraper/breakingin.py
+++ scraper/breakingin.py
@@ -1,4 +1,5 @@
 import re
+import uuid
 
 from datetime import (
     datetime,
@@ -46,6 +47,8 @@ class BreakinginSpider(SitemapSpider):
         re.IGNORECASE
     )
 
+    use_proxy = "On"
+
     # Other settings
     sitemap_datetime_format = "%Y-%m-%dT%H:%M:%S"
     post_datetime_format = "%Y-%m-%dT%H:%M:%S"
@@ -72,6 +75,36 @@ class BreakinginSpider(SitemapSpider):
             post_date.strip()[:-1],
             self.post_datetime_format
         )
+    
+    def start_requests(self):
+        # Temporary action to start spider
+        yield Request(
+            url=self.temp_url,
+            headers=self.headers,
+            callback=self.pass_cloudflare
+        )
+
+    def pass_cloudflare(self, response):
+        # Load cookies and ip
+        cookies, ip = self.get_cloudflare_cookies(
+            base_url=self.base_url,
+            proxy=True,
+            fraud_check=True
+        )
+
+        # Init request kwargs and meta
+        meta = {
+            "cookiejar": uuid.uuid1().hex,
+            "ip": ip
+        }
+
+        yield Request(
+            url=self.base_url,
+            headers=self.headers,
+            meta=meta,
+            cookies=cookies,
+            callback=self.parse
+        )
 
     def parse(self, response):
 
@@ -83,6 +116,7 @@ class BreakinginSpider(SitemapSpider):
 
         # update stats
         self.crawler.stats.set_value("mainlist/mainlist_count", len(all_forums))
+        
         for forum_url in all_forums:
             # Standardize forum url
             if self.base_url not in forum_url:
