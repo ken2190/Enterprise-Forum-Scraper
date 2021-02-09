54c1439fd117bd88882578abaf1e0aa1cf85f56b
jaazielvale <jaazielvale@gmail.com>
2021-02-09 13:23:24 +0000

    spider: updated xss

--- scraper/xss.py
+++ scraper/xss.py
@@ -13,8 +13,8 @@ from scraper.base_scrapper import SitemapSpider, SiteMapScrapper
 
 # USER = "cyrax6626"
 # PASS = "#@nightlion6626"
-USER = "ronnyboy18@protonmail.ch"
-PASS = "Xs_Rb118!"
+USER = "kennyg"
+PASS = "KXpro218gj2"
 
 class XSSSpider(SitemapSpider):
     name = "xss_spider"
