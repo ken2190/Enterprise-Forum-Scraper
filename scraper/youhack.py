f1e2ebf1e19d0d81e329cf7fa0d406452d6e4d9d
nunofragoso <nunofragoso67@gmail.com>
2021-03-10 02:58:14 +0000

    update: youhack spider

--- scraper/youhack.py
+++ scraper/youhack.py
@@ -20,211 +20,121 @@ class YouHackSpider(SitemapSpider):
 
     name = 'youhack_spider'
 
-    base_url = "https://youhack.ru/"
-    sitemap_url = "https://youhack.ru/sitemap.php"
+    base_url = "https://youhack.xyz/"
 
     # Xpath stuffs
-    forum_sitemap_xpath = "//sitemap/loc/text()"
-    thread_sitemap_xpath = "//url[loc[contains(text(),\"/threads/\")] and lastmod]"
-    thread_url_xpath = "//loc/text()"
-    thread_lastmod_xpath = "//lastmod/text()"
-    forbidden_text_xpath = "//h1/text()[contains(.,\"Forbidden\")]"
+    captcha_form_css = "form[action]"
+    forum_xpath = '//h3[@class="nodeTitle"]/a[contains(@href, "forums/")]/@href|'\
+                    '//ol[@class="subForumList"]//h4[@class="nodeTitle"]/a/@href'
+    thread_xpath = '//li[contains(@id, "thread-")]'
+    thread_first_page_xpath = './/h3[@class="title"]'\
+                              '/a[contains(@href,"threads/")]/@href'
+    thread_last_page_xpath = './/span[@class="itemPageNav"]'\
+                             '/a[last()]/@href'
+    thread_date_xpath = './/dl[@class="lastPostInfo"]'\
+                        '//a[@class="dateTime"]/*/@title'
+    pagination_xpath = '//nav/a[last()]/@href'
+    thread_pagination_xpath = '//nav/a[@class="text"]/@href'
+    thread_page_xpath = '//nav//a[contains(@class, "currentPage")]'\
+                        '/text()'
+    post_date_xpath = '//div[@class="messageDetails"]'\
+                      '//span[@class="DateTime"]/text()|'\
+                      '//div[@class="messageDetails"]'\
+                      '//abbr[@class="DateTime"]/@data-datestring'
 
+    avatar_xpath = '//div[@class="avatarHolder"]/a/img/@src'
+    
+    recaptcha_site_key_xpath = '//div[@class="g-recaptcha"]/@data-sitekey'
     # Regex stuffs
-    topic_pattern = re.compile(r'threads/(\d+)')
-    avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-    pagination_pattern = re.compile(r'.*page-(\d+)')
+    topic_pattern = re.compile(
+        r"threads/(\d+)/",
+        re.IGNORECASE
+    )
+    avatar_name_pattern = re.compile(
+        r".*/(\w+\.\w+)",
+        re.IGNORECASE
+    )
+    pagination_pattern = re.compile(
+        r"page-(\d+)",
+        re.IGNORECASE
+    )
 
     # Other settings
-    sitemap_datetime_format = "%Y-%m-%dT%H:%M:%S"
-    handle_httpstatus_list = [403]
+    use_proxy = "On"
+    sitemap_datetime_format = "%d/%m/%y"
+    post_datetime_format = "%d/%m/%y"
 
-    use_proxy = 'On'
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.headers.update(
-            {
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": 'none',
-                "sec-fetch-user": "?1",
+    def start_requests(self,):
+        yield Request(
+            url=self.base_url,
+            headers=self.headers,
+            callback=self.parse_captcha,
+            meta={
+                "cookiejar": uuid.uuid1().hex
+            },
+            dont_filter=True,
+        )
+
+    def parse_captcha(self, response):
+        # Synchronize cloudfare user agent
+        self.synchronize_headers(response)
+
+        if response.xpath(self.recaptcha_site_key_xpath):
+            formdata = {
+                "g-recaptcha-response": self.solve_recaptcha(response).solution.token,
+                "swp_sessionKey": response.xpath("//input[@name='swp_sessionKey']/@value").extract_first()
             }
-        )
 
-    def parse_thread_date(self, thread_date):
-        return datetime.strptime(
-            thread_date[:-6],
-            self.sitemap_datetime_format
-        )
-
-    def parse_sitemap(self, response):
-
-        # Check forbidden
-        forbidden_text = response.xpath(
-            self.forbidden_text_xpath
-        ).extract_first()
-
-        # Process out of forbidden
-        if response.status == 403 and forbidden_text:
-            self.logger.info(
-                "Rotated ip has been forbidden, rotating."
+            yield FormRequest.from_response(
+                response,
+                formcss=self.captcha_form_css,
+                formdata=formdata,
+                meta=self.synchronize_meta(response),
+                dont_filter=True,
+                headers=self.headers,
+                callback=self.parse
             )
-            request = response.request
-            request.dont_filter = True
-            request.meta["cookiejar"] = uuid.uuid1().hex
-            yield request
         else:
-            yield from super().parse_sitemap(response)
-
-    def start_requests(self):
-        yield from super().start_requests(
-            uuid.uuid1().hex
+            yield Request(
+            url=self.base_url,
+            headers=self.headers,
+            callback=self.parse,
+            meta={
+                "cookiejar": uuid.uuid1().hex
+            },
+            dont_filter=True,
         )
-
-    def parse_sitemap_forum(self, response):
-
-        # Load selector
-        selector = Selector(text=gunzip(response.body))
-
-        # Load thread
-        all_threads = selector.xpath(self.thread_sitemap_xpath).extract()
-
-        for thread in all_threads:
-            yield from self.parse_sitemap_thread(thread, response)
 
     def parse(self, response):
-        # Synchronize header user agent with cloudfare middleware
+        # Synchronize user agent for cloudfare middleware
         self.synchronize_headers(response)
 
-        forums = response.xpath(
-            '//ol[@class="nodeList"]//h3[@class="nodeTitle"]'
-            '/a[contains(@href, "forums/")]')
-        subforums = response.xpath(
-            '//ol[@class="subForumList"]//h4[@class="nodeTitle"]'
-            '/a[contains(@href, "forums/")]')
-        forums.extend(subforums)
-        for forum in forums:
-            url = forum.xpath('@href').extract_first()
-            if self.base_url not in url:
-                url = self.base_url + url
+        # Load all forums
+        all_forums = response.xpath(self.forum_xpath).extract()
+
+        # update stats
+        self.crawler.stats.set_value("mainlist/mainlist_count", len(all_forums))
+
+        for forum_url in all_forums:
+
+            # Standardize url
+            if self.base_url not in forum_url:
+                forum_url = self.base_url + forum_url
 
             yield Request(
-                url=url,
+                url=forum_url,
                 headers=self.headers,
-                callback=self.parse_forum,
-                meta=self.synchronize_meta(response)
-            )
-
-    def parse_forum(self, response):
-
-        # Synchronize header user agent with cloudfare middleware
-        self.synchronize_headers(response)
-
-        self.logger.info(
-            "Next_page_url: %s" % response.request.url
-        )
-
-        threads = response.xpath(
-            '//ol[@class="discussionListItems"]/li//h3[@class="title"]/a'
-        )
-
-        for thread in threads:
-            thread_url = thread.xpath('@href').extract_first()
-            if self.base_url not in thread_url:
-                thread_url = self.base_url + thread_url
-
-            topic_id = self.get_topic_id(thread_url)
-            if not topic_id:
-                continue
-
-            file_name = '{}/{}-1.html'.format(self.output_path, topic_id)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=thread_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta=self.synchronize_meta(
-                    response,
-                    default_meta={'topic_id': topic_id}
-                )
-            )
-
-        next_page_url = response.xpath(
-            '//nav/a[text()="Вперёд >"]/@href').extract_first()
-        if next_page_url:
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
+                meta=self.synchronize_meta(response),
                 callback=self.parse_forum
             )
 
     def parse_thread(self, response):
 
-        # Synchronize header user agent with cloudfare middleware
-        self.synchronize_headers(response)
+        # Save generic thread
+        yield from super().parse_thread(response)
 
-        topic_id = response.meta['topic_id']
-        pagination = self.pagination_pattern.findall(response.url)
-        paginated_value = pagination[0] if pagination else 1
-        file_name = '{}/{}-{}.html'.format(
-            self.output_path, topic_id, paginated_value)
-        with open(file_name, 'wb') as f:
-            f.write(response.text.encode('utf-8'))
-            print(f'{topic_id}-{paginated_value} done..!')
-        self.crawler.stats.inc_value("mainlist/detail_saved_count")
-
-        avatars = response.xpath(
-            '//div[@class="avatarHolder"]/a/img')
-        for avatar in avatars:
-            avatar_url = avatar.xpath('@src').extract_first()
-            if not avatar_url:
-                continue
-            if self.base_url not in avatar_url:
-                avatar_url = self.base_url + avatar_url
-            name_match = self.avatar_name_pattern.findall(avatar_url)
-            if not name_match:
-                continue
-            name = name_match[0]
-            file_name = '{}/{}'.format(self.avatar_path, name)
-            if os.path.exists(file_name):
-                continue
-            yield Request(
-                url=avatar_url,
-                headers=self.headers,
-                callback=self.parse_avatar,
-                meta=self.synchronize_meta(
-                    response,
-                    default_meta={
-                        'file_name': file_name,
-                    }
-                )
-            )
-
-        next_page_url = response.xpath(
-            '//nav/a[text()="Вперёд >"]/@href').extract_first()
-        if next_page_url:
-            if self.base_url not in next_page_url:
-                next_page_url = self.base_url + next_page_url
-            yield Request(
-                url=next_page_url,
-                headers=self.headers,
-                callback=self.parse_thread,
-                meta=self.synchronize_meta(
-                    response,
-                    default_meta={'topic_id': topic_id}
-                )
-            )
-
-    def parse_avatar(self, response):
-        file_name = response.meta['file_name']
-        file_name_only = file_name.rsplit('/', 1)[-1]
-        with open(file_name, 'wb') as f:
-            f.write(response.body)
-            print(f"Avatar {file_name_only} done..!")
-        self.crawler.stats.inc_value("mainlist/avatar_saved_count")
+        # Save avatars
+        yield from super().parse_avatars(response)
         
 
 class YouHackScrapper(SiteMapScrapper):
