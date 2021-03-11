f12652e2c49607b6aa22166f94f2bfcbdc26946e
nunofragoso <nunofragoso67@gmail.com>
2021-03-11 10:17:16 +0000

    update: xaker parser

--- templates/xaker_template.py
+++ templates/xaker_template.py
@@ -16,7 +16,22 @@ class XakerParser(BaseTemplate):
         self.post_text_xpath = './/div[contains(@class,"messageContent")]//article/blockquote/descendant::text()[not(ancestor::div[contains(@class,"bbCodeQuote")])]'
         self.avatar_xpath = './/div[contains(@class,"avatarHolder")]//img/@src'
         self.title_xpath = '//h1/text()'
-        self.index = 1
+        self.index = 0
 
         # main function
         self.main()
+    
+    def get_comment_id(self, tag):
+        comment_id = ""
+        if self.comment_block_xpath:
+            comment_block = tag.xpath(self.comment_block_xpath)
+            comment_block = ''.join(comment_block)
+        else:
+            self.index += 1
+            return str(self.index)
+
+        if comment_block:
+            comment_id = re.compile(r'(\d+)').findall(comment_block)[0]
+            # comment_id = ''.join(comment_block).strip().split('#')[-1]
+
+        return comment_id.replace(',', '').replace('.', '')
