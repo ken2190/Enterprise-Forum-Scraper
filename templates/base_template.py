b65609bf38ed1c91b097b48b771d500db1969083
nunofragoso <nunofragoso67@gmail.com>
2021-03-11 10:41:15 +0000

    update: comment id block update

--- templates/base_template.py
+++ templates/base_template.py
@@ -292,7 +292,7 @@ class BaseTemplate:
         comment_id = ""
         if self.comment_block_xpath:
             comment_block = tag.xpath(self.comment_block_xpath)
-            comment_block = ''.join(comment_block)
+            comment_block = ''.join(comment_block).replace(',', '').replace('.', '')
         else:
             return str(self.index)
 
