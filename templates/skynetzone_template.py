bd036aea9f502dbb283d726511df797661bd0092
Syed <syed7474747@gmail.com>
2021-04-30 06:29:05 +0500

    fixed date_xpath for skynetzone

--- templates/skynetzone_template.py
+++ templates/skynetzone_template.py
@@ -23,7 +23,7 @@ class SkynetzoneParser(BaseTemplate):
         self.comments_xpath = '//article[contains(@class,"message--post")]'
         self.header_xpath = '//article[contains(@class,"message--post")]'
         self.title_xpath = '//h1[@class="p-title-value"]//text()'
-        self.date_xpath = './/time//text()'
+        self.date_xpath = './/time//@data-time'
         self.author_xpath = './/div[@class="message-userDetails"]/h4/a//text()'
         self.post_text_xpath = './/article[contains(@class,"selectToQuote")]/descendant::text()[not(ancestor::div[contains(@class,"bbCodeBlock--quote")])]'
         self.avatar_xpath = './/div[@class="message-avatar "]//img/@src'
