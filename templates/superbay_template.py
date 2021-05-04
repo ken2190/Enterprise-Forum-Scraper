d2ebca48080b8db160b3ce49ed6cdd3db5ab0b32
Syed <syed7474747@gmail.com>
2021-05-04 07:31:49 +0500

    fixing date xpath/pattern as well as timezone issue

--- templates/superbay_template.py
+++ templates/superbay_template.py
@@ -1,5 +1,8 @@
 # -- coding: utf-8 --
+import datetime
 import re
+
+import dateparser
 
 from .base_template import BaseTemplate
 
@@ -20,7 +23,7 @@ class SuperBayParser(BaseTemplate):
         self.files = self.get_filtered_files(kwargs.get('files'))
         self.comments_xpath = '//div[contains(@class,"post classic")]'
         self.header_xpath = '//div[contains(@class,"post classic")]'
-        self.date_xpath = './/span[@class="post_date"]/text()'
+        self.date_xpath = './/span[@class="post_date"]/span[1]/@title|.//span[@class="post_date"]/text()'
         self.author_xpath = './/div[@class="author_information"]//span[@class="largetext"]/descendant::text()'
         self.title_xpath = '//td[@class="thead"]/div/strong/text()'
         self.post_text_xpath = './/div[@class="post_body scaleimages"]/descendant::text()[not(ancestor::blockquote)]'
@@ -28,7 +31,7 @@ class SuperBayParser(BaseTemplate):
         self.avatar_ext = 'jpg'
         self.comment_block_xpath = './/div[@class="post_head"]//strong/a/text()'
         self.date_pattern = "%b %d, %Y, %H:%M %p"
-
+        self.offset_hours = 4
         # main function
         self.main()
 
@@ -53,5 +56,45 @@ class SuperBayParser(BaseTemplate):
         if comment_block:
             comment_id = comment_block[-1].strip().split('#')[-1]
 
-
         return comment_id.replace(',', '').replace('.', '')
+
+    def construct_date_string(self, date_block):
+        if not date_block:  # if xpaths don't match return empty string.
+            return None
+        if ':' in date_block[0]:  # if title has complete date with time. return it as is.
+            return date_block[0].strip()
+        else:  # if title does not have time in it, grabs it from span text and append it.
+            return ''.join(date_block).strip()
+
+    def get_date(self, tag):
+        date_block = tag.xpath(self.date_xpath)
+        date = self.construct_date_string(date_block)
+
+        if not date:
+            return ""
+
+        # check if date is already a timestamp
+        try:
+            date_obj = datetime.datetime.strptime(date, self.date_pattern)
+            if self.offset_hours:
+                date_obj += datetime.timedelta(hours=self.offset_hours)
+            date = date_obj.timestamp()
+        except:
+            try:
+                date = float(date)
+            except:
+                err_msg = f"WARN: could not figure out date from: ({date}) using date pattern ({self.date_pattern})"
+                print(err_msg)
+                date_obj = dateparser.parse(date)
+                if self.offset_hours:
+                    date_obj += datetime.timedelta(hours=self.offset_hours)
+                date = date_obj.timestamp()
+
+        if date:
+            curr_epoch = datetime.datetime.today().timestamp()
+            if date > curr_epoch:
+                err_msg = f"ERROR: the timestamp ({date}) is after current time ({curr_epoch})"
+                print(err_msg)
+                raise RuntimeError(err_msg)
+            return str(date)
+        return ""
