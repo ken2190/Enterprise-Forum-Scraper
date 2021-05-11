39105b87418dfea4cedf3407f4651f3711462056
Syed <syed7474747@gmail.com>
2021-05-12 04:10:51 +0500

    fixes for broken itshqip scraper
    fixes for broken itshqip scraper
    
    Things fixed:
    - Scraper was not able to parse dates to them being in Albanian, added code to seelct English language on the site before starting the scrape.
    - incorrect date xpath in scraper for recent posts leading to pages with only posts from past two weeks not being scraped
    - use available timestamp directly for recent posts instead of date.
    - set offset_hours to be -1 to offset for DST dates.

--- scraper/itshqip.py
+++ scraper/itshqip.py
@@ -1,4 +1,7 @@
 import re
+from datetime import datetime
+
+import dateparser as dateparser
 from scrapy import Request
 from scraper.base_scrapper import (
     SitemapSpider,
@@ -11,26 +14,27 @@ class ItshqipSpider(SitemapSpider):
 
     # Url stuffs
     base_url = "https://forum.itshqip.com/"
+    change_language_url = "https://forum.itshqip.com/misc/language?language_id=1&redirect=https%3A%2F%2Fforum.itshqip.com%2F"
 
     # Xpath stuffs
     forum_xpath = '//h3[@class="nodeTitle"]/a[contains(@href, "forums/")]/@href'
     thread_xpath = '//ol[@class="discussionListItems"]/li'
-    thread_first_page_xpath = './/h3[@class="title"]'\
+    thread_first_page_xpath = './/h3[@class="title"]' \
                               '/a[contains(@href,"threads/")]/@href'
-    thread_last_page_xpath = './/span[@class="itemPageNav"]'\
+    thread_last_page_xpath = './/span[@class="itemPageNav"]' \
                              '/a[last()]/@href'
-    thread_date_xpath = './/dl[@class="lastPostInfo"]'\
-                        '//a[@class="dateTime"]/abbr/@data-datestring|'\
-                        './/dl[@class="lastPostInfo"]'\
-                        '//a[@class="dateTime"]/span/text()'
+    thread_date_xpath = './/dl[@class="lastPostInfo"]' \
+                        '//a[@class="dateTime"]/abbr/@data-time|' \
+                        './/dl[@class="lastPostInfo"]' \
+                        '//a[@class="dateTime"]/span/@title'
     pagination_xpath = '//nav/a[last()]/@href'
     thread_pagination_xpath = '//nav/a[@class="text"]/@href'
-    thread_page_xpath = '//nav//a[contains(@class, "currentPage")]'\
+    thread_page_xpath = '//nav//a[contains(@class, "currentPage")]' \
                         '/text()'
-    post_date_xpath = '//div[@class="privateControls"]'\
-                      '//span[@class="DateTime"]/text()|'\
-                      '//div[@class="privateControls"]'\
-                      '//abbr[@class="DateTime"]/@data-datestring'
+    post_date_xpath = '//div[@class="privateControls"]' \
+                      '//abbr[@class="DateTime"]/@data-time|'\
+                      '//div[@class="privateControls"]' \
+                      '//span[@class="DateTime"]/@title'
 
     avatar_xpath = '//div[@class="avatarHolder"]/a/img/@src'
 
@@ -46,8 +50,45 @@ class ItshqipSpider(SitemapSpider):
 
     # Other settings
     use_proxy = "On"
-    sitemap_datetime_format = '%b %d, %Y'
-    post_datetime_format = '%b %d, %Y'
+    sitemap_datetime_format = '%b %d, %Y at %I:%M %p'
+    post_datetime_format = '%b %d, %Y at %I:%M %p'
+
+    def start_requests(self, cookiejar=None, ip=None):
+        # Load meta
+        meta = {}
+        if cookiejar:
+            meta["cookiejar"] = cookiejar
+        if ip:
+            meta["ip"] = ip
+
+        # Branch choices requests
+        if self.sitemap_url:
+            yield Request(
+                url=self.sitemap_url,
+                headers=self.headers,
+                callback=self.parse_sitemap,
+                errback=self.check_site_error,
+                dont_filter=True,
+                meta=meta
+            )
+        else:
+            yield Request(
+                url=self.base_url,
+                headers=self.headers,
+                errback=self.check_site_error,
+                dont_filter=True,
+                callback=self.set_language_to_english,
+                meta=meta
+            )
+
+    def set_language_to_english(self, response):
+        yield Request(
+            url=self.change_language_url,
+            headers=self.headers,
+            errback=self.check_site_error,
+            dont_filter=True,
+            meta=self.synchronize_meta(response)
+        )
 
     def parse(self, response):
 
@@ -80,9 +121,36 @@ class ItshqipSpider(SitemapSpider):
         # Save avatars
         yield from super().parse_avatars(response)
 
+    def parse_thread_date(self, thread_date):
+        try:  # check if datetime is a float
+            date = datetime.fromtimestamp(float(thread_date))
+        except:
+            try:
+                date = datetime.strptime(
+                    thread_date.strip(),
+                    self.post_datetime_format
+                )
+            except:
+                date = dateparser.parse(thread_date)
+        if date:
+            return date.replace(tzinfo=None)
+
+    def parse_post_date(self, post_date):
+        try:  # check if datetime is a float
+            date = datetime.fromtimestamp(float(post_date))
+        except:
+            try:
+                date = datetime.strptime(
+                    post_date.strip(),
+                    self.post_datetime_format
+                )
+            except:
+                date = dateparser.parse(post_date)
+        if date:
+            return date.replace(tzinfo=None)
+
 
 class ItshqipScrapper(SiteMapScrapper):
-
     spider_class = ItshqipSpider
     site_name = 'itshqip.com'
     site_type = 'forum'
--- templates/itshqip_template.py
+++ templates/itshqip_template.py
@@ -12,12 +12,20 @@ class ItshqipParser(BaseTemplate):
         self.mode = 'r'
         self.comments_xpath = '//ol[@class="messageList"]/li[contains(@class,"message")]'
         self.header_xpath = '//ol[@class="messageList"]/li[contains(@class,"message")]'
-        self.date_xpath = './/div[contains(@class,"messageMeta")]//span[contains(@class,"DateTime")]/text()'
+        self.date_xpath = './/div[contains(@class,"messageMeta")]//abbr[contains(@class,"DateTime")]/@data-time|' \
+                          './/div[contains(@class,"messageMeta")]//span[contains(@class,"DateTime")]/@title'
         self.author_xpath = './/div[contains(@class,"messageMeta")]//span[contains(@class,"authorEnd")]/a/text()'
         self.post_text_xpath = './/div[contains(@class,"messageContent")]//article/blockquote/descendant::text()[not(ancestor::div[contains(@class,"bbCodeQuote")])]'
         self.avatar_xpath = './/div[contains(@class,"avatarHolder")]//img/@src'
         self.title_xpath = '//div[contains(@class,"titleBar")]/h1/text()'
         self.comment_block_xpath = './/div[@class="publicControls"]/a//text()'
-
+        self.date_pattern = '%b %d, %Y at %I:%M %p'
+        self.offset_hours = -1  # DST
         # main function
         self.main()
+
+    def get_date(self, tag):
+        date_block = tag.xpath(self.date_xpath)
+        date_string = date_block[0].strip() if date_block else None
+        date = self.parse_date_string(date_string)
+        return date
