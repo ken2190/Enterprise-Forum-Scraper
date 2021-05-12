ea2037c9f876c371777dd3ec1b45a85531ca6031
kylelopez29 <kyle.lopz29@gmail.com>
2021-05-12 02:38:18 -0700

    Fixed datetime difference issue in xaker.name

--- templates/xaker_template.py
+++ templates/xaker_template.py
@@ -1,4 +1,7 @@
 import re
+import datetime
+import dateparser
+
 from .base_template import BaseTemplate
 
 
@@ -18,6 +21,8 @@ class XakerParser(BaseTemplate):
         self.title_xpath = '//h1/text()'
         self.index = 0
 
+        self.offset_hours = -10
+
         # main function
         self.main()
     
@@ -35,3 +40,31 @@ class XakerParser(BaseTemplate):
             # comment_id = ''.join(comment_block).strip().split('#')[-1]
 
         return comment_id.replace(',', '').replace('.', '')
+
+    def get_date(self, tag):
+        date_block = tag.xpath(self.date_xpath)
+        date = date_block[0].strip() if date_block else None
+
+        if not date:
+            return ""
+
+        try:
+            date = float(date)
+        except:
+            try:
+                date = dateparser.parse(date)
+
+                if self.offset_hours:
+                    date += datetime.timedelta(hours=self.offset_hours)
+                date = date.timestamp()
+            except Exception as err:
+                err_msg = f"ERROR: Parsing {date} date is failed. {err}"
+                raise ValueError(err_msg)
+
+        curr_epoch = datetime.datetime.today().timestamp()
+
+        if date > curr_epoch:
+            err_msg = f"ERROR: the timestamp ({date}) is after current time ({curr_epoch})"
+            print(err_msg)
+            raise RuntimeError(err_msg)
+        return str(date)
