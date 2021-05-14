f0cbdf935f1923e845882e72091d5bf4ee316054
kylelopez29 <kyle.lopz29@gmail.com>
2021-05-13 23:12:30 -0700

    Fixed the dateparser issue in the xaker

--- templates/xaker_template.py
+++ templates/xaker_template.py
@@ -13,7 +13,7 @@ class XakerParser(BaseTemplate):
         self.mode = 'r'
         self.comments_xpath = '//ol[@class="messageList"]/li[contains(@class,"message")]'
         self.header_xpath = '//ol[@class="messageList"]/li[contains(@class,"message")]'
-        self.date_xpath = './/span[@class="DateTime"]/@title'
+        self.date_xpath = './/span[@class="DateTime"]/@title | .//abbr[@class="DateTime"]/text()'
         self.author_xpath = './/div[@class="userText"]/a[contains(@class,"username")]/text()|'\
                             './/div[@class="userText"]/a[contains(@class,"username")]/span/text()'
         self.post_text_xpath = './/div[contains(@class,"messageContent")]//article/blockquote/descendant::text()[not(ancestor::div[contains(@class,"bbCodeQuote")])]'
