246105d71dc0881de0d319eb66f121ebe2ba51a3
kylelopez29 <kyle.lopz29@gmail.com>
2021-05-24 02:13:30 -0700

    Built the carding team forum site scraper

--- scraper/carding_team.py
+++ scraper/carding_team.py
@@ -21,45 +21,26 @@ from scraper.base_scrapper import (
     SiteMapScrapper
 )
 
-
-USER = 'Cyrax_011'
-PASS = 'c2Yv9EP8MsgGHJr'
+USERNAME = 'MrSnow'
+PASSWORD = 'C1rh_2hf81!d'
 
 
 class CardingTeamSpider(SitemapSpider):
     name = 'cardingteam_spider'
 
     # Url stuffs
-    base_url = "https://cardingteam.net/"
+    base_url = "https://cardingteam.is/"
     login_url = f'{base_url}member.php?action=login'
-    # sitemap_url = 'https://cardingteam.cc/sitemap-index.xml'
-
-    # Sitemap Stuffs
-    forum_sitemap_xpath = '//sitemap[loc[contains(text(),"sitemap-threads.xml")] and lastmod]/loc/text()'
-    thread_sitemap_xpath = '//url[loc[contains(text(),"Thread-")] and lastmod]'
-    thread_url_xpath = '//loc/text()'
-    thread_lastmod_xpath = "//lastmod/text()"
 
     # Xpath stuffs
     login_form_xpath = '//form[@action="member.php"]'
-    forum_xpath = '//a[contains(@href, "Forum-")]/@href'
-    pagination_xpath = '//div[@class="pagination"]'\
+    forum_xpath = '//tbody//a[contains(@href, "Forum-")]/@href'
+    pagination_xpath = '//div[@class="pagination"]' \
                        '/a[@class="pagination_next"]/@href'
     thread_xpath = '//tr[@class="inline_row"]'
-    thread_first_page_xpath = './/span[contains(@id,"tid_")]/a/@href'
-    thread_last_page_xpath = './/td[contains(@class,"forumdisplay_")]/div'\
-                             '/span/span[contains(@class,"smalltext")]'\
-                             '/a[last()]/@href'
-    thread_date_xpath = './/td[contains(@class,"forumdisplay")]'\
-                        '/span[@class="lastpost smalltext"]/text()[1]|'\
-                        '..//td[contains(@class,"forumdisplay")]'\
-                        '/span[@class="lastpost smalltext"]/span/@title'
-    thread_pagination_xpath = '//div[@class="pagination"]'\
-                              '//a[@class="pagination_previous"]/@href'
+    thread_pagination_xpath = '//div[@class="pagination"]' \
+                              '//a[@class="pagination_next"]/@href'
     thread_page_xpath = '//span[@class="pagination_current"]/text()'
-    post_date_xpath = '//span[@class="post_date2"]/text()[1]|'\
-                      '//span[@class="post_date2"]/span/@title'\
-
     avatar_xpath = '//div[@class="author_avatar"]/a/img/@src'
 
     # Login Failed Message
@@ -70,103 +51,202 @@ class CardingTeamSpider(SitemapSpider):
         r".*/(\S+\.\w+)",
         re.IGNORECASE
     )
-    pagination_pattern = re.compile(
-        r".*page=(\d+)",
-        re.IGNORECASE
-    )
 
     # Other settings
     use_proxy = "On"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.master_list_dir = kwargs.get('master_list_path')
+
+        if not os.path.exists(self.master_list_dir):
+            os.mkdir(self.master_list_dir)
+
+    @staticmethod
+    def get_thread_info(thread_element):
+        """
+        Extract the thread URL and ID from the given thread_element.
+        """
+        subject_element = thread_element.xpath(".//span[contains(@class, 'subject_old')]")
+        thread_url = subject_element.xpath('.//a/@href').extract_first()
+        thread_id = subject_element.xpath('@id').extract_first()
+        thread_id = thread_id.replace('tid_', '').strip()
+
+        return thread_id, thread_url
+
+    @staticmethod
+    def get_post_ids(response):
+        """
+        Extract post IDs from new posts in the page.
+        """
+        post_ids = response.xpath("//div[@class='newposts']/a/@id").extract()
+        post_ids = [i.replace('pid', '').strip() for i in post_ids]
+
+        return post_ids
+
+    def check_parse_topic(self, topic_id, post_ids):
+        """
+        Check discrepancies between a list of current posts and the old posts
+        obtained from a txt file named with topic_id. And then determine whether or not
+        we need to parse the topic.
+
+        :param topic_id: indicates the ID of the topic
+        :param post_ids: a list of Post IDs.
+        """
+        topic_txt_file = f'{self.master_list_dir}/{topic_id}.txt'
+
+        if not os.path.exists(topic_txt_file):
+            # This means the topic is completely new.
+            with open(topic_txt_file, 'w') as file:
+                file.write(','.join(post_ids))
+            return True
+
+        with open(topic_txt_file, 'r') as file:
+            post_id_str = file.read().replace('\n', '')
+
+        old_post_ids = [i.strip() for i in post_id_str.split(',')]
+
+        if not set(post_ids) <= set(old_post_ids):
+            post_ids = list(set(old_post_ids + post_ids))
+            # Save the latest post ids in the txt file.
+            with open(topic_txt_file, 'w') as file:
+                file.write(','.join(post_ids))
+            return True
+        return False
+
     def start_requests(self):
         """
-        :return: => request start urls if no sitemap url or no start date
-                 => request sitemap url if sitemap url and start date
+        Send a get request to the login url to fetch the _token value
+        before start login.
         """
+        yield Request(
+            url=self.login_url,
+            headers=self.headers,
+            callback=self.start_login
+        )
 
-        # Load cookies
-        cookies, ip = self.get_cookies(proxy=self.use_proxy)
-
-        if self.start_date and self.sitemap_url:
-            yield Request(
-                url=self.sitemap_url,
-                headers=self.headers,
-                cookies=cookies,
-                dont_filter=True,
-                callback=self.parse_sitemap,
-                meta={
-                    "ip": ip
-                }
-            )
-        else:
-            yield Request(
-                url=self.login_url,
-                callback=self.proceed_for_login,
-                headers=self.headers,
-                dont_filter=True,
-                cookies=cookies,
-                meta={
-                    "ip": ip
-                }
-            )
-
-    def proceed_for_login(self, response):
-        # Synchronize user agent for cloudfare middleware
+    def start_login(self, response):
+        """
+        Login to the site using a given account credential.
+        """
         self.synchronize_headers(response)
-        formdata = {
-            'username': USER,
-            'password': PASS,
+
+        form_data = {
+            'username': USERNAME,
+            'password': PASSWORD,
         }
+
         yield FormRequest.from_response(
             response=response,
             formxpath=self.login_form_xpath,
-            formdata=formdata,
+            formdata=form_data,
             headers=self.headers,
-            meta=self.synchronize_meta(response),
-            callback=self.parse,
+            meta=self.synchronize_meta(response)
         )
 
-    def parse_thread_date(self, thread_date):
-        return datetime.today()
+    def parse_forum(self, response, thread_meta={}, is_first_page=True):
+        self.logger.info(f'Page_url: {response.url}')
 
-    def parse_post_date(self, thread_date):
-        return datetime.today()
+        threads = response.xpath(self.thread_xpath)
 
-    def parse(self, response):
-        # Synchronize cloudfare user agent
-        self.synchronize_headers(response)
+        for thread in threads:
+            thread_id, thread_url = self.get_thread_info(thread)
 
-        # Check if login failed
-        self.check_if_logged_in(response)
-        
-        all_forums = response.xpath(self.forum_xpath).extract()
+            # Check thread meta
+            if thread_meta:
+                meta = thread_meta
+            else:
+                meta = self.synchronize_meta(response)
 
-        # update stats
-        self.crawler.stats.set_value("mainlist/mainlist_count", len(all_forums))
-        for forum_url in all_forums:
-
-            # Standardize url
-            if self.base_url not in forum_url:
-                forum_url = response.urljoin(forum_url)
+            meta['topic_id'] = thread_id
 
             yield Request(
-                url=forum_url,
+                url=response.urljoin(thread_url),
+                headers=self.headers,
+                callback=self.parse_thread,
+                meta=meta
+            )
+
+        # get next page
+        next_page = self.get_forum_next_page(response)
+
+        if is_first_page:
+            self.crawler.stats.inc_value('mainlist/mainlist_processed_count')
+        if next_page:
+            # update stats
+            if is_first_page:
+                self.crawler.stats.inc_value('mainlist/mainlist_next_page_count')
+
+            yield Request(
+                url=next_page,
                 headers=self.headers,
                 callback=self.parse_forum,
                 meta=self.synchronize_meta(response),
+                cb_kwargs={'is_first_page': False}
             )
 
     def parse_thread(self, response):
+        self.synchronize_headers(response)
 
-        # Parse generic thread
-        yield from super().parse_thread(response)
+        topic_id = response.meta['topic_id']
 
-        # Parse generic avatar
-        yield from super().parse_avatars(response)
+        post_ids = self.get_post_ids(response)
+        parse_topic = self.check_parse_topic(topic_id, post_ids)
+
+        if not parse_topic:
+            # This topic doesn't have new comments, so, skipping...
+            self.logger.info(f'Already parsed this thread: {response.url} so, skipping...!')
+            self.crawler.stats.inc_value('mainlist/detail_already_scraped_count')
+            self.crawler.stats.set_value('mainlist/detail_count', len(self.topics))
+
+        if topic_id not in self.topics:
+            self.topics.add(topic_id)
+            self.crawler.stats.set_value('mainlist/detail_count', len(self.topics))
+
+        # Save thread content
+        if parse_topic and not self.useronly:
+            current_page = self.get_thread_current_page(response)
+
+            with open(file=os.path.join(self.output_path,
+                                        f"{topic_id}-{current_page}.html"),
+                      mode="w+",
+                      encoding="utf-8") as file:
+                file.write(response.text)
+
+            self.logger.info(f'{topic_id}-{current_page} done..!')
+            self.topic_pages_saved += 1
+
+            # Update stats
+            self.topics_scraped.add(topic_id)
+            self.crawler.stats.set_value('mainlist/detail_saved_count',
+                                         len(self.topics_scraped))
+
+            # Parse generic avatar
+            yield from super().parse_avatars(response)
+
+            # Kill task if kill count met
+            if self.kill and self.topic_pages_saved >= self.kill:
+                raise CloseSpider(reason='Kill count met, shut down.')
+
+        # get next page
+        next_page = self.get_thread_next_page(response)
+
+        if next_page:
+            self.crawler.stats.inc_value('mainlist/detail_next_page_count')
+
+            meta = self.synchronize_meta(response)
+            meta['topic_id'] = topic_id
+
+            yield Request(
+                url=next_page,
+                headers=self.headers,
+                callback=self.parse_thread,
+                meta=meta
+            )
 
 
 class CardingTeamScrapper(SiteMapScrapper):
-
     spider_class = CardingTeamSpider
     site_name = 'cardingteam.cc'
     site_type = 'forum'
--- templates/carding_team_template.py
+++ templates/carding_team_template.py
@@ -13,54 +13,20 @@ class CardingTeamParser(BaseTemplate):
         super().__init__(*args, **kwargs)
         # locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
         self.parser_name = "cardingteam.cc"
-        self.thread_name_pattern = re.compile(
-            r'(\d+).*html$'
-        )
-        self.pagination_pattern = re.compile(
-            r'.*-(\d+)\.html$'
-        )
         self.avatar_name_pattern = re.compile(r'.*/(\S+\.\w+)')
-        self.files = self.get_filtered_files(kwargs.get('files'))
         self.comments_xpath = '//div[@class="newposts"]'
         self.header_xpath = '//div[@class="newposts"]'
-        self.date_xpath = 'div//span[@class="post_date2"]/descendant::text()'
-        self.author_xpath = 'div//strong/span[@class="largetext"]/descendant::text()'
+        self.author_xpath = './/strong/span[@class="largetext"]/descendant::text()'
         self.title_xpath = '//td[@class="thead"]/div/strong/text()'
-        self.post_text_xpath = 'div//div[@class="post_body scaleimages"]/descendant::text()[not(ancestor::blockquote)]'
-        self.comment_block_xpath = 'div//div[@class="float_right"]/strong/a/text()'
-        self.avatar_xpath = 'div//div[@class="author_avatar"]/a/img/@src'
+        self.post_text_xpath = './/div[contains(@class, "post_body")]/descendant::text()[not(ancestor::blockquote)]'
+        self.comment_block_xpath = './/div[@class="float_right"]/strong/a/text()'
+        self.avatar_xpath = './/div[@class="author_avatar"]/a/img/@src'
 
         # main function
         self.main()
 
-    def get_filtered_files(self, files):
-        filtered_files = list(
-            filter(
-                lambda x: self.thread_name_pattern.search(x) is not None,
-                files
-            )
-        )
-        sorted_files = sorted(
-            filtered_files,
-            key=lambda x: (self.thread_name_pattern.search(x).group(1),
-                           self.pagination_pattern.search(x).group(1)))
-
-        return sorted_files
-
     def get_date(self, tag):
-        date_block = tag.xpath(self.date_xpath)
-        date = date_block[0].strip() if date_block else ""
-        try:
-            date = str(datetime.datetime.now().year) + ' ' + date
-            pattern = "%Y %A %d | %I:%M:%p"
-            date = datetime.datetime.strptime(date, pattern).timestamp()
-            return str(date)
-        except Exception:
-            date = ' '.join(date_block)
-            try:
-                date = dateparser.parse(date).timestamp()
-                return str(date)
-            except:
-                pass
-
-        return ""
+        """
+        Return empty value since this site has anonymous timestamp for every post.
+        """
+        return ''
