972e1cf297545a0cb0c817600d08b7ab6541e5de
Vinny Troia <vinny@nightlionsecurity.com>
2022-01-03 10:14:45 +0000

    year fix

--- tools/daily-backup.sh
+++ tools/daily-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 YEAR="2022"
 
-rclone copy /data/processing/offsite/2021/paste/ b2:/ViperStorage/pastes/$YEAR/ -v >> /var/log/aria-backup.log
-rclone copy /data/processing/offsite/2021/shadownet/ b2:/ViperStorage/shadownet/$YEAR/ -v >> /var/log/aria-backup.log
-rclone copy /data/processing/offsite/2021/forum/ b2:/ViperStorage/forums/$YEAR/ -v >> /var/log/aria-backup.log
-rclone copy /data/processing/offsite/2021/marketplace/ b2:/ViperStorage/forums/$YEAR/ -v >> /var/log/aria-backup.log
-rclone copy /data/processing/offsite/2021/meta/ b2:/ViperStorage/meta/$YEAR/ -v >> /var/log/aria-backup.log
-rclone copy /data/processing/offsite/2021/telegram/ b2:/ViperStorage/telegram/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/paste/ b2:/ViperStorage/pastes/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/shadownet/ b2:/ViperStorage/shadownet/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/forum/ b2:/ViperStorage/forums/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/marketplace/ b2:/ViperStorage/forums/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/meta/ b2:/ViperStorage/meta/$YEAR/ -v >> /var/log/aria-backup.log
+rclone copy /data/processing/offsite/$YEAR/telegram/ b2:/ViperStorage/telegram/$YEAR/ -v >> /var/log/aria-backup.log
