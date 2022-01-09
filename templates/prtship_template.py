6dd382b98b8758c872436cbf1c9ed6158e199111
Syed <syed7474747@gmail.com>
2022-01-10 04:02:21 +0500

    fixed prtship scraper

--- templates/prtship_template.py
+++ templates/prtship_template.py
@@ -12,19 +12,17 @@ class PrtShipParser(BaseTemplate):
         self.avatar_name_pattern = re.compile(r'.*\.(\d+)/')
         self.comments_xpath = '//div[@class="message-inner"]'
         self.header_xpath = '//div[@class="message-inner"]'
-        self.date_xpath = 'div//div[@class="message-attribution'\
-                          '-main"]/a/time/@data-time'
-        self.author_xpath = 'div//div[@class="message-userDetails'\
-                            '"]/h4/a//text()'
+        self.date_xpath = 'div//a/time/@data-time'
+        self.author_xpath = 'div//div[@class="message-userDetails"]/h4/a//text()'
         self.title_xpath = '//h1[@class="p-title-value"]/text()'
-        self.post_text_xpath = 'div//article/div[@class="bbWrapper"]/'\
+        self.post_text_xpath = 'div//article/div[@class="bbWrapper"]/text()|' \
+                               'div//article/div[@class="bbWrapper"]/img[contains(@class,"smilie") and @alt]/@alt|' \
+                               'div//article/div[@class="bbWrapper"]/'\
                                'descendant::text()[not(ancestor::blockquote)]'
         self.avatar_xpath = 'div//div[@class="message-avatar-wrapper"]/a'\
                             '[img/@src]/@href'
         self.avatar_ext = 'jpg'
-        self.comment_block_xpath = 'div//ul[@class="message-attribution-oppos'\
-                                   'ite message-attribution-opposite--list"]/'\
-                                   'li/a/text()'
+        self.comment_block_xpath = 'div//header//a[contains(text(),"#")]/text()'
 
         # main function
         self.main()
